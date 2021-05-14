75c43023dc66c326a531b75902626737f8a22608
Anna Sucher <a.sucher@uib.de>
2021-05-14 19:04:11 +0200

    refactor: to typescript

--- Button/BTNLogout.vue
+++ Button/BTNLogout.vue
@@ -1,29 +1,28 @@
 <template>
-  <b-button @click="logout">
+  <b-button @click="doLogout">
     Logout
   </b-button>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { mapMutations } from 'vuex'
-export default Vue.extend({
-  methods: {
-    ...mapMutations({
-      Xlogout: 'auth/logout'
-    }),
-    async logout () {
-      const response = await this.$axios.$post('/api/auth/logout')
-      if (response.result === 'logout success') {
-        this.Xlogout()
-        if (this.$route.name !== 'login') {
-          this.$router.push({ path: '/login' })
-        }
+import { Component, Vue, namespace } from 'nuxt-property-decorator'
+const auth = namespace('auth')
+@Component
+export default class BTNLogout extends Vue {
+  @auth.Mutation public logout!: () => void
+
+  async doLogout () {
+    const response = await this.$axios.$post('/api/auth/logout')
+    if (response.result === 'logout success') {
+      this.logout()
+      if (this.$route.name !== 'login') {
+        this.$router.push({ path: '/login' })
       }
     }
   }
-})
+}
 </script>
+
 <style>
 
 </style>
