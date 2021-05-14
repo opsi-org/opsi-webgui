1ea30571ddaf7b3d859a4b1018c94c01e6e83480
Anna Sucher <a.sucher@uib.de>
2021-05-14 19:24:37 +0200

    refactor: to typescript

--- opsiweb/components/Form/FLogin.vue
+++ opsiweb/components/Form/FLogin.vue
@@ -6,7 +6,7 @@
       <b-form-input v-model="form.password" :state="validPassword" type="password" />
       <!-- <b-button @click="login" :disabled="(validUsername&&validPassword)!==null">login</b-button> -->
       <!-- <b-button @click="login" :disabled="(validUsername!==null&&validPassword!==null)">login</b-button> -->
-      <b-button @click="login">
+      <b-button @click="doLogin">
         login
       </b-button>
       <p>{{ result }}</p>
@@ -14,66 +14,62 @@
   </div>
 </template>
 
-<script>
-import Vue from 'vue'
-import { mapMutations } from 'vuex'
+<script lang="ts">
+import { Component, Vue, namespace } from 'nuxt-property-decorator'
+const auth = namespace('auth')
+interface FormUser {
+    username: string,
+    password: string
+}
 
-export default Vue.extend({
-  name: 'FLogin',
-  fetchOnServer: false, // call fetch only on client-side
-  data () {
-    return {
-      opsiconfigserver: '',
-      form: { username: 'adminuser', password: 'adminuser' },
-      result: ''
-    }
-  },
+@Component export default class DDTheme extends Vue {
+  form: FormUser = { username: '', password: '' }
+  opsiconfigserver: string = ''
+  result: string = ''
+
+  @auth.Mutation public login!: (username: string) => void
+  @auth.Mutation public logout!: () => void
+
   async fetch () {
     this.opsiconfigserver = (await this.$axios.$post('/api/user/opsiserver')).result
-  },
-  computed: {
-    validUsername () {
-      if (this.form.username !== '') { return null }
-      return false
-    },
-    validPassword () {
-      if (this.form.password !== '') { return null }
-      return false
-    }
-  },
-  methods: {
-    ...mapMutations({
-      Xlogin: 'auth/login',
-      Xlogout: 'auth/logout'
-    }),
-    login () {
-      this.result = ''
-      if (!this.form.username) { return }
-      if (!this.form.password) { return }
-
-      const User = new FormData()
-      User.append('username', this.form.username)
-      User.append('password', this.form.password)
-      // let response = await this.$axios.$post('/api/auth/login', User)
-      this.$axios.post('/api/auth/login', User)
-        .then((response) => {
-          if (response.data.result === 'Login success') {
-            this.Xlogin(this.form.username)
-            if (this.$route.name === 'login') {
-              this.$router.push({ path: '/' })
-            } else {
-              this.$router.back()
-            }
-          }
-        }).catch((error) => {
-          // eslint-disable-next-line no-console
-          console.error(error)
-          this.Xlogout()
-          this.result = 'login failed'
-        })
-    }
   }
-})
+
+  get validUsername () {
+    if (this.form.username !== '') { return null }
+    return false
+  }
+
+  get validPassword () {
+    if (this.form.password !== '') { return null }
+    return false
+  }
+
+  doLogin () {
+    this.result = ''
+    if (!this.form.username) { return }
+    if (!this.form.password) { return }
+
+    const User = new FormData()
+    User.append('username', this.form.username)
+    User.append('password', this.form.password)
+    this.$axios.post('/api/auth/login', User)
+      .then((response) => {
+        if (response.data.result === 'Login success') {
+          this.login(this.form.username)
+          if (this.$route.name === 'login') {
+            this.$router.push({ path: '/' })
+          } else {
+            this.$router.back()
+          }
+        }
+      }).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error)
+        this.logout()
+        this.result = 'login failed'
+      })
+  }
+}
 </script>
 
 <style>
