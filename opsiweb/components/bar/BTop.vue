a336513362f81ee5c82c01242857310e70e7bc45
Jeena John <j.john@uib.de>
2021-05-14 17:38:02 +0200

    fix(layout): Fixed topbar and scrollable content; Ref #4

--- opsiweb/components/bar/BTop.vue
+++ opsiweb/components/bar/BTop.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <b-navbar toggleable="sm" sticky fixed="top" type="dark" variant="secondary">
+    <b-navbar class="topbar" toggleable="sm" fixed="top" type="dark" variant="secondary">
       <b-navbar-brand href="/">
         <img src="../../assets/images/LogoOpsi.png" class="logo"/>
         Opsiweb
       </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"> </b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
-        <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
             <ButtonBTNLogout />
-            <!-- <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button> -->
           </b-nav-form>
         </b-navbar-nav>
       </b-collapse>
@@ -22,5 +20,8 @@
 <style>
 .logo {
   height: 25px !important;
+}
+.topbar{
+  position: fixed;
 }
 </style>
---
+++ opsiweb/pages/depots.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    HELLO
+  </div>
+</template>
+
+
