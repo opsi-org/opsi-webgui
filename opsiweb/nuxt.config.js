c4479c6ee2c7eb493a5cbbeb3eeea120ee117849
Jeena John <j.john@uib.de>
2021-05-17 17:23:19 +0200

    build: Add bootstrap vue icons with nuxt

--- opsiweb/nuxt.config.js
+++ opsiweb/nuxt.config.js
@@ -61,6 +61,10 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios'
   ],
+  bootstrapVue: {
+    // Install the `IconsPlugin` plugin (in addition to `BootstrapVue` plugin)
+    icons: true
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // baseURL: process.env.API_URL,
