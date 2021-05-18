7c00ae9a03e5b65d053efa791c6060de34e8f9c0
Jeena John <j.john@uib.de>
2021-05-18 16:32:05 +0200

    feat(sidemenu): Add mini variant for navigation drawer

--- bar/BSide.vue
+++ bar/BSide.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <b-sidebar id="sidemenu" visible no-header bg-variant="secondary" text-variant="light">
+    <b-sidebar
+      id="sidemenu"
+      :class="{sidemenu_small: !attributes.currentlyOpen}"
+      bg-variant="secondary"
+      text-variant="light"
+      no-header
+      :no-close-on-route-change="$mq == 'desktop' ? true : false"
+      :visible="attributes.visible"
+    >
+      <!-- @hidden="attributes.visible =! attributes.visible" -->
       <b-nav vertical tabs>
         <b-nav-item active>
           Depots
@@ -8,12 +17,50 @@
         <b-nav-item>Clients</b-nav-item>
         <b-nav-item>Products</b-nav-item>
       </b-nav>
+      <template v-if="$mq === 'desktop'" #footer="">
+        <div class="sidemenu_footer">
+          <b-button
+            v-b-tooltip.hover
+            :title=" (!attributes.currentlyOpen)? 'Expand': 'Collapse'"
+            @click="toggleSidebar(!attributes.currentlyOpen)"
+          >
+            <b-icon v-if="!attributes.currentlyOpen" icon="chevron-double-right" variant="light" />
+            <b-icon v-else-if="attributes.currentlyOpen" icon="chevron-double-left" variant="light" />
+          </b-button>
+        </div>
+      </template>
     </b-sidebar>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      attributes: { visible: true, currentlyOpen: false }
+    }
+  },
+  methods: {
+    toggleSidebar (opened: boolean) {
+      this.attributes.currentlyOpen = opened
+    }
+  }
+})
+</script>
+
 <style>
 #sidemenu {
   top: 56px !important;
   width: 200px;
+  /* margin-bottom: 70px; */
+}
+.sidemenu_small > #sidemenu {
+  width: 40px;
+}
+.sidemenu_footer {
+  display: inline;
+  float: right;
+  margin-bottom: 70px;
 }
 </style>
