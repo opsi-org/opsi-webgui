b7be6f811764eef7a3e2682f12e358f0ecabe140
Jeena John <j.john@uib.de>
2021-05-25 14:16:46 +0200

    feat: Add hoverable dropdown-nav and collapsible navitem

---
+++ opsiweb/components/nav/NItemDropdownHoverable.vue
@@ -0,0 +1,56 @@
+<template>
+  <div @mouseover="onMouseOver" @mouseleave="onMouseLeave">
+    <b-nav-item-dropdown ref="dropdown" class="sidemenu_dropdown" block dropright no-caret>
+      <template #button-content>
+        <b-icon :icon="icon" />
+      </template>
+      <b-dropdown-item disabled>
+        {{ title }}
+      </b-dropdown-item>
+      <b-dropdown-divider />
+      <b-dropdown-item v-for="sub in submenu" :key="sub.title" :to="sub.route">
+        {{ sub.title }}
+      </b-dropdown-item>
+    </b-nav-item-dropdown>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    route: {
+      type: String,
+      default: ''
+    },
+    submenu: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  methods: {
+    onMouseOver () {
+      (this.$refs.dropdown as any).visible = true
+    },
+    onMouseLeave () {
+      (this.$refs.dropdown as any).visible = false
+    }
+  }
+})
+</script>
+
+<style>
+.sidemenu_dropdown .dropdown-menu{
+  left: calc(var(--width-sidebar-collapsed) - 6px) !important;
+}
+</style>
--- opsiweb/components/nav/NSidebar.vue
+++ opsiweb/components/nav/NSidebar.vue
@@ -1,35 +1,25 @@
 <template>
   <div>
-    Note: Dummy links
+    <span style="color:blue">Note: Edit navItems route in components/nav/NSidebar</span>
     <b-nav vertical tabs class="sidemenu_nav">
       <span v-for="catogery in navItems" :key="catogery.title">
         <NavNTitle :expanded="expanded" :title="catogery.title" />
         <span v-for="menuitem in catogery.menu" :key="menuitem.title">
           <template v-if="menuitem.submenu">
-            <!-- @mouseover="onOverDropdown(menuitem.title)" @mouseleave="onLeaveDropdown(menuitem.title)" -->
-            <div v-if="!expanded">
-              <b-nav-item-dropdown :ref="'dropdown-' + menuitem.title" class="sidemenu_dropdown" block dropright no-caret>
-                <template #button-content>
-                  <b-icon :icon="menuitem.icon" />
-                </template>
-                <b-dropdown-item disabled>
-                  {{ menuitem.title }}
-                </b-dropdown-item>
-                <b-dropdown-divider />
-                <b-dropdown-item v-for="submenu in menuitem.submenu" :key="submenu.title" :to="submenu.route">
-                  {{ submenu.title }}
-                </b-dropdown-item>
-              </b-nav-item-dropdown>
-            </div>
-            <div v-else>
-              <NavNItemCollapse
-                :expanded="expanded"
-                :title="menuitem.title"
-                :icon="menuitem.icon"
-                :route="menuitem.route"
-                :submenu="menuitem.submenu"
-              />
-            </div>
+            <NavNItemCollapsible
+              v-if="expanded"
+              :title="menuitem.title"
+              :icon="menuitem.icon"
+              :route="menuitem.route"
+              :submenu="menuitem.submenu"
+            />
+            <NavNItemDropdownHoverable
+              v-else
+              :title="menuitem.title"
+              :icon="menuitem.icon"
+              :route="menuitem.route"
+              :submenu="menuitem.submenu"
+            />
           </template>
           <template v-else>
             <NavNItem :expanded="expanded" :title="menuitem.title" :icon="menuitem.icon" :route="menuitem.route" />
@@ -63,6 +53,7 @@ export default Vue.extend({
               title: 'Depots',
               icon: 'hdd-stack-fill',
               submenu: [
+                { title: 'All Depots', route: '/dashboard' },
                 { title: 'Configuration', route: '/support' },
                 { title: 'Logs', route: '/support' }
               ]
@@ -71,7 +62,8 @@ export default Vue.extend({
               title: 'Clients',
               icon: 'laptop',
               submenu: [
-                { title: 'Add New', route: '/support' },
+                { title: 'All Clients', route: '/dashboard' },
+                { title: 'Add New', route: '/dashboard' },
                 { title: 'Configuration', route: '/support' },
                 { title: 'Logs', route: '/support' }
               ]
@@ -82,23 +74,13 @@ export default Vue.extend({
         {
           title: 'Configure',
           menu: [
-            { title: 'Settings', icon: 'gear-fill' },
+            { title: 'Settings', icon: 'gear-fill', route: '/support' },
             { title: 'Index page', icon: 'collection-fill', route: '/' }
           ]
         }
       ]
     }
   }
-  // methods: {
-  //   onOverDropdown (title: String) {
-  //     (this.$refs['dropdown-' + title] as any).visible = true
-  //     // (this.$refs.dropdown as any).visible = true
-  //   },
-  //   onLeaveDropdown (title: String) {
-  //     (this.$refs['dropdown-' + title] as any).visible = false
-  //     // (this.$refs.dropdown as any).visible = false
-  //   }
-  // }
 })
 </script>
 
@@ -106,8 +88,5 @@ export default Vue.extend({
 .sidemenu_nav{
   position:absolute;
   width: 100%;
-}
-.sidemenu_dropdown .dropdown-menu{
-  left: calc(var(--width-sidebar-collapsed) - 6px) !important;
 }
 </style>
