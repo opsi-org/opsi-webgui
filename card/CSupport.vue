51eb5e0fc22372b6855fc3837a918f30277c45df
Jeena John <j.john@uib.de>
2021-05-28 10:29:43 +0200

    feat: Add support page

---
+++ card/CSupport.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-container fluid>
+    <b-card>
+      <b-row>
+        <b-col md="7">
+          <b-card-body :title="item.title">
+            <b-card-text>
+              {{ item.description }}
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+        <b-col md="5">
+          <b-card-body>
+            <a class="btn btn-default border support-button" :href="item.link" target="_blank">{{ item.buttonname }}</a>
+          </b-card-body>
+        </b-col>
+      </b-row>
+    </b-card>
+  </b-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    item: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
+})
+</script>
+
+<style>
+.support-button {
+    width: 230px !important;
+}
+</style>
