6a42ad1aade4ffa13952c4b84a852cb407b631d2
Jeena John <j.john@uib.de>
2021-05-31 17:05:19 +0200

    fix: Visiblity issue in sidemenu

--- opsiweb/layouts/default.vue
+++ opsiweb/layouts/default.vue
@@ -17,7 +17,7 @@ import Vue from 'vue'
 export default Vue.extend({
   data () {
     return {
-      sidebarAttr: (this as any).$mq === 'desktop' ? { visible: true, expanded: false } : { visible: false, expanded: true }
+      sidebarAttr: { visible: true, expanded: false }
     }
   },
   head () {
