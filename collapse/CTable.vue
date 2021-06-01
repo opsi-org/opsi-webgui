b0f139b6811269c47439acca20640d4b100517ee
Jeena John <j.john@uib.de>
2021-06-01 18:20:12 +0200

    feat: Add collpasible tables with nav-collapse; Closes #12(3)

---
+++ collapse/CTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <BarBPageHeader
+      navbartype="collapse"
+      :collapsed="visible"
+      :aria-controls="title+'-collapse'"
+      :aria-expanded="visible"
+      :title="title"
+      @click.native="visible = !visible"
+    />
+    <b-collapse :id="title+'-collapse'" v-model="visible" accordion="table-accordion">
+      <b-container fluid>
+        <b-table striped hover :items="tableitems" />
+      </b-container>
+    </b-collapse>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    tableitems: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data: () => ({
+    visible: false
+  })
+})
+</script>
