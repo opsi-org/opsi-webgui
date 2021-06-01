3cf83b08b8bdaa9fa930be45d3521d8a718937a5
Jeena John <j.john@uib.de>
2021-06-01 17:12:39 +0200

    feat: page header for child; Closes #12(2.1, 2.4)

---
+++ opsiweb/components/bar/BPageHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-navbar>
+      <span><b>{{ title }}</b></span>
+      <b-navbar-nav class="ml-auto">
+        <b-button :to="closeroute">
+          <b-icon icon="x" />
+        </b-button>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    closeroute: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style>
+</style>
