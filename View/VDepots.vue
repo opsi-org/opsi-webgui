a7b26a83584c9196f2f56936955ecdb34f6438fc
Anna Sucher <a.sucher@uib.de>
2021-06-02 19:52:29 +0200

    fix: add missing files

--- View/VDepots.vue
+++ View/VDepots.vue
@@ -1,42 +1,51 @@
 <template>
-  <div>
-    <div class="mt-3">
-      <InputIFilter v-model="tableData.filterQuery" />
-      <TableTTable
-        id="tabledepots"
-        datakey="depotId"
-        :fields="Object.values(headerData).filter((h) => { return (h.visible || h._fixed) })"
-        :headers="headerData"
-        :items="fetchedData.depots"
-        :selection="selectionDepots"
-        :onchangeselection="setSelectionDepots"
-        :loading="isLoading"
-        :totalrows="fetchedData.total"
-        :no-local-sorting="true"
-        :sort-by.sync="tableData.sortBy"
-        :sort-desc.sync="tableData.sortDesc"
-        sort-icon-left
-        select-mode="multi"
-        selectable
-        striped
-        hover
-      >
-        <template #cell(actions)="">
-          <!-- :appendtitle="'of ' + row.item.ident" -->
-          <!-- <ButtonBTNGotoClients
-            @click="rowChanged([row.item])"
-          /> -->
-          <!-- {{row.item.ident}} -->
-        </template>
-      </TableTTable>
-      <BarBPagination
-        :tabledata="tableData"
-        :total-rows="fetchedData.total"
-        aria-controls="tabledepots"
-      />
-    </div>
-    {{ selectionDepots }}
-  </div>
+  <GridGTwoColumnLayout breadcrumb="" :showchild="secondColumnOpened">
+    <template #parent>
+      <div>
+        <div class="mt-3">
+          <InputIFilter v-model="tableData.filterQuery" />
+          Selection: {{ selectionDepots }}
+          <TableTTable
+            id="tabledepots"
+            datakey="depotId"
+            :fields="Object.values(headerData).filter((h) => { return (h.visible || h._fixed) })"
+            :headers="headerData"
+            :items="fetchedData.depots"
+            :selection="selectionDepots"
+            :onchangeselection="setSelectionDepots"
+            :loading="isLoading"
+            :totalrows="fetchedData.total"
+            :no-local-sorting="true"
+            :sort-by.sync="tableData.sortBy"
+            :sort-desc.sync="tableData.sortDesc"
+            sort-icon-left
+            select-mode="multi"
+            selectable
+            striped
+            hover
+          >
+            <template #cell(actions)="row">
+              <b-button
+                :to="{ path: '/depots/config', params: { id: row.item.last_name }}"
+                :class="{'nuxt-link-active': isRouteActive('/depots/config')}"
+                @click="rowId = row.item.ident;"
+              >
+                config
+              </b-button>
+            </template>
+          </TableTTable>
+          <BarBPagination
+            :tabledata="tableData"
+            :total-rows="fetchedData.total"
+            aria-controls="tabledepots"
+          />
+        </div>
+      </div>
+    </template>
+    <template #child>
+      <NuxtChild :id="rowId" />
+    </template>
+  </GridGTwoColumnLayout>
 </template>
 
 <script lang="ts">
@@ -45,8 +54,13 @@ import { ITableData, ITableHeaders } from '~/types/tsettings'
 const selections = namespace('selections')
 @Component
 export default class VClients extends Vue {
+  @selections.Getter public selectionDepots!: Array<string>
+  @selections.Mutation public setSelectionDepots!: (s: Array<string>) => void
+
+  rowId: string = ''
   fetchedData: object = {}
   isLoading: boolean = true
+  secondColumnOpened: boolean = false
   tableData: ITableData = {
     pageNumber: 1,
     perPage: 3,
@@ -66,11 +80,15 @@ export default class VClients extends Vue {
     actions: { key: 'actions', label: 'a', visible: true, _fixed: true }
   }
 
-  @selections.Getter public selectionDepots!: Array<string>
-  @selections.Mutation public setSelectionDepots!: (s: Array<string>) => void
+  isRouteActive (id: string) {
+    if (this.$route.path.includes(id)) {
+      this.secondColumnOpened = true
+    } else {
+      this.secondColumnOpened = false
+    }
+  }
 
-  @Watch('tableData', { deep: true })
-  tableDataChanged () { this.$fetch() }
+  @Watch('tableData', { deep: true }) tableDataChanged () { this.$fetch() }
 
   async fetch () {
     this.isLoading = true
