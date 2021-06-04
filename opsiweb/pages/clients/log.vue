c9fe5e3a862b0775d61228cf7809d6abbc0fe32a
Anna Sucher <a.sucher@uib.de>
2021-06-04 10:29:31 +0200

    fix: resture path /client/ for using twocolumnlayout

---
+++ opsiweb/pages/clients/log.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <BarBPageHeader title="Log" closeroute="/depots" />
+    <h1>Client LOG</h1>
+    <p>Hello {{ id }} !</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+@Component
+export default class PClientLog extends Vue {
+  @Prop({ }) id!: string
+  mounted () {
+    if (this.id === '') {
+      this.$router.push({ path: '/depots/' })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
