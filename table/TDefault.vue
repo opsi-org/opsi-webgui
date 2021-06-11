e629126ca916039923593ac0e789c2fb5ef701dd
Jeena John <j.john@uib.de>
2021-06-11 13:34:48 +0200

    feat(defaulttable): Add more options for config

--- table/TDefault.vue
+++ table/TDefault.vue
@@ -1,6 +1,29 @@
 <template>
   <b-container fluid>
-    <b-table striped hover :items="tableitems" />
+    <b-table striped hover :items="tableitems">
+      <template #cell(value)="row">
+        <template v-if="row.item.property == 'opsiHostKey'">
+          <b-input-group>
+            <b-form-input v-model="row.item.value" readonly :type="hideValue ? 'text': 'password' " />
+            <b-button :pressed.sync="hideValue">
+              <b-icon v-if="hideValue" icon="eye-slash" />
+              <b-icon v-else icon="eye" />
+            </b-button>
+          </b-input-group>
+        </template>
+        <template v-else-if="row.item.type == 'boolean'">
+          <b-form-checkbox v-model="row.item.value" disabled>
+            True
+          </b-form-checkbox>
+        </template>
+        <template v-else-if="row.item.type == 'switch'">
+          <b-form-checkbox v-model="row.item.value" switch disabled>
+            ON
+          </b-form-checkbox>
+        </template>
+        <b-form-input v-else v-model="row.item.value" readonly />
+      </template>
+    </b-table>
   </b-container>
 </template>
 
@@ -14,6 +37,9 @@ export default Vue.extend({
         return []
       }
     }
-  }
+  },
+  data: () => ({
+    hideValue: false
+  })
 })
 </script>
