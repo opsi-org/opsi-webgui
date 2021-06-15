4273f005b5f6d165b7e987af52f67e5f917fb4c0
Jeena John <j.john@uib.de>
2021-06-15 17:14:14 +0200

    refactor: Split log component to children

--- opsiweb/components/view/VClientsLog.vue
+++ opsiweb/components/view/VClientsLog.vue
@@ -1,67 +1,51 @@
 <template>
   <div>
-    <!-- TODO: Need backend method like '/api/opsidata/clientIds' for fetching only list of clientIds  -->
     <BarBPageHeader v-if="asChild" :title="'Log - ' + id" closeroute="/clients/" />
     <BarBPageHeader>
       <template #filter>
         <b-form-input v-model.trim="filterQuery" placeholder="Filter" @keyup="filterLog" />
       </template>
       <template v-if="!asChild" #selection>
-        {{ fetchClientIds () }}
-        <b-form-select v-model="id" :options="clientIds">
-          <template #first>
-            <b-form-select-option :value="null" disabled>
-              -- Please select a Client --
-            </b-form-select-option>
-          </template>
-        </b-form-select>
+        <slot name="IDSelection" />
       </template>
       <template #log>
-        <b-form-select
-          v-model="logtype"
-          :options="logTypes"
-        />
-        <b-form-spinbutton
-          v-model="loglevel"
-          class="loglevel_spinbutton"
-          min="0"
-          max="8"
-          step="1"
-          inline
-        />
+        <SelectSLogtype :logtype.sync="logtype" />
+        <SpinbuttonSBLoglevel :loglevel.sync="loglevel" />
       </template>
     </BarBPageHeader>
     <IconILoading v-if="isLoading" />
-    <div v-else class="container-fluid result-scrollable">
-      <div v-if="filteredLog == ''" class="container-fluid">
-        No Logs Found !
-      </div>
-      <div
-        v-for="(log, index) in filteredLog"
-        :key="index"
-        :class="{ 'd-none': !isLoglevelSmaller(log, loglevel) }"
-      >
-        <span
-          v-if="index != 0"
-          style="font-family: monospace;"
-          :class="{
-            'text-secondary': true,
-            'text-secondary': log.startsWith('[0]'),
-            'text-secondary': log.startsWith('[1]'),
-            'text-danger': log.startsWith('[2]'),
-            'text-warning': log.startsWith('[3]'),
-            'text-primary': log.startsWith('[4]'),
-            'text-success': log.startsWith('[5]'),
-            'text-secondary': log.startsWith('[6]'),
-            'text-muted': log.startsWith('[7]'),
-            'text-muted': log.startsWith('[8]'),
-            'text-muted': log.startsWith('[9]')
-          }"
+    <DivDScrollable v-else>
+      <template slot="content">
+        <div v-if="filteredLog == ''" class="container-fluid">
+          No Logs Found !
+        </div>
+        <div
+          v-for="(log, index) in filteredLog"
+          :key="index"
+          :class="{ 'd-none': !isLoglevelSmaller(log, loglevel) }"
         >
-          ({{ index }}) {{ log }}
-        </span>
-      </div>
-    </div>
+          <span
+            v-if="index != 0"
+            style="font-family: monospace;"
+            :class="{
+              'text-secondary': true,
+              'text-secondary': log.startsWith('[0]'),
+              'text-secondary': log.startsWith('[1]'),
+              'text-danger': log.startsWith('[2]'),
+              'text-warning': log.startsWith('[3]'),
+              'text-primary': log.startsWith('[4]'),
+              'text-success': log.startsWith('[5]'),
+              'text-secondary': log.startsWith('[6]'),
+              'text-muted': log.startsWith('[7]'),
+              'text-muted': log.startsWith('[8]'),
+              'text-muted': log.startsWith('[9]')
+            }"
+          >
+            ({{ index }}) {{ log }}
+          </span>
+        </div>
+      </template>
+    </DivDScrollable>
   </div>
 </template>
 
@@ -77,25 +61,19 @@ export default class VClientLog extends Vue {
   @Prop({ default: false }) 'asChild'!: string
 
   logtype: string = 'opsiconfd'
-  loglevel: number = 5
+  loglevel: number = 0
   logResult: Array<string> = []
   filteredLog: Array<string> = []
   filterQuery: string = ''
   logrequest: LogRequest = { selectedClient: '', selectedLogType: '' }
-  logTypes: Array<string> = ['bootimage', 'clientconnect', 'instlog', 'opsiconfd', 'userlogin']
   isLoading: boolean = false
-  clientIds: Array<string> = []
 
-  @Watch('logtype', { deep: true }) logtypeChanged () { this.getLog(this.id, this.logtype) }
-  @Watch('id', { deep: true }) idChanged () { this.getLog(this.id, this.logtype) }
+  @Watch('logtype', { deep: true }) logtypeChanged () { if (this.logtype && this.id) { this.getLog(this.id, this.logtype) } }
+  @Watch('id', { deep: true }) idChanged () { if (this.logtype && this.id) { this.getLog(this.id, this.logtype) } }
   @Watch('filterQuery', { deep: true }) filterQueryChanged () { this.filterLog() }
 
   beforeMount () {
     if (this.logtype && this.id) { this.getLog(this.id, this.logtype) }
-  }
-
-  fetchClientIds () {
-    this.clientIds = ['agorumcore-tst.uib.local', 'akunde1.uib.local']
   }
 
   filterLog () {
@@ -133,11 +111,4 @@ export default class VClientLog extends Vue {
 </script>
 
 <style>
-.loglevel_spinbutton{
-  min-width: 160px !important;
-}
-.result-scrollable{
-  height:700px;
-  overflow-y: scroll;
-}
 </style>
