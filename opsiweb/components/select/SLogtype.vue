ea86634d5c4639662219b512cd48196f870fa11a
Jeena John <j.john@uib.de>
2021-06-15 15:48:39 +0200

    feat: Child components for id selection

---
+++ opsiweb/components/select/SLogtype.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-form-select v-model="type" :options="logTypes" @change="$emit('update:logtype', type)">
+    <template #first>
+      <b-form-select-option :value="null" disabled>
+        -- Please select a Log type --
+      </b-form-select-option>
+    </template>
+  </b-form-select>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class SLogtype extends Vue {
+  // @Prop({ }) logtype!: string
+
+  logTypes: Array<string> = ['bootimage', 'clientconnect', 'instlog', 'opsiconfd', 'userlogin']
+  type: string = 'opsiconfd'
+}
+</script>
+
+<style>
+
+</style>
