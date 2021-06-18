b765b6133d9eb6cffd31a031c053dd8a9591d8c7
Jeena John <j.john@uib.de>
2021-06-18 14:36:17 +0200

    build: Add vue-treeselect

---
+++ opsiweb/plugins/vue-treeselect.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Treeselect from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
+// Vue.use(Treeselect)
+// Vue.use(Treeselect, { name: 'treeselect' })
+
+Vue.component('Treeselect', Treeselect)
