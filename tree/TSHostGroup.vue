b0b15043c9bbd5731966f32c299f3275dab0b8ba
Jeena John <j.john@uib.de>
2021-06-23 08:55:07 +0200

    feat: Extend the treeselect

--- tree/TSHostGroup.vue
+++ tree/TSHostGroup.vue
@@ -1,45 +1,13 @@
 <template>
-  <div>
-    <treeselect
-      v-model="groupSelection"
-      :multiple="true"
-      :options="hostGroup"
-      value-format="object"
-      :max-height="200"
-      @select="groupSelect"
-      @deselect="groupDeselect"
-    >
-      <div slot="option-label" slot-scope="{ node }">
-        <div :ref="'tree-item-'+node.id">
-          <b-icon v-if="node.isBranch" icon="hdd-network-fill" />
-          <b-icon v-else icon="laptop" />
-          <small> {{ node.label }} </small>
-        </div>
-      </div>
-    </treeselect>
-    groupSelection: {{ groupSelection }}
-  </div>
+  <TreeTSTreeselect :options="hostGroup" :type="'hostgroup'" />
 </template>
 
 <script lang="ts">
-import { Component, namespace, Watch, Vue } from 'nuxt-property-decorator'
-const selections = namespace('selections')
+import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
 export default class TSHostGroup extends Vue {
-  groupSelection: Array<any> = []
   hostGroup: Array<object> = []
-  groupIdList: Array<string> = []
-  item: any
-
-  @selections.Getter public selectionClients!: Array<string>
-  @selections.Mutation public setSelectionClients!: (s: Array<string>) => void
-  @selections.Mutation public pushToSelectionClients!: (s: string) => void
-  @selections.Mutation public delFromSelectionClients!: (s: string) => void
-
-  @Watch('selectionClients', { deep: true }) selectionClientsChanged () {
-    this.syncStoreToTree()
-  }
 
   fetch () {
     // TODO: Need backend method like '/api/opsidata/hostgroup' for fetching hostgroups for the selectedDepotsList
@@ -92,102 +60,6 @@ export default class TSHostGroup extends Vue {
         ]
       }
     ]
-    this.mapElementsByValue(this.hostGroup, 'ObjectToGroup', 'type', 'label', this.groupIdList)
-  }
-
-  arrEqual (aOrg: Array<string>, bOrg: Array<string>) {
-    if (aOrg.length === bOrg.length && aOrg.length === 0) { return true } else if (aOrg.length !== bOrg.length) { return false }
-    const a = JSON.parse(JSON.stringify(aOrg))
-    const b = JSON.parse(JSON.stringify(bOrg))
-    if (a === b) { return true }
-    if (a == null || b == null) { return false }
-    if (a.length !== b.length) { return false }
-    a.sort()
-    b.sort()
-
-    for (let i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) {
-        return false
-      }
-    }
-    return true
-  }
-
-  syncStoreToTree () {
-    const storeData = this.selectionClients
-    let treeData = this.groupSelection.filter(item => item.type === 'ObjectToGroup')
-    treeData = [...new Set(treeData)]
-    if (this.arrEqual(storeData, treeData)) {
-      // eslint-disable-next-line no-useless-return
-      return
-    }
-    const elementsInTree: Array<string> = []
-    // found all elements in tree with node.text == clientId
-    for (const index in storeData) {
-      if (this.groupIdList.includes(storeData[index])) {
-        this.filterElementsByValue(
-          this.hostGroup, storeData[index],
-          'label', elementsInTree)
-      }
-    }
-    this.groupSelection = elementsInTree
-    // eslint-disable-next-line no-console
-    console.debug('syncStoreToTree groupSelection', JSON.stringify(this.groupSelection))
-  }
-
-  filterElementsByValue (elements: any, matchingValue: string, key: string, resultArray:Array<string>) {
-    for (const elementKey in elements) {
-      const element = elements[elementKey]
-      if (element[key] === matchingValue) {
-        resultArray.push(element)
-      } else if (element.children != null) {
-        this.filterElementsByValue(element.children, matchingValue, key, resultArray)
-      }
-    }
-  }
-
-  mapElementsByValue (elements:any, matchingValue: string, compareKey:string, mapKey:string, resultArray:Array<string>) {
-    for (const elementKey in elements) {
-      const element = elements[elementKey]
-      if (element[compareKey] === matchingValue) {
-        resultArray.push(element[mapKey])
-      } else if (element.children != null) {
-        this.mapElementsByValue(element.children, matchingValue, compareKey, mapKey, resultArray)
-      }
-    }
-  }
-
-  groupChange (value: object, type: string) {
-    let idList: Array<string> = []
-    this.mapElementsByValue([value], 'ObjectToGroup', 'type', 'label', idList)
-    idList = [...new Set(idList)]
-    let storeData
-    storeData = this.selectionClients
-    storeData = [...new Set(storeData)]
-
-    for (const i in idList) {
-      const objectId = idList[i]
-      if (type === 'select') {
-        // if (storeData.includes(objectId)) {
-        //   return null
-        // } else {
-        this.pushToSelectionClients(objectId)
-        // }
-      }
-      if (type === 'deselect') {
-        if (storeData.includes(objectId)) {
-          this.delFromSelectionClients(objectId)
-        }
-      }
-    }
-  }
-
-  groupSelect (selection: any) {
-    this.groupChange(selection, 'select')
-  }
-
-  groupDeselect (deselection: object) {
-    this.groupChange(deselection, 'deselect')
   }
 }
 </script>
