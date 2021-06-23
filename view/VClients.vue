2308db0c80edbeaa8a065d0f8e9062e33b882f81
Jeena John <j.john@uib.de>
2021-06-23 15:44:29 +0200

    feat:Add Header and footer for all tables

--- view/VClients.vue
+++ view/VClients.vue
@@ -1,62 +1,60 @@
 <template>
   <GridGTwoColumnLayout :showchild="secondColumnOpened">
     <template #parent>
-      <div class="mt-3">
-        Filter: {{ tableData.filterQuery }}
-        <div class="inline">
+      <BarBPageHeader>
+        <template #filter>
           <InputIFilter :data="tableData" />
+        </template>
+        <template #selection>
           <DropdownDDDepotIds v-if="fetchedDataDepotIds.length > 1" />
-          Selection: {{ selectionClients }} <br>
-          rowID {{ rowId }}
-        </div>
-        <TableTTable
-          id="tableclients"
-          datakey="clientId"
-          :fields="Object.values(headerData).filter((h) => { return (h.visible || h._fixed) })"
-          :headers="headerData"
-          :items="fetchedData.clients"
-          :selection="selectionClients"
-          :onchangeselection="setSelectionClients"
-          :loading="isLoading"
-          :totalrows="fetchedData.total"
-          :no-local-sorting="true"
-          :sort-by.sync="tableData.sortBy"
-          :sort-desc.sync="tableData.sortDesc"
-          select-mode="multi"
-          selectable
-        >
-          <template #cell(rowactions)="row">
-            <ButtonBTNRowLinkTo
-              title="config"
-              icon="gear"
-              to="/clients/config"
-              :ident="row.item.ident"
-              :pressed="isRouteActive"
-              :click="routeRedirectWith"
-            />
+        </template>
+      </BarBPageHeader>
+      <TableTTable
+        id="tableclients"
+        datakey="clientId"
+        :fields="Object.values(headerData).filter((h) => { return (h.visible || h._fixed) })"
+        :headers="headerData"
+        :items="fetchedData.clients"
+        :selection="selectionClients"
+        :onchangeselection="setSelectionClients"
+        :loading="isLoading"
+        :totalrows="fetchedData.total"
+        :no-local-sorting="true"
+        :sort-by.sync="tableData.sortBy"
+        :sort-desc.sync="tableData.sortDesc"
+        select-mode="multi"
+        selectable
+      >
+        <template #cell(rowactions)="row">
+          <ButtonBTNRowLinkTo
+            title="config"
+            icon="gear"
+            to="/clients/config"
+            :ident="row.item.ident"
+            :pressed="isRouteActive"
+            :click="routeRedirectWith"
+          />
 
-            <ButtonBTNRowLinkTo
-              title="log"
-              icon="file-earmark-text"
-              to="/clients/log"
-              :ident="row.item.ident"
-              :pressed="isRouteActive"
-              :click="routeRedirectWith"
-            />
-          </template>
-        </TableTTable>
-        <BarBPagination
-          :tabledata="tableData"
-          :total-rows="fetchedData.total"
-          aria-controls="tableclients"
-        />
-      </div>
-      <div>
-        {{ selectionDepots }}
-      </div>
-      <div>
-        {{ selectionClients }}
-      </div>
+          <ButtonBTNRowLinkTo
+            title="log"
+            icon="file-earmark-text"
+            to="/clients/log"
+            :ident="row.item.ident"
+            :pressed="isRouteActive"
+            :click="routeRedirectWith"
+          />
+        </template>
+      </TableTTable>
+      <BarBPagination
+        :tabledata="tableData"
+        :total-rows="fetchedData.total"
+        aria-controls="tableclients"
+      />
+      <b>Selection: </b> <br>
+      Depots : {{ selectionDepots }} <br>
+      Clients : {{ selectionClients }} <br>
+      <!-- rowID {{ rowId }} <br>
+      Filter Query: {{ tableData.filterQuery }} -->
     </template>
     <template #child>
       <NuxtChild :id="rowId" :as-child="true" />
@@ -81,7 +79,7 @@ export default class VClients extends Vue {
   fetchOptions: IFetchOptions = { fetchClients: true, fetchDepotIds: true }
   tableData: ITableData = {
     pageNumber: 1,
-    perPage: 2,
+    perPage: 5,
     sortBy: 'clientId',
     sortDesc: false,
     filterQuery: '',
