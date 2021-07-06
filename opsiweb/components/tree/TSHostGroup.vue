77144e0459692508730f35e239d92604611da69a
Jeena John <j.john@uib.de>
2021-07-06 18:51:41 +0200

    feat: fetch hostgroup data

--- opsiweb/components/tree/TSHostGroup.vue
+++ opsiweb/components/tree/TSHostGroup.vue
@@ -1,5 +1,7 @@
 <template>
-  <TreeTSTreeselect :options="hostGroup" :type="'hostgroup'" />
+  <div>
+    <LazyTreeTSTreeselect v-if="hostGroup" :options="hostGroup" :type="'hostgroup'" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,57 +11,8 @@ import { Component, Vue } from 'nuxt-property-decorator'
 export default class TSHostGroup extends Vue {
   hostGroup: Array<object> = []
 
-  fetch () {
-    // TODO: Need backend method like '/api/opsidata/hostgroup' for fetching hostgroups for the selectedDepotsList
-    // this.hostGroup = (await this.$axios.$post('/api/opsidata/hostgroup', selectedDepotsList)).result
-    this.hostGroup = [
-      {
-        id: '40_gefundene_software',
-        label: '40_gefundene_software',
-        type: 'HostGroup',
-        children: [
-          {
-            id: 'agorumcore-tst.uib.local;40_gefundene_software',
-            label: 'agorumcore-tst.uib.local',
-            type: 'ObjectToGroup'
-          },
-          {
-            id: 'akunde1.uib.local;40_gefundene_software',
-            label: 'akunde1.uib.local',
-            type: 'ObjectToGroup'
-          },
-          {
-            id: 'testgroup1;40_gefundene_software',
-            label: 'testgroup1',
-            type: 'HostGroup',
-            children: [
-              {
-                id: 'customibmtm62.uib.local;testgroup1',
-                label: 'customibmtm62.uib.local',
-                type: 'ObjectToGroup'
-              },
-              {
-                id: 'noteabdul.uib.local;testgroup1',
-                label: 'noteabdul.uib.local',
-                type: 'ObjectToGroup'
-              }
-            ]
-          },
-          {
-            id: 'testgroup2;40_gefundene_software',
-            label: 'testgroup2',
-            type: 'HostGroup',
-            children: [
-              {
-                id: 'akunde1.uib.local;testgroup2',
-                label: 'akunde1.uib.local',
-                type: 'ObjectToGroup'
-              }
-            ]
-          }
-        ]
-      }
-    ]
+  async fetch () {
+    this.hostGroup = Object.values((await this.$axios.$post('/api/opsidata/hosts/groups', '')).result.groups.children)
   }
 }
 </script>
