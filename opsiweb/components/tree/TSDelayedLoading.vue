bd9374c34390151e9275965537ba83df3ed00880
Jeena John <j.john@uib.de>
2021-07-14 17:17:11 +0200

    feat(treeselect): Sync store to delayed loading children

--- opsiweb/components/tree/TSDelayedLoading.vue
+++ opsiweb/components/tree/TSDelayedLoading.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
     <treeselect
-      v-model="value"
+      v-model="groupSelection"
       :multiple="true"
       :options="options"
+      :normalizer="normalizer"
       :load-options="loadOptions"
       placeholder="Dummy Hostgroup with delayed loading children"
-    />
+      value-format="object"
+      :max-height="400"
+      @select="groupSelect"
+      @deselect="groupDeselect"
+    >
+      <div slot="option-label" slot-scope="{ node }">
+        <div :ref="'tree-item-'+node.id">
+          <b-icon v-if="node.isBranch" icon="hdd-network-fill" />
+          <b-icon v-else icon="laptop" />
+          <small> {{ node.label }} </small>
+        </div>
+      </div>
+    </treeselect>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, namespace, Watch, Vue } from 'nuxt-property-decorator'
+const selections = namespace('selections')
 
 @Component
 export default class TSDelayedLoading extends Vue {
@@ -19,24 +33,125 @@ export default class TSDelayedLoading extends Vue {
   children: Array<object> = []
   options: Array<object> = []
 
+  groupSelection: Array<any> = []
+  groupIdList: Array<string> = []
+  item: any
+  storeData : Array<string> = []
+
+  @selections.Getter public selectionClients!: Array<string>
+  @selections.Mutation public pushToSelectionClients!: (s: string) => void
+  @selections.Mutation public delFromSelectionClients!: (s: string) => void
+
+  @Watch('selectionClients', { deep: true }) selectionClientsChanged () {
+    this.syncStoreToTree()
+  }
+
+  @Watch('options', { deep: true }) optionsChanged () {
+    this.syncStoreToTree()
+  }
+
   fetch () {
-    // TODO: request backend for groups with children:'null'
+    // TODO: request backend for groups (sorted order) with children:'null'
     // request: Request = { selectedDepots: [], parentGroup: '' }
     // where request.parentGroup = '' // empty string
     // this.hostGroup = Object.values((await this.$axios.$post('/api/opsidata/hosts/groups', JSON.stringify(this.request))).result)
     this.options = [{
       id: '40_gefundene_software',
-      label: '40_gefundene_software',
+      text: '40_gefundene_software',
       children: null
     }, {
       id: 'aa_opsiconf_basis',
-      label: 'aa_opsiconf_basis',
+      text: 'aa_opsiconf_basis',
       children: null
     }, {
       id: 'ab_group',
-      label: 'ab_group',
+      text: 'ab_group',
       children: null
     }]
+  }
+
+  beforeUpdate () {
+    this.storeData = this.selectionClients
+    this.filterObjectLabel(this.options, 'ObjectToGroup', 'type', 'text', this.groupIdList)
+    this.syncStoreToTree()
+  }
+
+  normalizer (node: any) {
+    return {
+      id: node.id,
+      type: node.type,
+      label: node.text.replace(/_+$/, ''),
+      children: node.children
+    }
+  }
+
+  arrEqual (arr1: Array<string>, arr2: Array<string>) {
+    return JSON.stringify(arr1) === JSON.stringify(arr2)
+  }
+
+  syncStoreToTree () {
+    let treeData = this.groupSelection.filter(item => item.type === 'ObjectToGroup')
+    treeData = [...new Set(treeData)]
+    if (this.arrEqual(this.storeData, treeData)) {
+      // eslint-disable-next-line no-useless-return
+      return
+    }
+    const elementsInTree: Array<string> = []
+    for (const index in this.storeData) {
+      if (this.groupIdList.includes(this.storeData[index])) {
+        this.filterObject(
+          this.options, this.storeData[index],
+          'text', elementsInTree)
+      }
+    }
+    this.groupSelection = elementsInTree
+  }
+
+  filterObject (elements: any, matchingValue: string, key: string, resultArray:Array<string>) {
+    for (const elementKey in elements) {
+      const element = elements[elementKey]
+      if (element[key] === matchingValue) {
+        resultArray.push(element)
+      } else if (element.children != null) {
+        this.filterObject(element.children, matchingValue, key, resultArray)
+      }
+    }
+  }
+
+  filterObjectLabel (elements:any, matchingValue: string, compareKey:string, mapKey:string, resultArray:Array<string>) {
+    for (const elementKey in elements) {
+      const element = elements[elementKey]
+      if (element[compareKey] === matchingValue) {
+        resultArray.push(element[mapKey])
+      } else if (element.children != null) {
+        this.filterObjectLabel(element.children, matchingValue, compareKey, mapKey, resultArray)
+      }
+    }
+  }
+
+  groupChange (value: object, type: string) {
+    const idList : Array<string> = []
+    this.filterObjectLabel([value], 'ObjectToGroup', 'type', 'text', idList)
+
+    for (const i in idList) {
+      const objectId = idList[i]
+      if (type === 'select') {
+        this.pushToSelectionClients(objectId)
+      }
+      if (type === 'deselect') {
+        if (this.storeData.includes(objectId)) {
+          this.delFromSelectionClients(objectId)
+        }
+      }
+    }
+  }
+
+  groupSelect (selection: any) {
+    this.groupChange(selection, 'select')
+  }
+
+  groupDeselect (deselection: object) {
+    this.groupChange(deselection, 'deselect')
   }
 
   loadOptions ({ action, parentNode, callback }: any) {
@@ -46,7 +161,7 @@ export default class TSDelayedLoading extends Vue {
     }
   }
 
-  // TODO: Again request backend
+  // TODO: Again request backend for children (sorted order)
   // request: Request = { selectedDepots: [], parentGroup: '' }
   // where request.parentGroup = this.parentId
   // this.hostGroup = Object.values((await this.$axios.$post('/api/opsidata/hosts/groups', JSON.stringify(this.request))).result)
@@ -54,39 +169,39 @@ export default class TSDelayedLoading extends Vue {
     if (parentId === '40_gefundene_software') {
       this.children = [{
         id: 'windows_treiberpaket',
-        label: 'windows_treiberpaket',
+        text: 'windows_treiberpaket',
         children: null
       }]
     }
     if (parentId === 'windows_treiberpaket') {
       this.children = [{
-        id: 'dummy1.uib.local',
-        label: 'dummy1.uib.local',
+        id: 'agorumcore-tst.uib.local',
+        text: 'agorumcore-tst.uib.local',
         type: 'ObjectToGroup'
       }, {
-        id: 'dummy2.uib.local',
-        label: 'dummy1.uib.local',
+        id: 'akunde1.uib.local',
+        text: 'akunde1.uib.local',
         type: 'ObjectToGroup'
       },
       {
-        id: 'dummy3.uib.local',
-        label: 'dummy1.uib.local',
+        id: 'alena.uib.local',
+        text: 'alena.uib.local',
         type: 'ObjectToGroup'
       }]
     }
     if (parentId === 'aa_opsiconf_basis') {
       this.children = this.children = [{
-        id: 'dummy4.uib.local',
-        label: 'dummy1.uib.local',
+        id: 'akunde1.uib.local',
+        text: 'akunde1.uib.local',
         type: 'ObjectToGroup'
       }, {
-        id: 'dummy5.uib.local',
-        label: 'dummy1.uib.local',
+        id: 'alena.uib.local',
+        text: 'alena.uib.local',
         type: 'ObjectToGroup'
       },
       {
-        id: 'dummy6.uib.local',
-        label: 'dummy1.uib.local',
+        id: 'anna-vm-24001.uib.local',
+        text: 'anna-vm-24001.uib.local',
         type: 'ObjectToGroup'
       }]
     }
