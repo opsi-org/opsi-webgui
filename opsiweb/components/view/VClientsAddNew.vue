16d11569b17fc73c3fc34728b16442a7faa7d6da
Jeena John <j.john@uib.de>
2021-07-21 17:49:07 +0200

    [chg] Add new client

--- opsiweb/components/view/VClientsAddNew.vue
+++ opsiweb/components/view/VClientsAddNew.vue
@@ -2,21 +2,91 @@
   <div>
     <BarBPageHeader>
       <template #right>
-        <!-- @click="resetCreateNewOpsiClientModal()" -->
-        <b-button>
+        <b-button @click="resetNewClientForm()">
           <b-icon icon="arrow-counterclockwise" /> Reset
         </b-button>
-        <!-- :disabled="!newClient.clientNamewithoutDomain" -->
-        <!-- @click="createOpsiClient()" -->
-        <b-button>
+        <b-button :disabled="!clientNamewithoutDomain" @click="createOpsiClient()">
           <b-icon icon="plus" /> Add
         </b-button>
       </template>
     </BarBPageHeader>
-    <!-- <IconILoading v-if="isLoading" /> -->
-    <DivDScrollResult>
+    <IconILoading v-if="isLoading" />
+    <DivDScrollResult v-else>
       <template slot="content">
-        <FormFAddNewClient />
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right">
+            Client ID:
+          </b-col>
+          <b-col>
+            <b-form-input
+              v-model="clientNamewithoutDomain"
+              type="text"
+              required
+            />
+          </b-col>
+          <b-col>
+            <b-form-input v-model="domainName" size="sm" type="text" required />
+          </b-col>
+        </b-row>
+        <b>Client Details: </b>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right">
+            Description:
+          </b-col>
+          <b-col>
+            <b-form-input
+              v-model="newClient.description"
+              type="text"
+            />
+          </b-col>
+        </b-row>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right">
+            Inventory Number:
+          </b-col>
+          <b-col>
+            <b-form-input
+              v-model="newClient.inventoryNumber"
+              type="text"
+            />
+          </b-col>
+        </b-row>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right">
+            Hardware Address:
+          </b-col>
+          <b-col>
+            <b-form-input
+              v-model="newClient.hardwareAddress"
+              type="text"
+            />
+          </b-col>
+        </b-row>
+
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right">
+            IP Address:
+          </b-col>
+          <b-col>
+            <b-form-input
+              v-model="newClient.ipAddress"
+              type="text"
+            />
+          </b-col>
+        </b-row>
+        <b>Additional Information: </b>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right">
+            Notes:
+          </b-col>
+          <b-col>
+            <b-form-textarea
+              v-model="newClient.notes"
+              rows="2"
+              no-resize
+            />
+          </b-col>
+        </b-row>
       </template>
     </DivDScrollResult>
   </div>
@@ -24,9 +94,38 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+interface NewClient {
+  hostId: string,
+  description: string,
+  inventoryNumber: string,
+  hardwareAddress: string,
+  ipAddress: any,
+  notes: string
+}
 
 @Component export default class VClientsAddNew extends Vue {
+  isLoading: boolean = false
+  domainName: string = '.uib.local'
+  clientNamewithoutDomain: string = ''
+  newClient: NewClient = {
+    hostId: '',
+    description: '',
+    inventoryNumber: '',
+    hardwareAddress: '',
+    ipAddress: null,
+    notes: ''
+  }
 
+  resetNewClientForm () {
+    this.clientNamewithoutDomain = ''
+    this.newClient = {} as NewClient
+  }
+
+  createOpsiClient () {
+    this.isLoading = true
+    // await this.$axios.$post('/api/opsidata/clients', JSON.stringify(this.newClient))
+    this.isLoading = false
+  }
 }
 </script>
 
