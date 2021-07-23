99fc59401fc8eadfafd4121ba39d1fa68bd36be0
Jeena John <j.john@uib.de>
2021-07-23 17:41:56 +0200

    [chg] Hide modules signature; add busy state

--- opsiweb/pages/settings.vue
+++ opsiweb/pages/settings.vue
@@ -2,14 +2,7 @@
   <div class="container-fluid">
     <b-tabs>
       <b-tab title="Modules content" active>
-        <DivDScrollResult>
-          <template slot="content">
-            <b-table stacked :items="[modules]" />
-            <!-- <div v-for="v,k in modules" :key="k">
-              <p>{{ k }}:{{ v }}</p>
-            </div> -->
-          </template>
-        </DivDScrollResult>
+        <ViewVModules />
       </b-tab>
     </b-tabs>
   </div>
@@ -19,11 +12,6 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 @Component
 export default class PSettings extends Vue {
-  async asyncData ({ $axios }: any): Promise<any> {
-    return {
-      modules: (await $axios.$post('/api/opsidata/modulesContent')).result
-    }
-  }
 }
 </script>
 
