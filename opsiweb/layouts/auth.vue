f0136a00f8b100b1fc0451be3eb6154f28486857
Jeena John <j.john@uib.de>
2021-08-03 13:14:33 +0200

    [chg] add themes to auth layout

--- opsiweb/layouts/auth.vue
+++ opsiweb/layouts/auth.vue
@@ -4,3 +4,21 @@
     <BarBBottom />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, namespace, Vue } from 'nuxt-property-decorator'
+const settings = namespace('settings')
+
+@Component
+export default class LayoutAuth extends Vue {
+  @settings.Getter public colortheme!: any
+  head () {
+    return {
+      link: [{
+        rel: 'stylesheet',
+        href: (this.colortheme) ? this.colortheme.rel : ''
+      }]
+    }
+  }
+}
+</script>
