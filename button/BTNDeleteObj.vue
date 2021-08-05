e77afede9fe91846ebb4143daaec736b8359ca1d
Jeena John <j.john@uib.de>
2021-08-05 17:45:57 +0200

    [chg] hide modal after removing changes

--- button/BTNDeleteObj.vue
+++ button/BTNDeleteObj.vue
@@ -9,9 +9,13 @@ import { Component, Prop, Vue } from 'nuxt-property-decorator'
 @Component
 export default class BTNDeleteObj extends Vue {
   @Prop({ }) item!: object
+  @Prop({ }) hide!: string
   @Prop({ }) deleteFrom!: Array<object>
   deleteObj () {
     this.deleteFrom.splice(this.deleteFrom.indexOf(this.item), 1)
+    if (this.deleteFrom.length === 0) {
+      this.$bvModal.hide(this.hide)
+    }
   }
 }
 </script>
