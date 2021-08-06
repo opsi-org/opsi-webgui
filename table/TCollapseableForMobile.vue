c2553028ba6e4a427ad45cf74b3b4f37397bce16
Anna Sucher <a.sucher@uib.de>
2021-08-06 14:35:44 +0200

    [chg] use collapsable product tables on mobile view

---
+++ table/TCollapseableForMobile.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    v-if="$mq=='mobile'"
+  >
+    <BarBPageHeader
+      navbartype="collapse"
+      :collapsed="visible"
+      :aria-controls="title+'-collapse'"
+      :aria-expanded="visible"
+      :title="title"
+      @click.native="visible = !visible"
+    />
+    <b-collapse :id="title+'-collapse'" v-model="visible" accordion="table-accordion">
+      <TableTTable v-bind="$props" :tabledata="tabledata">
+        <template
+          v-for="slotName in Object.keys($scopedSlots)"
+          #[slotName]="slotScope"
+        >
+          <slot :name="slotName" v-bind="slotScope" />
+        </template>
+      </TableTTable>
+
+      <slot name="pagination" />
+    </b-collapse>
+  </div>
+  <div v-else>
+    <TableTTable v-bind="$props" :tabledata="tabledata">
+      <template
+        v-for="slotName in Object.keys($scopedSlots)"
+        #[slotName]="slotScope"
+      >
+        <slot :name="slotName" v-bind="slotScope" />
+      </template>
+    </TableTTable>
+    <slot name="pagination" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'nuxt-property-decorator'
+import TTable from '~/components/table/TTable.vue'
+import { ITableData } from '~/types/ttable'
+
+@Component
+export default class TCollapseable extends TTable {
+  @Prop({ }) title!: string
+  @Prop({ }) tabledata!: ITableData
+  visible: Boolean = true
+}
+
+</script>
