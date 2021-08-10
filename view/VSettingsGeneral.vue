e511ab1c39d46949a80e5e90366ca7fd7a13c480
Jeena John <j.john@uib.de>
2021-08-10 17:52:42 +0200

    [new] add general settings

---
+++ view/VSettingsGeneral.vue
@@ -0,0 +1,25 @@
+<template>
+  <DivDScrollResult>
+    <template slot="content">
+      <b-row class="mb-2">
+        <b-col sm="3" class="text-sm-right">
+          {{ $t('settingsPage.theme') }}:
+        </b-col>
+        <b-col>
+          <DropdownDDTheme />
+        </b-col>
+      </b-row>
+    </template>
+  </DivDScrollResult>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+export default class VSettingsGeneral extends Vue {
+}
+</script>
+
+<style>
+
+</style>
