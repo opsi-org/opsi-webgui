6c4c263a70bfc39f751ed571c4493931552c1d20
Jeena John <j.john@uib.de>
2021-08-20 18:01:32 +0200

    [fix] Remove deleted client from selection store

--- opsiweb/components/modal/MDeleteClient.vue
+++ opsiweb/components/modal/MDeleteClient.vue
@@ -28,8 +28,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'nuxt-property-decorator'
-
+import { Component, namespace, Prop, Vue } from 'nuxt-property-decorator'
+const selections = namespace('selections')
 interface DeleteClient {
   clientid: string
 }
@@ -39,6 +39,8 @@ interface DeleteClient {
   deleteClient: DeleteClient = {
     clientid: ''
   }
+
+  @selections.Mutation public delFromSelectionClients!: (s: string) => void
 
   makeToast (message:string = '', title:any = '', variant:string = 'primary', autoHideDelay: number = 5000, append = false) {
     this.$bvToast.toast(message, {
@@ -57,6 +59,7 @@ interface DeleteClient {
         // eslint-disable-next-line no-console
         console.error(response)
         this.makeToast(this.id + this.$t('message.deleteMessage'), this.$t('message.success'), 'success', 5000)
+        this.delFromSelectionClients(this.id)
       }).catch((error) => {
         this.makeToast((this as any).$t('message.errortext'), this.$t('message.error'), 'danger', 8000)
         // eslint-disable-next-line no-console
