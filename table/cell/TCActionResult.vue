9c01d7b4a8bd12fa63c446653d084f169c1a7746
Anna Sucher <a.sucher@uib.de>
2021-08-20 13:33:56 +0200

    [chg] add icons for product installationStatus and actionResult

---
+++ table/cell/TCActionResult.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-badge v-if="text=='mixed'" variant="warning">
+      &#8800;
+    </b-badge>
+    <b-badge v-else-if="text=='successful'" variant="success">
+      <b-icon icon="check" alt="successful" />
+    </b-badge>
+    <b-badge v-else-if="text=='failed'" variant="danger">
+      <b-icon icon="x" alt="failed" />
+    </b-badge>
+    <b-badge v-else-if="text=='none'" />
+    <b-badge v-else>
+      {{ text }}
+    </b-badge>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class TCSpan extends Vue {
+  @Prop({ }) text!: string
+}
+</script>
+
+<style scoped>
+</style>
