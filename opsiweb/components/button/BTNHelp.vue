d8a4af3f593a58eb9b05e04fc2ee39b39953c9c7
Jeena John <j.john@uib.de>
2021-08-23 13:34:28 +0200

    [new] Add Help Info for new features

---
+++ opsiweb/components/button/BTNHelp.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-button :id="id" size="sm" pill variant="light">
+    <b-icon-question />
+  </b-button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+@Component
+export default class BTNHelp extends Vue {
+  @Prop({ }) id!: string
+}
+</script>
+
+<style>
+
+</style>
