25913194e76e7fc577e9c8de497dedabc4c76e7b
Anna Sucher <a.sucher@uib.de>
2021-08-26 11:14:41 +0200

    [chg] set dynamic size of logo and text

--- opsiweb/components/icon/IOpsiLogo.vue
+++ opsiweb/components/icon/IOpsiLogo.vue
@@ -1,6 +1,15 @@
 <template>
-  <img src="../../assets/images/LogoOpsi.png" alt="opsi logo">
+  <img src="../../assets/images/LogoOpsi.png" alt="opsi logo" :height="height" :width="width">
 </template>
 
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class NSidebar extends Vue {
+  @Prop({ default: '60%' }) height!: string
+  @Prop({ default: '61%' }) width!: string
+}
+</script>
 <style>
 </style>
