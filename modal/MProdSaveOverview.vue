7b297d291bf94b889f4a05c30ac3446fa335f3fa
Jeena John <j.john@uib.de>
2021-09-16 17:24:38 +0200

    [new] add new type ChangeObj

--- modal/MProdSaveOverview.vue
+++ modal/MProdSaveOverview.vue
@@ -30,13 +30,15 @@
 import { Component, namespace, Vue } from 'nuxt-property-decorator'
 import { makeToast } from '@/mixins/toast'
 import { IObjectString2String } from '~/types/tsettings'
+import { ChangeObj } from '~/types/tchanges'
 const changes = namespace('changes')
+
 @Component
 export default class MProdSaveOverview extends Vue {
-  @changes.Getter public changesProducts!: Array<object>
+  @changes.Getter public changesProducts!: Array<ChangeObj>
   @changes.Mutation public delFromChangesProducts!: (s: object) => void
 
-  async save (item: any) {
+  async save (item: ChangeObj) {
     const change = {
       clientIds: [item.clientId],
       productIds: [item.productId],
