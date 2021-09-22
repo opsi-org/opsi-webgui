fc00bac3ce68c2eb20780848835e8ebfdd7ae0d0
Jeena John <j.john@uib.de>
2021-09-22 12:56:44 +0200

    [new] use treeselect for depotids selection

--- opsiweb/pages/depotsconfig.vue
+++ opsiweb/pages/depotsconfig.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <ViewVConfig :id="id" :as-child="false">
       <template slot="IDSelection">
-        <SelectSDepotIds :id.sync="id" />
+        <TreeTSDepotsSelect :id.sync="id" />
       </template>
     </ViewVConfig>
   </div>
