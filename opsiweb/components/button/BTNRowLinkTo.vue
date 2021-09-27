9e302f58cd1a29d3b1f391282d3660640890f7ce
Jeena John <j.john@uib.de>
2021-09-27 13:09:58 +0200

    [chg] no border for row components

--- opsiweb/components/button/BTNRowLinkTo.vue
+++ opsiweb/components/button/BTNRowLinkTo.vue
@@ -1,7 +1,7 @@
 <template>
   <b-button
     variant="outline-primary"
-    class="Btn_Row_action"
+    class="border-0"
     :title="title"
     size="sm"
     :pressed="pressed(to, ident)"
@@ -25,9 +25,3 @@ export default class BTNRowLinkTo extends Vue {
   @Prop({ }) icon!: string
 }
 </script>
-
-<style>
-.Btn_Row_action .btn {
-  border: none;
-}
-</style>
