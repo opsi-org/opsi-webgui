4545e0b0abf071bf990d24b5a86b23f5997a1ff6
Anna Sucher <a.sucher@uib.de>
2021-09-29 16:57:49 +0200

    [fix] add details component and add error handling

---
+++ opsiweb/components/icon/IDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-badge
+    v-bind="$props"
+    :variant="variant"
+  >
+    {{ content==='unequal'? '&ne;':content }}
+  </b-badge>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class IDetails extends Vue {
+  @Prop({ default: '*' }) content?: string
+  @Prop({ default: 'warning' }) variant?: string
+}
+</script>
+
+<style>
+</style>
