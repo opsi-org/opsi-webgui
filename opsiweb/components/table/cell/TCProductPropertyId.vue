417e0b2aa689470232a3364539bb0f8550fe0b9c
Anna Sucher <a.sucher@uib.de>
2021-10-01 12:57:50 +0200

    [chg] refactor

--- opsiweb/components/table/cell/TCProductPropertyId.vue
+++ opsiweb/components/table/cell/TCProductPropertyId.vue
@@ -24,41 +24,6 @@
       :title="`product-version only on depots: ${Object.keys(row.item.depots)}`"
       content="*"
     />
-    <!-- </template> -->
-    <!-- <template #content>
-
-      </template> -->
-    <!-- </CollapseCContent> -->
-    <!-- <TooltipTTTooltip
-      variant="dark"
-      triggers="click"
-      classes="TCPPropertyId_TT"
-      :target="`TProductProperties_PropertyId_hover_${row.item.propertyId}`"
-    >
-      <template #tooltip>
-        Defaults: <b v-if="row.item.default!='mixed'">[{{ row.item.details }}]</b>
-        <div v-else>
-          <p v-for="v,k in row.item.defaultDetails" :key="k">
-            {{ k }}: <b>{{ v }}</b>
-          </p>
-        </div>
-        <br>
-        <div v-if="row.item.anyDepotDifferentFromDefault">
-          Depots:
-          <p v-for="v,k in row.item.depots" :key="k">
-            {{ k }}: <b>{{ v }}</b>
-          </p>
-        </div>
-        <br>
-        Description: <b v-if="row.item.description!='mixed'">{{ row.item.description }}</b>
-        <div v-else>
-          <p v-for="v,k in row.item.descriptionDetails" :key="k">
-            {{ k }}: <b>{{ v }}</b>
-          </p>
-        </div>
-      </template>
-    </TooltipTTTooltip> -->
-    <!-- product on each depot, but property not on every depot-->
   </div>
 </template>
 
@@ -69,7 +34,7 @@ import { ITableRowProperty } from '~/types/ttable'
 const selections = namespace('selections')
 
 @Component
-export default class TCSpan extends Vue {
+export default class TCProductPropertyId extends Vue {
   @Prop({ }) row!: ITableRowProperty
   @Prop({ }) productVersions!: IObjectString2StringOrUndefined
 
@@ -79,24 +44,4 @@ export default class TCSpan extends Vue {
 </script>
 
 <style>
-/* .TCPPropertyId_TT {
-  max-width: 50% !important;
-  width: auto !important;
-}
-.TCPPropertyId_TT  .tooltip-inner {
-    text-align: left !important;
-    text-align: left !important;
-    width: inherit;
-    max-width: 300px !important;
-    word-wrap: inherit;
-    min-width: inherit !important;
-}
-.TCPPropertyId_TT p {
-  margin-left: 10px !important;
-  margin-bottom: 0 !important;
-} */
-/* [id^='pp_row-'] {
-  max-width: 100px;
-  overflow: visible;
-} */
 </style>
