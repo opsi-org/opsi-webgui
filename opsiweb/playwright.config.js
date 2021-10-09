bd8dd534394c1bf27369f6214559485330e5cc7d
Anna Sucher <a.sucher@uib.de>
2021-10-09 20:05:37 +0200

    [fix] e2e login and logout

--- opsiweb/playwright.config.js
+++ opsiweb/playwright.config.js
@@ -4,6 +4,7 @@
 const config = {
   testIgnore: 'test/unit/**/*.spec.js',
   use: {
+    browserName: 'firefox',
     baseURL: 'https://localhost:8888/webgui/app/',
     // headless: false,
     // viewport: { width: 1280, height: 720 },
--- opsiweb/test/e2e/LoginLogout.spec.js
+++ opsiweb/test/e2e/LoginLogout.spec.js
@@ -1,19 +1,52 @@
 const { test, expect } = require('@playwright/test')
-const apiMock = (page, apiPath, response) => page.route(apiPath, route => route.fulfill({
-  status: 200,
-  headers: { 'access-control-allow-origin': '*' },
-  contentType: 'application/json',
-  body: JSON.stringify(response)
-}))
+const apiMock = (page, apiPath, response, withCookie = true) => {
+  page.route(apiPath, route => route.fulfill({
+    status: 200,
+    headers: {
+      'access-control-allow-origin': 'https://localhost:8888',
+      'access-control-allow-credentials': true,
+      'access-control-allow-headers': '*',
+      'access-control-allow-methods': '*'
+      // 'set-cookie': (!withCookie) ? '' : 'opsiconfd-session=any-value; domain=localhost; path=/; sameSite=None; secure=true'
+    },
+    contentType: 'application/json',
+    body: JSON.stringify(response)
+  }))
+}
+
+const c = [{
+  name: 'opsiconfd-session',
+  value: 'any-value',
+  domain: 'localhost',
+  path: '/',
+  expires: -1,
+  httpOnly: false,
+  secure: true,
+  sameSite: 'None'
+}]
 
 test.beforeEach(async ({ page }) => {
-  const apiServerPath = '**/api/user/opsiserver'
-  await page.unroute(apiServerPath)
-  await apiMock(page, apiServerPath, { result: 'mydepot.uib.local' })
+  page.on('console', m => console.log(m.text()))
+  page.on('requestfailed', request => (!request.url().includes('localhost')) ? '' : console.log(`>>f ${request.method()} ${request.url()} ${JSON.stringify(request.failure())}`))
+  page.on('request', request => {
+    if (request.url().includes('localhost')) {
+      console.log(`>> ${request.method()} ${request.url()}`)
+    }
+  })
+  page.on('response', async response => {
+    if (response.url().includes('4447')) {
+      let txt = (await response.body()).toString()
+      console.log(`<< ${response.status()} ${response.url()} ${txt}`)
+    }
+  })
+  await page.unroute('**/api/**')
+  await apiMock(page, '**/api/user/opsiserver', { result: 'mydepot.uib.local' })
+  await apiMock(page, 'https://localhost:4447/webgui/api/auth/login', { result: 'Login success' })
+  await apiMock(page, 'https://localhost:4447/webgui/api/auth/logout', { result: 'logout success' })
   await page.goto('./login')
 })
 
-test('login and logout again', async ({ page }) => {
+test('should be possible to login and logout again', async ({ page, context }) => {
   // await page.goto('./login')
   title = page.locator('.login_title')
   await expect(title).toHaveText('OPSIWEB')
@@ -25,11 +58,17 @@ test('login and logout again', async ({ page }) => {
   await page.press('[placeholder="Username"]', 'Tab')
   await page.fill('[placeholder="Password"]', 'adminuser')
   await page.press('[placeholder="Password"]', 'Enter')
-  await apiMock(page, '**/api/auth/login', { result: 'Login success' })
-  await page.waitForNavigation({ url: './' })
 
+  await context.addCookies(c)
+  title = await context.cookies()
+  expect(title).toEqual(c)
+
+  await page.waitForNavigation({ url: './' })
+  title = page.locator('[data-testid="btn-logout"]')
+  await expect(title).toHaveText('Logout')
+
+  await apiMock(page, '**/api/auth/logout', { result: 'logout success' }, 'POST')
   await page.click('[data-testid="btn-logout"]')
-  await apiMock(page, '**/api/auth/logout', { result: 'logout success' })
   await page.waitForNavigation({ url: './login' })
   title = page.locator('.login_title')
   await expect(title).toHaveText('OPSIWEB')
