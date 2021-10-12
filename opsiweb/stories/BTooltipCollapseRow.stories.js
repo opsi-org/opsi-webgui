aa4d95d258694d9a7d84846c9d0d5646e0ab5c34
Anna Sucher <a.sucher@uib.de>
2021-10-12 16:29:22 +0200

    [chg] add story+snap btooltiprow

---
+++ opsiweb/stories/BTooltipCollapseRow.stories.js
@@ -0,0 +1,32 @@
+import { argTypeBoolFalse, argTypeBoolTrue, argTypeTextContent, argTypeTextTitle, argTypeVariants } from '~/scripts/types/ttestconsts'
+
+export default {
+  title: 'bar/B Tooltip Collapse Row',
+  parameters: { docs: { description: { component: 'Bar/BTooltipCollapseRow description' } } },
+  argTypes: {
+    title: argTypeTextTitle,
+    value: argTypeTextContent,
+    valueVariant: argTypeVariants,
+    collapseable: argTypeBoolTrue,
+    collapsed: argTypeBoolFalse
+  }
+}
+
+const DefaultVisibleTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  template: ` <BarBTooltipCollapseRow
+  :title="${args.title}||$props.title"
+  :value="${args.value}||$props.value"
+  :collapsed="${args.collapsed}||$props.collapsed"
+  :collapseable="${args.collapseable}||$props.collapseable"
+  :value-variant="${args.valueVariant}||$props.valueVariant"
+>
+  `
+})
+
+export const BTooltipCollapseRow = DefaultVisibleTemplate.bind({})
+// BTooltipCollapseRow.args = {
+//   totalRows: 20,
+//   pageNumber: 1,
+//   perPage: 10
+// }
