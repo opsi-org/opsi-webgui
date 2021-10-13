913fa8d4bdc6fa49377cb04122ff2963077676d7
Anna Sucher <a.sucher@uib.de>
2021-10-13 14:39:55 +0000

    [chg] storiy+snap btnhelptooltip

--- opsiweb/components/button/BTNHelpTooltip.vue
+++ opsiweb/components/button/BTNHelpTooltip.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <b-button :id="id" size="sm" pill variant="transparent" class="border-primary">
+    <b-button
+      :id="id"
+      data-testid="BTNHelpTooltip"
+      class="border-primary"
+      variant="transparent"
+      size="sm"
+      pill
+    >
       <b-icon-question />
     </b-button>
-    <TooltipTTTooltip variant="info" :target="id" :tooltip="tooltip" />
+    <TooltipTTTooltip variant="info" :target="id" :triggers="triggers" :tooltip="tooltip" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
 @Component
-export default class BTNHelp extends Vue {
+export default class BTNHelpTooltip extends Vue {
   @Prop({ }) id!: string
   @Prop({ }) tooltip!: string
+  @Prop({ default: 'hover' }) triggers!: string
 }
 </script>
