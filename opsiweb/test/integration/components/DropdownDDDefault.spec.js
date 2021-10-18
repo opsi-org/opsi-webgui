65918305d78c8a39acf5deff18ec93511b438639
Anna Sucher <a.sucher@uib.de>
2021-10-18 15:01:53 +0200

    [chg] threshold for ddefault

--- opsiweb/test/integration/components/DropdownDDDefault.spec.js
+++ opsiweb/test/integration/components/DropdownDDDefault.spec.js
@@ -12,7 +12,7 @@ test('dropdown ddefault single not origin snapshot', async ({ page }) => {
   await page.click('[data-testid="DropdownDDDefault-Button"]')
   await page.click('[data-testid="DropdownDDDefault-Item-b"]')
   // const component = await page.locator('[data-testid="DropdownDDDefault"]')
-  expect(await page.screenshot()).toMatchSnapshot('dropdown-dddefault-single-not-origin.png')
+  expect(await page.screenshot()).toMatchSnapshot('dropdown-dddefault-single-not-origin.png', { threshold: 0.2 })
 })
 
 test('dropdown ddefault multi origin snapshot', async ({ page }) => {
@@ -27,5 +27,5 @@ test('dropdown ddefault multi not origin snapshot', async ({ page }) => {
   await page.click('[data-testid="DropdownDDDefault-Item-c"]')
   await page.click('[data-testid="DropdownDDDefault-Item-a"]')
   // const component = await page.locator('[data-testid="DropdownDDDefault"]')
-  expect(await page.screenshot()).toMatchSnapshot('dropdown-dddefault-multi-not-origin.png')
+  expect(await page.screenshot()).toMatchSnapshot('dropdown-dddefault-multi-not-origin.png', { threshold: 0.2 })
 })
