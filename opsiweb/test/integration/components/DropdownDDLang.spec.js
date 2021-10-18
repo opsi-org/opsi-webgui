e1143d87d817962d3012008912c96f5325036d2b
Anna Sucher <a.sucher@uib.de>
2021-10-18 18:44:12 +0200

    [chg] snap only component not full page

--- opsiweb/test/integration/components/DropdownDDLang.spec.js
+++ opsiweb/test/integration/components/DropdownDDLang.spec.js
@@ -4,16 +4,14 @@ const { callStoryId } = require('../../.utils-pw/pw-story-call')
 test('dropdown lang snapshot', async ({ page }) => {
   await callStoryId(page, 'dropdown-dd-lang', 'dd-lang')
   const component = await page.locator('[data-testid="DropdownDDLang"]')
-  expect(await component.screenshot()).toMatchSnapshot('dropdown-ddlang.png')
+  expect(await component.screenshot()).toMatchSnapshot('dropdown-ddlang-normal.png')
 })
 
 test('dropdown lang opened snapshot', async ({ page }) => {
-  // TODO: hover does not work (im sure its a playwright problem.. )
-  // test.fail(browserName === 'webkit', 'This feature currently has problems on Mac')
   await callStoryId(page, 'dropdown-dd-lang', 'dd-lang')
   await page.click('[data-testid="DropdownDDLang"]')
-  // await page.click('[data-testid="DropdownDDLang-Item-de"]')
-  expect(await page.screenshot()).toMatchSnapshot('dropdown-ddlang-opened.png')
+  const component = await page.locator('[data-testid="DropdownDDLang"] > ul')
+  expect(await component.screenshot()).toMatchSnapshot('dropdown-ddlang-opened.png')
 })
 
 test('dropdown lang changed snapshot', async ({ page }) => {
