a9aca42c1942e08c8471e683e9fbf3caa2b85fc2
Anna Sucher <a.sucher@uib.de>
2021-10-18 17:18:31 +0200

    [chg] remove test.fail for some browsers

--- opsiweb/test/integration/components/BarBTooltipCollapseRow.spec.js
+++ opsiweb/test/integration/components/BarBTooltipCollapseRow.spec.js
@@ -1,10 +1,8 @@
 const { test, expect } = require('@playwright/test')
 const { callStoryId } = require('../../.utils-pw/pw-story-call')
 
-test('bar btooltipcollapserow snapshot', async ({ page, browserName }) => {
+test('bar btooltipcollapserow snapshot', async ({ page }) => {
   await callStoryId(page, 'bar-b-tooltip-collapse-row', 'b-tooltip-collapse-row')
   const component = await page.locator('[data-testid="BarBTooltipCollapseRow"]')
-  // TODO: chromium and webkit looks different from firefox
-  test.fail(browserName === 'webkit', 'This feature currently has problems on webkit/chromium')
   expect(await component.screenshot()).toMatchSnapshot('bar-btooltipcollapserow.png')
 })
