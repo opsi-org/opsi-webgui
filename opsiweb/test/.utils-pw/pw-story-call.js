9a6f2aad47c555047abef781d46b21cf6324318c
Anna Sucher <a.sucher@uib.de>
2021-10-22 12:50:46 +0200

    [chg] add wait in pw-tests
    Former-commit-id: a5ebbe94def4295ab26b27c78f9c82e4e0c68148

--- opsiweb/test/.utils-pw/pw-story-call.js
+++ opsiweb/test/.utils-pw/pw-story-call.js
@@ -1,3 +1,10 @@
+const wait = function (ms, s) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 module.exports.callStory = (page, path) => page.goto(`http://localhost:3003/${path}`)
-module.exports.callStoryId = (page, fullId, id) => page.goto(`http://localhost:3003/iframe.html?id=${fullId}--${id}&args=&viewMode=story'`)
+module.exports.callStoryId = async (page, fullId, id) => {
+  await page.goto(`http://localhost:3003/iframe.html?id=${fullId}--${id}&args=&viewMode=story'`)
+  return wait(1000, fullId)
+}
 
