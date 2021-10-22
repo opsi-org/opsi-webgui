47a07e70252e9c9f56c021a923644c856d01fe3f
Anna Sucher <a.sucher@uib.de>
2021-10-22 14:08:20 +0200

    [chg] test running tests
    Former-commit-id: 48d24c8641ec51b130ae4888f1c6b4b98df251da

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-echo "install dependencies"
+# echo "[script.sh] install dependencies"
 # npm install
 # echo "setting permissions"
-chown -f -R 1000:1000 /root/.npm
-chown -f -R 1000:1000 /root/.npm/*
-chown -f -R 1000:1000 /root/.cache/
-chown -f -R 1000:1000 /root/.cache/*
-
+# echo "[script.sh] setting permissions"
+# npm install npm@8.1.1
+# npm install -g npm@8.1.1
+# chown -f -R 1000:1000 /root/.npm
+# chown -f -R 1000:1000 /root/.npm/*
+# chown -f -R 1000:1000 /root/.cache/
+# chown -f -R 1000:1000 /root/.cache/*
 
 # su - node <<SHT
-cd /workspace/opsiweb
-# echo "[script.sh] update npm..."
-# npm install -g npm
-
+cd ../opsiweb
 echo "[script.sh] install dependencies..."
 npm install
-# npm audit fix
+npm i -D @playwright/test
 echo "[script.sh] configure playwright..."
 npx playwright install
 npx playwright install-deps
