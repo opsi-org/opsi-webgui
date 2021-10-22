e94c417cc3a9b685afe6f004f4c6d0b3cba6bf46
Jeena John <j.john@uib.de>
2021-10-22 13:02:11 +0200

    [chg] product config type changes

--- opsiweb/scripts/types/tchanges.ts
+++ opsiweb/scripts/types/tchanges.ts
@@ -2,6 +2,8 @@ export interface ChangeObj {
   depotId: string,
   clientId: string,
   productId: string,
+  property: string,
+  propertyValue: Array<boolean|string>
   type: string,
   actionRequest: string
 }
