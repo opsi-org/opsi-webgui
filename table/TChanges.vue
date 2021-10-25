6b9d0142b3e69636d814012b579cabadc91b812d
Jeena John <j.john@uib.de>
2021-10-25 17:24:43 +0200

    [new] notify after product property saving

--- table/TChanges.vue
+++ table/TChanges.vue
@@ -122,7 +122,7 @@ export default class TChanges extends Vue {
       .then((response) => {
         // eslint-disable-next-line no-console
         console.log(response)
-        makeToast(t, this.$t('message.prodPropSave') as string, this.$t('message.success') as string, 'success')
+        makeToast(t, 'Product Property ' + JSON.stringify(change) + ' saved succefully', this.$t('message.success') as string, 'success')
         this.delFromChangesProducts(item)
       }).catch((error) => {
         makeToast(t, (error as IObjectString2Any).message, this.$t('message.error') as string, 'danger', 8000)
