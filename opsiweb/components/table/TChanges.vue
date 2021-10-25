e496e4ed2f8a678ef85f4a222b5e036daf10dc3f
Jeena John <j.john@uib.de>
2021-10-25 12:30:12 +0200

    [new] Save single rowitem for product property changes

--- opsiweb/components/table/TChanges.vue
+++ opsiweb/components/table/TChanges.vue
@@ -46,7 +46,7 @@
 <script lang="ts">
 import { Component, Prop, Watch, namespace, Vue } from 'nuxt-property-decorator'
 import { makeToast } from '@/scripts/utils/scomponents'
-import { IObjectString2String } from '~/scripts/types/tgeneral'
+import { IObjectString2String, IObjectString2Any } from '~/scripts/types/tgeneral'
 import { ChangeObj } from '~/scripts/types/tchanges'
 const changes = namespace('changes')
 
@@ -75,7 +75,7 @@ export default class TChanges extends Vue {
     }, {})
   }
 
-  async save (item: ChangeObj) {
+  async saveProd (item: ChangeObj) {
     const change = {
       clientIds: [item.clientId],
       productIds: [item.productId],
@@ -98,6 +98,50 @@ export default class TChanges extends Vue {
     if (this.changesProducts.length === 0) {
       this.$bvModal.hide('ProductSaveModal')
       this.$nuxt.refresh()
+    }
+  }
+
+  async saveProdProp (item: ChangeObj) {
+    const t:any = this
+    const propObj: any = {}
+    propObj[item.property] = item.propertyValue
+    let change = {}
+    if (item.clientId !== '') {
+      change = {
+        clientIds: item.clientId,
+        properties: propObj
+      }
+    } else {
+      change = {
+        depotIds: item.depotId,
+        properties: propObj
+      }
+    }
+    // eslint-disable-next-line no-console
+    console.log(change)
+    await this.$axios.$post(`/api/opsidata/products/${item.productId}/properties`, { data: change })
+      .then((response) => {
+        // eslint-disable-next-line no-console
+        console.log(response)
+        makeToast(t, this.$t('message.prodPropSave') as string, this.$t('message.success') as string, 'success')
+        this.delFromChangesProducts(item)
+      }).catch((error) => {
+        makeToast(t, (error as IObjectString2Any).message, this.$t('message.error') as string, 'danger', 8000)
+        // eslint-disable-next-line no-console
+        console.error(error)
+      })
+    if (this.changesProducts.length === 0) {
+      this.$bvModal.hide('ProductSaveModal')
+    }
+    this.$nuxt.refresh()
+  }
+
+  save (rowItem: ChangeObj) {
+    const change = rowItem
+    if (change.actionRequest) {
+      this.saveProd(change)
+    } else if (change.property) {
+      this.saveProdProp(change)
     }
   }
 }
