3e10df01971d914e487aec58d94e1bcec0f16de1
Jeena John <j.john@uib.de>
2021-11-09 17:30:23 +0100

    [new] stories for view components
    Former-commit-id: 338c8081aaf1ac51886e3d79b40e3e34975b7a55

--- opsiweb/test/integration/components/ViewVClientsAddNew.spec.js
+++ opsiweb/test/integration/components/ViewVClientsAddNew.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test')
 const { callStoryId } = require('../../.utils-pw/pw-story-call')
 
 test('view vclients addnew snapshot', async ({ page }) => {
-  await callStoryId(page, 'view-v-clientsaddnew', 'v-clientsaddnew')
+  await callStoryId(page, 'view-v-clientsaddnew', 'v-clients-add-new')
   const component = await page.locator('[data-testid="VClientsAddNew"]')
   expect(await component.screenshot()).toMatchSnapshot('view-vclientsaddnew.png')
 })
