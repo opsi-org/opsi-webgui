859c2e833eddc5c9f63d895c91c6694773dc3576
Jeena John <j.john@uib.de>
2021-11-10 15:23:48 +0100

    [new] Button for clear selected clients
    Former-commit-id: ef4c524475ed9e9d75f1fdf25e9dfc0f93284cce

---
+++ opsiweb/components/button/BTNClearSelectedClients.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-button size="sm" variant="outline-primary" @click="clearSelected()">
+    <b-icon icon="trash" /> Clear Selected Clients
+  </b-button>
+</template>
+
+<script lang="ts">
+import { Component, namespace, Vue } from 'nuxt-property-decorator'
+const changes = namespace('changes')
+@Component
+export default class BTNDeleteAll extends Vue {
+  @changes.Getter public changesProducts!: Array<object>
+  @changes.Mutation public deleteAllChanges!: () => void
+
+  // clearSelected () {
+  //   this.deleteAllChanges()
+  //   this.$nuxt.refresh()
+  // }
+}
+</script>
