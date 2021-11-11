744dbcfd90d3af05b8011991b04d298410d65351
Anna Sucher <a.sucher@uib.de>
2021-11-11 16:02:48 +0000

    [chg] moving integration tests to component

---
+++ button/BTNInfo.test.integration.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test')
+const { callStoryId } = require('../.utils/playwright/pw-story-call')
+
+test('button info snapshot', async ({ page }) => {
+  await callStoryId(page, 'button-btn-info', 'btn-info')
+  const component = await page.locator('[data-testid="ButtonBTNInfo"]')
+  expect(await component.screenshot()).toMatchSnapshot('button-btninfo.png')
+})
