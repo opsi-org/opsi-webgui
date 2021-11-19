f2b0ae739744ffd252ee4f2b890ce7df110410cf
Jeena John <j.john@uib.de>
2021-11-19 16:32:44 +0100

    [new] add new switch checkbox

---
+++ checkbox/CBMultiselection.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-form-checkbox v-model="checked" switch @change="$emit('update:multiselect', checked)">
+    Multiselection
+  </b-form-checkbox>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class CBMultiSelection extends Vue {
+  checked: boolean = false
+}
+</script>
