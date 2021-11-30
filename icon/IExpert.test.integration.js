dd5ad88ca3daad9d5b8fa145562ab902ff3a82dd
Jeena John <j.john@uib.de>
2021-11-30 16:21:24 +0100

    [chg] test desc changes

--- icon/IExpert.test.integration.js
+++ icon/IExpert.test.integration.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test')
 const { callStoryId } = require('../.utils/playwright/pw-story-call')
 
-test('icon deatils snapshot', async ({ page }) => {
+test('icon-expert snapshot', async ({ page }) => {
   await callStoryId(page, 'icon-i-expert', 'i-expert')
   const component = await page.locator('[data-testid="IExpert"]')
   expect(await component.screenshot()).toMatchSnapshot('icon-iexpert.png')
