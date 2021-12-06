dd7715d83a40e8701378ccad5d6419883fedfc0a
Jeena John <j.john@uib.de>
2021-12-06 17:49:28 +0100

    [new] client products page
    Former-commit-id: 7a8e1dbc13c6cda6c936b97ee4381c7040f9db35

---
+++ opsiweb/pages/clients/products.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <BarBPageHeader :title="'Products - ' + id" closeroute="/clients/" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+@Component
+export default class PClientsProductsColumn extends Vue {
+  @Prop({ }) id!: string
+}
+</script>
