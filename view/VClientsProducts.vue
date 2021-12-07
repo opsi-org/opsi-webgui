2ebf310c2c82d5b589c9725eb566ce33fef7554d
Jeena John <j.john@uib.de>
2021-12-07 16:28:07 +0100

    [new] add client sub view for products

---
+++ view/VClientsProducts.vue
@@ -0,0 +1,20 @@
+<template>
+  <div data-testid="VProducts">
+    <BarBPageHeader>
+      <template #left>
+        <TreeTSProductGroup />
+      </template>
+    </BarBPageHeader>
+    <TableTProductsNetboot :rowident="id" />
+    <TableTProductsLocalboot :rowident="id" />
+    <ButtonBTNClearSelection store="products" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+@Component
+export default class VClientsProducts extends Vue {
+  @Prop({ }) id!: string
+}
+</script>
