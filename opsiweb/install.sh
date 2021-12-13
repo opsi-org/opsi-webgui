31ccac2b673a05a6e83f88d19c05b24cc9f76f5d
Anna Sucher <a.sucher@uib.de>
2021-12-13 12:57:23 +0000

    [chg] update ci
    Former-commit-id: a9f30f5fe1fd75accc7eeacac4a52decd0fd5943

--- opsiweb/install.sh
+++ opsiweb/install.sh
@@ -1,9 +1,12 @@
 
 #!/bin/bash
-echo "[install.sh] install dry..."
+# RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+printf "${ORANGE}[install.sh] install dry...${NC}\n"
 npm i -g dry-dry
-echo "[install.sh] merge package-json files..."
+printf "${ORANGE}[install.sh] merge package-json files...${NC}\n"
 dry i --dry-save-package-json-to package-merged.json --dry-keep-package-json
-echo "[install.sh] install playwright..."
+printf "${ORANGE}[install.sh] install playwright...${NC}\n"
 dry i -D @playwright/test
 npx playwright install
