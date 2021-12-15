9ee7be4b245a426502f22213641fca727e6379f5
Jeena John <j.john@uib.de>
2021-12-15 14:21:15 +0100

    [chg] countdown timer

--- badge/BCountdowntimer.vue
+++ badge/BCountdowntimer.vue
@@ -1,7 +1,6 @@
 <template>
   <b-badge>
-    Expires in
-    <!-- {{ countdowntime }} -->
+    {{ countdowntime }}
   </b-badge>
 </template>
 
@@ -14,24 +13,26 @@ export default class BCountdowntimer extends Vue {
   @auth.Mutation public logout!: () => void
   @auth.Mutation public clearSession!: () => void
 
-  // get countdowntime () {
-  //   return setInterval(() => { this.countdown() }, 1000)
-  // }
+  get countdowntime () {
+    // return this.countdown()
+    return setInterval(() => { this.countdown() }, 1000)
+  }
 
   countdown () {
-    const expiry = JSON.parse(Cookie.get('X-opsi-session-lifetime'))
-    const countdownEnd = expiry.ExpiresIn
+    const expiry = Cookie.get('X-opsi-session-lifetime')
+    const countdownEnd = expiry as unknown as number
     const countdownStart = new Date().getTime()
     const diff = countdownEnd - countdownStart
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    // const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    // const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
     const seconds = Math.floor((diff % (1000 * 60)) / 1000)
     if (diff < 0) {
       this.logout()
       this.clearSession()
       return 'EXPIRED'
-    } else { return days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's' }
+      // return days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's'
+    } else { return 'Expires in' + minutes + 'm ' + seconds + 's' }
   }
 }
 </script>
