3c2c1e89a0ecd579f9327a2ba0c1ef30c6b4a4a0
Jeena John <j.john@uib.de>
2021-12-15 18:27:39 +0100

    [new] add notification in 5 minutes

--- badge/BCountdowntimer.vue
+++ badge/BCountdowntimer.vue
@@ -1,16 +1,18 @@
 <template>
-  <b-badge>
+  <div>
     {{ countdowntimer }}
-  </b-badge>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, namespace, Vue } from 'nuxt-property-decorator'
 import Cookie from 'js-cookie'
+import { makeToast } from '@/scripts/utils/scomponents'
 const auth = namespace('auth')
 @Component
 export default class BCountdowntimer extends Vue {
   countdowntimer : string = ''
+  notify: boolean = false
   @auth.Mutation public logout!: () => void
   @auth.Mutation public clearSession!: () => void
 
@@ -22,6 +24,12 @@ export default class BCountdowntimer extends Vue {
     const timeinterval = setInterval(() => {
       const t = this.getRemainingTime()
       this.countdowntimer = 'Expires in ' + t.days + 'd ' + t.hours + 'h ' + t.minutes + 'm ' + t.seconds + 's'
+      const notifyInMinutes = 5
+      const notifyInMilliSec = notifyInMinutes * 60000
+      if (t.diff <= notifyInMilliSec && !this.notify) {
+        this.notify = true
+        makeToast(this, 'Session expires in ' + notifyInMinutes + ' minutes', 'Session Timeout', 'warning', notifyInMilliSec)
+      }
       if (isNaN(t.diff)) {
         this.countdowntimer = 'EXPIRED'
         clearInterval(timeinterval)
@@ -34,12 +42,10 @@ export default class BCountdowntimer extends Vue {
   getRemainingTime () {
     const endtime = Cookie.get('X-opsi-session-lifetime')
     const diff = Date.parse(endtime as unknown as string) - Date.parse(new Date() as unknown as string)
-    console.log(diff)
     const seconds = Math.floor((diff / 1000) % 60)
     const minutes = Math.floor((diff / 1000 / 60) % 60)
     const hours = Math.floor((diff / (1000 * 60 * 60)) % 24)
     const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-
     return { diff, days, hours, minutes, seconds }
   }
 }
