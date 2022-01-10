74b221c7e74d1da2e64f014e098bd0ba29313490
Anna Sucher <a.sucher@uib.de>
2022-01-10 17:17:35 +0000

    [fix] lint

--- components/view/VProductProperty.stories.js
+++ components/view/VProductProperty.stories.js
@@ -2,10 +2,10 @@
 import withMock from 'storybook-addon-mock'
 const { mockBackendCall } = require('../../.utils/storybook/mock')
 const dependencies = {
-  dependencies:[{"productId":"productId1", productAction:"setup", version:"4.1.1.14-3", requiredProductId:"l-system-update", requiredVersion:null, requiredAction:"setup", requiredInstallationStatus:null, requirementType:"before"}],
-  productVersions:{ clientId1:"4.1.1.14-3"},
-  productDescriptionDetails:{ clientId1:"Installs opsi-server packages, configures and create adminuser"},
-  productDescription:"Installs opsi-server packages, configures and create adminuser"
+  dependencies: [{ productId: 'productId1', productAction: 'setup', version: '4.1.1.14-3', requiredProductId: 'l-system-update', requiredVersion: null, requiredAction: 'setup', requiredInstallationStatus: null, requirementType: 'before' }],
+  productVersions: { clientId1: '4.1.1.14-3' },
+  productDescriptionDetails: { clientId1: 'Installs opsi-server packages, configures and create adminuser' },
+  productDescription: 'Installs opsi-server packages, configures and create adminuser'
 }
 const properties = {
   properties: {
@@ -24,8 +24,8 @@ export default {
 
 const PrimaryTemplate = (_args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  template: `<ViewVProductProperty :id="$props.id" :asChild="$props.asChild" :closeroute="$props.closeroute"/>
-  `,
+  template: `<ViewVProductProperty :id='$props.id' :asChild='$props.asChild' :closeroute='$props.closeroute'/>
+  `
 })
 
 export const VProductProperty = PrimaryTemplate.bind({})
