6b155ccc2099dbab9cf2300196c5e9ae28324a9d
Anna Sucher <a.sucher@uib.de>
2022-01-17 12:39:39 +0000

    [chg] add cloning subrepo automaticly
    Former-commit-id: effef2e7509f3fd70f9605ef07ad46b0b6aea31c

--- .devcontainer/devenv.sh
+++ .devcontainer/devenv.sh
@@ -9,10 +9,9 @@ else
 	echo DEV_USER=$USER >> .env
 fi
 
-# TODO: Update this
-# git clone git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git opsiweb/components
+git clone --branch=$(git branch --show-current) git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git opsiweb/uib-components
 # echo ""
 # echo "Attention! "
 # echo "Please check if opsiweb/components directory is filled. "
 # echo "Otherwise clone it with e.g. "
-# echo "\tgit clone git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git opsiweb/components"
+# echo "\tgit clone --branch=$(git branch --show-current) git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git opsiweb/uib-components"
