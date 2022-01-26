deb2726e65bd8db0cbbb92e42490e8d74511df87
Jeena John <j.john@uib.de>
2022-01-26 15:38:53 +0100

    [chg] opsi doku as iframe in opsiweb
    Former-commit-id: 87f302df37764ce2ca0c7c53d31e5ede13190342

--- opsiweb/ui/pages/support.vue
+++ opsiweb/ui/pages/support.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="container-fluid">
+    <b-container fluid>
+      <b-card>
+        <b-card-body>
+          <iframe
+            :src="`https://docs.opsi.org/opsi-docs-de/4.2/index.html`"
+            width="100%"
+            height="700"
+            frameborder="0"
+          />
+        </b-card-body>
+      </b-card>
+    </b-container>
     <span v-for="item in supportItems" :key="item.title">
       <CardCSupport :item="item" />
     </span>
@@ -13,12 +25,12 @@ import { Component, Vue } from 'nuxt-property-decorator'
 export default class PageSupport extends Vue {
   get supportItems (): Array<object> {
     return [
-      {
-        title: 'supportPage.documentation.title',
-        description: 'supportPage.documentation.description',
-        buttonname: 'supportPage.documentation.button',
-        link: this.$i18n.locale === 'en' ? 'https://www.uib.de/en/opsi-documentation/documentation' : 'https://www.uib.de/de/opsi-dokumentation/dokumentationen'
-      },
+      // {
+      //   title: 'supportPage.documentation.title',
+      //   description: 'supportPage.documentation.description',
+      //   buttonname: 'supportPage.documentation.button',
+      //   link: this.$i18n.locale === 'en' ? 'https://www.uib.de/en/opsi-documentation/documentation' : 'https://www.uib.de/de/opsi-dokumentation/dokumentationen'
+      // },
       {
         title: 'supportPage.forum.title',
         description: 'supportPage.forum.description',
