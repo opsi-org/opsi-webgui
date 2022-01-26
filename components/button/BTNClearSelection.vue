1ac9c46142e3aa8cb84dc801e0ffec3de5323c86
Jeena John <j.john@uib.de>
2022-01-26 15:12:40 +0100

    [fix] add selection watchers for reset

--- components/button/BTNClearSelection.vue
+++ components/button/BTNClearSelection.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <template v-if="component === 'tree'">
-      <b-button size="sm" variant="light" @click="clearSelected()">
+      <b-button v-if="visible" size="sm" variant="light" @click="clearSelected()">
         <b-icon icon="x" />
       </b-button>
     </template>
     <template v-else>
-      <b-button size="sm" variant="outline-primary" @click="clearSelected()">
+      <b-button v-if="visible" size="sm" variant="outline-primary" @click="clearSelected()">
         Clear Selection
       </b-button>
     </template>
@@ -14,42 +14,49 @@
 </template>
 
 <script lang="ts">
-import { Component, namespace, Prop, Vue } from "nuxt-property-decorator";
-const selections = namespace("selections");
+import { Component, namespace, Watch, Prop, Vue } from 'nuxt-property-decorator'
+const selections = namespace('selections')
 @Component
 export default class BTNDeleteAll extends Vue {
   @Prop() store?: string;
   @Prop() component?: string;
-  // visible: Boolean = false
+  visible: Boolean = false
   // @selections.Getter public selectionDepots!: Array<string>
-  // @selections.Getter public selectionClients!: Array<strwatching>
-  // @selections.Getter public selectionProducts!: Array<string>
-  @selections.Mutation public setSelectionDepots!: (s: Array<string>) => void;
+  @selections.Getter public selectionClients!: Array<string>
+  @selections.Getter public selectionProducts!: Array<string>
+  // @selections.Mutation public setSelectionDepots!: (s: Array<string>) => void;
   @selections.Mutation public setSelectionClients!: (s: Array<string>) => void;
   @selections.Mutation public setSelectionProducts!: (s: Array<string>) => void;
-  // mounted () {
-  //   if (this.store === 'depots') {
-  //     if (this.selectionDepots.length > 0) {
-  //       this.visible = true
-  //     }
-  //   } else if (this.store === 'clients') {
-  //     if (this.selectionClients.length > 0) {
-  //       this.visible = true
-  //     }
-  //   } else if (this.store === 'products') {
-  //     if (this.selectionProducts.length > 0) {
-  //       this.visible = true
-  //     }
-  //   }
-  // }
 
-  clearSelected() {
-    if (this.store === "depots") {
-      this.setSelectionDepots([]);
-    } else if (this.store === "clients") {
-      this.setSelectionClients([]);
-    } else if (this.store === "products") {
-      this.setSelectionProducts([]);
+  @Watch('selectionClients', { deep: true }) selectionClientsChanged () {
+    if (this.store === 'clients') {
+      if (this.selectionClients.length > 0) {
+        this.visible = true
+      } else {
+        this.visible = false
+      }
+    }
+  }
+
+  @Watch('selectionProducts', { deep: true }) selectionProductsChanged () {
+    if (this.store === 'products') {
+      if (this.selectionProducts.length > 0) {
+        this.visible = true
+      } else {
+        this.visible = false
+      }
+    }
+  }
+
+  clearSelected () {
+    // if (this.store === 'depots') {
+    //   this.setSelectionDepots([])
+    // } else
+
+    if (this.store === 'clients') {
+      this.setSelectionClients([])
+    } else if (this.store === 'products') {
+      this.setSelectionProducts([])
     }
   }
 }
