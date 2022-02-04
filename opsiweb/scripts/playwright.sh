036f7cbebd3f01606bcbf00be4d7c014741ed89a
Anna Sucher <a.sucher@uib.de>
2022-02-04 08:52:15 +0000

    [fix] add RunOnSave (on vue/stories/test-integration) to trigger playwright tests
    Former-commit-id: 7718b273ec5239f7a33bf32f0b0228a64e418e48

---
+++ opsiweb/scripts/playwright.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+# $1: filename                      e.g. BAuthFooter.stories.js
+# $2: filename-full-extension       e.g. .stories.js
+# $3: test-full-extension           e.g. .test.integration.js
+# $4: playwright-config-path(rel)   e.g. .config/playwright-all-components.js
+file=$1
+file_ext=$2
+file_ext_new=$3
+npm_command=$4
+
+# import bash-aliases to be able to use npm-uib
+shopt -s expand_aliases
+source $HOME/.bashrc
+
+cd /workspace/opsiweb/
+# build filename of testfile
+echo "filename: ${file} - change extension from '${file_ext}' to '${file_ext_new}'"
+testfile=$(sed 's/'"$file_ext"'/'"$file_ext_new"'/g' <<<"$file")
+echo "---> testing file: $testfile"
+
+# run playwright test on the testfile
+echo "run: npm-uib run $npm_command $testfile"
+npm-uib run $npm_command $testfile
+
+cd -
