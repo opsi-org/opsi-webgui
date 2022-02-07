e83420e47ca4337baa3c5e277acc61b9ff168213
Anna Sucher <a.sucher@uib.de>
2022-02-07 13:20:08 +0000

    [fix] remove mock addon (fix lint)

--- nuxt.config.js
+++ nuxt.config.js
@@ -130,7 +130,7 @@ export default {
   },
   storybook: {
     stories: ['~/components/**/*.stories.js'],
-    decorators: ['<div><link rel=\\"stylesheet\\" href=\\"/themes/opsi-bootstrap-theme-light.css\\"/><story/></div>'],
+    decorators: ['<div><link rel=\\"stylesheet\\" href=\\"/themes/opsi-bootstrap-theme-light.css\\"/><story/></div>']
     // addons: ['storybook-addon-mock/register'] //, '@storybook/addon-mock'] // "storybook-addon-mock": "^2.0.2",
   }
 }
