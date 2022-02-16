bbc78ab121ae297b7a466f98008ce6d37d793e46
Jeena John <j.john@uib.de>
2022-02-16 13:23:32 +0100

    [chg] update modules

--- components/view/VModules.vue
+++ components/view/VModules.vue
@@ -1,21 +1,12 @@
 <template>
   <div data-testid="VModules">
-    <p v-if="errorText">
-      {{ errorText }}
-    </p>
-    <b-table
-      v-else
-      :busy="isLoading"
-      borderless
-      stacked
-      small
+    <TableTBVTable
+      :stacked="true"
+      :is-loading="isLoading"
+      :error="errorText"
       :items="[modules]"
       :fields="Object.keys(modules).filter(k => k !== 'signature')"
-    >
-      <template #table-busy>
-        <IconILoading />
-      </template>
-    </b-table>
+    />
   </div>
 </template>
 
@@ -24,22 +15,16 @@ import { Component, Vue } from 'nuxt-property-decorator'
 @Component
 export default class VModules extends Vue {
   $axios: any
-  // $nuxt: any
-  // $fetch: any
-  // $mq: any
-  // $t: any
+
   isLoading: boolean = false
   modules: object = {}
   errorText: string = ''
-  hideValue : boolean = false
-  // @auth.Mutation public setSession!: () => void
 
   async fetch () {
     this.isLoading = true
     await this.$axios.$get('/api/opsidata/modulesContent')
       .then((response) => {
         this.modules = response.result
-        // this.setSession()
       }).catch((error) => {
         // eslint-disable-next-line no-console
         console.error(error)
