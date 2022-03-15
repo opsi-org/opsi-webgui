6d2753988d2f0035b0c649bb806dc9376cffa21f
Jeena John <j.john@uib.de>
2022-03-15 13:39:03 +0100

    [fix] provide title for iframes
    Former-commit-id: 78c0d1a92ffc26692864985186aca9e85ff8d05a

--- opsiweb/pages/support.vue
+++ opsiweb/pages/support.vue
@@ -9,6 +9,7 @@
       <b-col>
         <b-card>
           <iframe
+            title="OPSI Docs"
             :src="`https://docs.opsi.org/opsi-docs-de/4.2/index.html`"
             width="100%"
             height="600"
