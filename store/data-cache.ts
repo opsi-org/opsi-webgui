1a3b870131ec66545036d659919ef24a4d3ac6dd
Anna Sucher <a.sucher@uib.de>
2022-03-23 11:50:57 +0000

    [fix] refetch configserver if empty

--- store/data-cache.ts
+++ store/data-cache.ts
@@ -1,16 +1,13 @@
-import { Module, VuexModule, VuexMutation } from 'nuxt-property-decorator'
+import { Module, VuexModule, VuexMutation, Watch } from 'nuxt-property-decorator'
 // chimport { ITheme } from '~/types/tsettings'
 
 @Module({ name: 'data-cache', stateFactory: true, namespaced: true })
-export default class Settings extends VuexModule {
+export default class DataCache extends VuexModule {
   $axios: any
   _opsiconfigserver: string = ''
 
   // get opsiconfigserver (): string { return this._opsiconfigserver }
   get opsiconfigserver (): string {
-    // if (this._opsiconfigserver === '') {
-    //   this._opsiconfigserver = (await this.$axios.$get('/api/user/opsiserver')).result
-    // }
     return this._opsiconfigserver
   }
   // set configserverId (s: string) { this.opsiconfigserver = s }
