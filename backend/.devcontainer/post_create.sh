1edfbd9ce7374092e2ddd1799f3713c5a909dc01
Fabian Kalweit <f.kalweit@uib.de>
2022-03-29 16:08:42 +0200

    [chg] backend devcontainer + fix in product version diff
    Former-commit-id: cddecf93b7e9c67b2b6c444ca9899aabc1797a18

---
+++ backend/.devcontainer/post_create.sh
@@ -0,0 +1,29 @@
+echo "* Running as $(whoami)"
+
+# install_jemalloc() {
+# 	echo "* Installing jemalloc"
+# 	cd /tmp
+# 	wget https://github.com/jemalloc/jemalloc/releases/download/$JEMALLOC_VERSION/jemalloc-$JEMALLOC_VERSION.tar.bz2
+# 	tar xvjf jemalloc-$JEMALLOC_VERSION.tar.bz2
+# 	cd jemalloc-$JEMALLOC_VERSION
+# 	./configure
+# 	make
+# 	sudo make install
+# }
+
+/workspace/scripts/setup-hosts.sh
+/workspace/scripts/setup-grafana.sh
+/workspace/scripts/setup-mysql.sh
+
+# [ -n "$JEMALLOC_VERSION" ] && install_jemalloc
+
+sudo mkdir -p /var/log/opsi
+sudo mkdir -p /var/lib/opsi/depot
+sudo mkdir -p /var/lib/opsi/public
+sudo mkdir -p /var/lib/opsi/repository
+sudo mkdir -p /var/lib/opsi/workbench
+sudo mkdir -p /var/lib/opsiconfd
+sudo mkdir -p /tftpboot
+sudo ln -s /workspace/addon /var/lib/opsiconfd/addons
+
+sudo chown -R $DEV_USER /workspace
---
+++ backend/.devcontainer/post_start.sh
@@ -0,0 +1,32 @@
+echo "* Running as $(whoami)"
+
+/workspace/scripts/setup-hosts.sh
+
+echo "* Fetch a test license"
+sudo mkdir -p /etc/opsi/licenses
+sudo wget --header="Authorization: Bearer ${OPSILICSRV_TOKEN}" "https://opsi-license-server.uib.gmbh/api/v1/licenses/test?usage=opsiconfd-dev-container" -O /etc/opsi/licenses/test.opsilic || true
+
+echo "* Upgrade opsi-dev-tool"
+sudo opsi-dev-tool --self-upgrade || true
+
+echo "* Restarting services"
+sudo service redis-server restart
+sudo service mysql restart
+sudo service grafana-server restart
+
+sudo chown -R $DEV_USER /workspace
+sudo opsi-set-rights
+
+echo "* Install git hooks"
+cd $HOME
+git clone https://oauth2:UqZXUJsgG4dBGLBbTjDM@gitlab.uib.gmbh/uib/opsi-git-hooks.git .opsi-git-hooks
+cd /workspace
+opsi-dev-tool git-hooks --install
+
+echo "* clone opsiconfd"
+git clone git@gitlab.uib.gmbh:uib/opsiconfd.git
+
+echo "* Setup poetry venv"
+cd /workspace/opsiconfd
+poetry lock --no-update
+poetry install --no-interaction --no-ansi
