f27adacc0ba60ba602d578f9ea7206c1ff43707f
Jeena John <j.john@uib.de>
2022-04-11 10:07:38 +0200

    [chg] clear selection btn style changes

--- components/button/BTNClearSelection.vue
+++ components/button/BTNClearSelection.vue
@@ -3,13 +3,13 @@
     v-b-tooltip.hover
     aria-label="clearSelection"
     :title="label? '' : $t('table.selection.clear')"
-    variant="transparent"
-    class="ClearSelection"
+    variant="outline-primary"
+    class="ClearSelection border-0"
     size="sm"
     @click="clearselection"
   >
     <span class="sr-only">{{ $t('table.selection.clear') }}</span>
-    <b-icon :icon="iconnames.clear" class="icon-primary" />
+    <b-icon :icon="iconnames.clear" />
     <span v-if="showLabel">{{ label? label: $t('table.selection.clear') }} </span>
   </b-button>
 </template>
@@ -19,27 +19,10 @@ import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import { Constants } from '../../mixins/uib-mixins'
 
 @Component({ mixins: [Constants] })
-export default class BTNDeleteAll extends Vue {
+export default class BTNClearSelection extends Vue {
   iconnames:any
   @Prop({}) label?: string
   @Prop({ default: true }) showLabel?: boolean
   @Prop({ default: () => { return () => { /* default */ } } }) clearselection!: Function
 }
 </script>
-<style>
-.ClearSelection {
-  color: var(--dark) !important;
-  border: none !important
-  /* background: green !important; */
-}
-.ClearSelection .icon-primary{
-  color: var(--primary) !important;
-}
-.ClearSelection:hover .icon-primary{
-  color: var(--light) !important;
-}
-.ClearSelection:hover {
-  background: var(--primary) !important;
-  color: var(--light) !important;
-}
-</style>
