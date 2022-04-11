e6eb3b6319bc8b185ce3ace004505ffddf26922f
Jeena John <j.john@uib.de>
2022-04-11 16:07:03 +0200

    [chg] same layout for settings pages

--- components/view/VSettingsLocalSpecific.vue
+++ components/view/VSettingsLocalSpecific.vue
@@ -1,25 +1,23 @@
 <template>
   <div data-testid="VSettingsLocalSpecific">
-    <div>
-      <b-row class="mb-2">
-        <b-col sm="3" class="text-sm-right">
-          {{ $t('settingsPage.theme') }}
-        </b-col>
-        <b-col>
-          <DropdownDDTheme />
-        </b-col>
-      </b-row>
-      <b-row class="mb-2">
-        <b-col sm="3" class="text-sm-right">
-          <ButtonBTNHelp id="expert-help" />
-          {{ $t('settingsPage.mode') }}
-          <TooltipTTHelpExpert target="expert-help" />
-        </b-col>
-        <b-col>
-          <ButtonBTNExpertMode />
-        </b-col>
-      </b-row>
-    </div>
+    <b-row>
+      <b-col class="text-sm-right" cols="2">
+        {{ $t('settingsPage.theme') }}
+      </b-col>
+      <b-col>
+        <DropdownDDTheme />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col class="text-sm-right" cols="2">
+        <ButtonBTNHelp id="expert-help" />
+        {{ $t('settingsPage.mode') }}
+        <TooltipTTHelpExpert target="expert-help" />
+      </b-col>
+      <b-col>
+        <ButtonBTNExpertMode />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
