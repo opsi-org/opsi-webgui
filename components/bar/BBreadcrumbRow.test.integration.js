28d260ebf33bfb412ffd49e18673886af5fceb78
Anna Sucher <a.sucher@uib.de>
2022-04-11 13:50:43 +0000

    [fix] tests bbreadcrumb

--- components/bar/BBreadcrumbRow.test.integration.js
+++ components/bar/BBreadcrumbRow.test.integration.js
@@ -4,5 +4,5 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 test('bar bbreadcrumb snapshot', async ({ page }) => {
   await callStoryId(page, 'bar-b-breadcrumb', 'b-breadcrumb')
   const component = await page.locator('[data-testid="BarBBreadcrumb"]')
-  expect(await component.screenshot()).toMatchSnapshot('bar-bbreadcrumb.png')
+  expect(await component.screenshot()).toMatchSnapshot('BBreadcrumbRow.png')
 })
