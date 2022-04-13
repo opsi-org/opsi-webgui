12edbe511f3e3f2087940474c49476c2b18cc1ef
Anna Sucher <a.sucher@uib.de>
2022-04-13 16:32:38 +0000

    [fix] update test+snap; Ref opsiweb-ui#297

--- components/icon/ITableColumn.test.integration.js
+++ components/icon/ITableColumn.test.integration.js
@@ -4,5 +4,5 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 test('icon opsilogo snapshot', async ({ page }) => {
   await callStoryId(page, 'icon-i-table-column', 'i-table-column')
   const component = await page.locator('[data-testid="ITableColumn"]')
-  expect(await component.screenshot()).toMatchSnapshot('icon-itablecolumn.png')
+  expect(await component.screenshot()).toMatchSnapshot('ITableColumn.png')
 })
