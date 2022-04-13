303551fc3e7bdb7c966df574212342a2d8860e2e
Anna Sucher <a.sucher@uib.de>
2022-04-13 08:38:23 +0000

    [fix] test+snap bside; Ref opsiweb-ui#297
    Former-commit-id: b8cb6931cbcd85298ee1fe80e79c8243093000f6

--- opsiweb/.config/playwright-all-components.config.js
+++ opsiweb/.config/playwright-all-components.config.js
@@ -4,6 +4,7 @@ const generalconfig = require('../uib-components/.utils/playwright/config/playwr
 // /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   // testDir: '../../../../', // /workspace/opsiweb
+  timeout: 1000 * 10, // 1ms * x sec
   testDir: '../', // /workspace/opsiweb/uib-components
   testMatch: '*.test.*(int|integration|component|comp).js',
   reporter: [
