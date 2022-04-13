12ed2d08e4c91b4d123b819821f869f5a7f5fdf6
Anna Sucher <a.sucher@uib.de>
2022-04-13 10:58:38 +0000

    [fix] test+snap BTooltipCollapseRow; Ref opsiweb-ui#297

--- components/bar/BTooltipCollapseRow.test.integration.js
+++ components/bar/BTooltipCollapseRow.test.integration.js
@@ -4,5 +4,5 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 test('bar btooltipcollapserow snapshot', async ({ page }) => {
   await callStoryId(page, 'bar-b-tooltip-collapse-row', 'b-tooltip-collapse-row')
   const component = await page.locator('[data-testid="BarBTooltipCollapseRow"]')
-  expect(await component.screenshot()).toMatchSnapshot('bar-btooltipcollapserow.png')
+  expect(await component.screenshot()).toMatchSnapshot('BTooltipCollapseRow.png')
 })
