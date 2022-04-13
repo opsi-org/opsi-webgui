2e267df09121ab61e0f75a63d031ea6dbbb2b7e8
Anna Sucher <a.sucher@uib.de>
2022-04-13 12:21:38 +0000

    [fix] update snapshots with light-theme; Ref opsiweb-ui#297

--- components/button/BTNClearChanges.test.integration.js
+++ components/button/BTNClearChanges.test.integration.js
@@ -4,5 +4,5 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 test('button clear all changes snapshot', async ({ page }) => {
   await callStoryId(page, 'button-btn-clear-changes', 'btn-clear-changes')
   const component = await page.locator('[data-testid="BTNClearChanges"]')
-  expect(await component.screenshot()).toMatchSnapshot('button-btnclearchanges.png')
+  expect(await component.screenshot()).toMatchSnapshot('BTNClearChanges.png')
 })
