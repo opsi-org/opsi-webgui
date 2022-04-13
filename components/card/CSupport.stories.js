4cdf32424af290081e5cab0b75ef22fb998c33dd
Anna Sucher <a.sucher@uib.de>
2022-04-13 13:32:12 +0000

    [fix] update test+snap; Ref opsiweb-ui#297

--- components/card/CSupport.stories.js
+++ components/card/CSupport.stories.js
@@ -1,22 +1,12 @@
-import { argTypeTextContent, argTypeTextTitle } from '../../.utils/types/ttestconsts'
-
-// import { argTypeBoolFalse, argTypeTextContent } from '../../.utils/types/ttestconsts'
 export default {
   title: 'Card/C Support',
-  parameters: { docs: { description: { component: 'Card/CardCSupport description' } } },
-  argTypes: {
-    // hide: { action: 'clicked' },
-    title: argTypeTextTitle,
-    content: argTypeTextContent,
-    buttonname: argTypeTextContent
-  }
+  parameters: { docs: { description: { component: 'Card/CSupport description' } } }
 }
 
 const PrimaryTemplate = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  template: `<CardCSupport
-  :item="{title:'${args.title}'||$props.title, description:'${args.content}'||$props.content, link:'.', buttonname:'${args.buttonname}'||$props.buttonname}"
-  />`
+  computed: { args () { return args } },
+  template: '<CardCSupport :item="{title: args.title, description: args.content, link:args.link, buttonname: args.buttonname}" />'
   // :item="{title:'title', description:'desc', link:'.', buttonname:'buttonname'}"
   // :item="{ title: 'props.title', description: 'props.content', link:'.', buttonname:'props.buttonname'}"
 })
@@ -25,5 +15,6 @@ export const CSupport = PrimaryTemplate.bind({})
 CSupport.args = {
   title: 'Card title',
   content: 'This is the description of the card. It can be a longer test. Thats because its the description. It can be a longer test. Thats because its the description. It can be a longer test. Thats because its the description. It can be a longer test. Thats because its the description. It can be a longer test. Thats because its the description.',
-  buttonname: 'Action Button'
+  buttonname: 'Action Button',
+  link: '.'
 }
