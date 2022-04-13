8e0fc3c7140d2d575e44405328f5c359e036fe05
Anna Sucher <a.sucher@uib.de>
2022-04-13 12:46:41 +0000

    [fix] update test+snap; Ref opsiweb-ui#297

--- components/button/BTNExpertMode.stories.js
+++ components/button/BTNExpertMode.stories.js
@@ -1,21 +1,16 @@
-import { argTypeBoolFalse } from '../../.utils/types/ttestconsts'
-
-// import { argTypeVariants } from '../../.utils/types/ttestconsts'
 export default {
   title: 'Button/BTN Expert Mode',
-  parameters: { docs: { description: { component: 'Button/BTNExpertMode description' } } },
-  argTypes: {
-    // hide: { action: 'clicked' },
-    navbar: argTypeBoolFalse
-  }
+  parameters: { docs: { description: { component: 'Button/BTNExpertMode description' } } }
 }
 
 const PrimaryTemplate = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  template: `<ButtonBTNExpertMode :navbar="${args.navbar}||$props.navbar" />`
+  computed: { args () { return args } },
+  template: '<ButtonBTNExpertMode :navbar="args.navbar" />'
 })
 
 // named export Primary to create respective story
 export const BTNExpertMode = PrimaryTemplate.bind({})
+BTNExpertMode.args = { navbar: false }
 export const BTNExpertModeNavbar = PrimaryTemplate.bind({})
 BTNExpertModeNavbar.args = { navbar: true }
