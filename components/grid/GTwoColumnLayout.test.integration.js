f69588116e51271d21138fdfd15dbc8c9f6a08d5
Anna Sucher <a.sucher@uib.de>
2022-04-13 16:16:42 +0000

    [fix] update test+snap; Ref opsiweb-ui#297

--- components/grid/GTwoColumnLayout.test.integration.js
+++ components/grid/GTwoColumnLayout.test.integration.js
@@ -4,5 +4,5 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 test('grid-two colum layout snapshot', async ({ page }) => {
   await callStoryId(page, 'grid-g-two-column-layout', 'g-two-column-layout')
   const component = await page.locator('[data-testid="GTwoColumnLayout"]')
-  expect(await component.screenshot()).toMatchSnapshot('grid-gtwocol.png')
+  expect(await component.screenshot()).toMatchSnapshot('GTwoColumnLayout.png')
 })
