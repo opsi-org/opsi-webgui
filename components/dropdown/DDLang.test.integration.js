bf20b14dc9d92c57d79d17d4ef1fcbbfa0c298a9
Anna Sucher <a.sucher@uib.de>
2022-04-13 15:13:14 +0000

    [fix] update test+snap; Ref opsiweb-ui#297

--- components/dropdown/DDLang.test.integration.js
+++ components/dropdown/DDLang.test.integration.js
@@ -4,14 +4,14 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 test('dropdown lang snapshot', async ({ page }) => {
   await callStoryId(page, 'dropdown-dd-lang', 'dd-lang')
   const component = await page.locator('[data-testid="DropdownDDLang"]')
-  expect(await component.screenshot()).toMatchSnapshot('dropdown-ddlang-normal.png')
+  expect(await component.screenshot()).toMatchSnapshot('DDLang-normal.png')
 })
 
 test('dropdown lang opened snapshot', async ({ page }) => {
   await callStoryId(page, 'dropdown-dd-lang', 'dd-lang')
   await page.click('[data-testid="DropdownDDLang"]')
   const component = await page.locator('[data-testid="DropdownDDLang"] > ul')
-  expect(await component.screenshot()).toMatchSnapshot('dropdown-ddlang-opened.png')
+  expect(await component.screenshot()).toMatchSnapshot('DDLang-opened.png')
 })
 
 test('dropdown lang changed snapshot', async ({ page }) => {
@@ -19,5 +19,5 @@ test('dropdown lang changed snapshot', async ({ page }) => {
   await page.click('[data-testid="DropdownDDLang"]')
   await page.click('[data-testid="DropdownDDLang-Item-de"]')
   const component = await page.locator('[data-testid="DropdownDDLang"]')
-  expect(await component.screenshot()).toMatchSnapshot('dropdown-ddlang-changed.png')
+  expect(await component.screenshot()).toMatchSnapshot('DDLang-changed.png')
 })
