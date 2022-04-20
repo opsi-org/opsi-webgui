e2c94259013c914100c2ec25bb270e6c03eef388
Anna Sucher <a.sucher@uib.de>
2022-04-20 12:32:29 +0000

    [fix] snap; Ref opsiweb-ui#297

--- components/table/TBVTable.stories.js
+++ components/table/TBVTable.stories.js
@@ -1,11 +1,70 @@
 export default {
-  title: 'Table/T B V Table',
+  title: 'Table/TBV Table',
   parameters: { docs: { description: { component: 'Table' } } }
 }
 
-const PrimaryTemplate = () => ({
-  template: '<TableTBVTable />'
+const PrimaryTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  computed: { args () { return args } },
+  template: `<TableTBVTable
+    type="small"
+    :noheader="true"
+    :hover="true"
+    :filter="''"
+    :filterfields="['depotId','clientId']"
+    :items="args.items"
+    :fields="['depotId', 'clientId', 'actionRequest', 'property', 'propertyValue']"
+  >
+    <template #cell()="row">
+      <small>{{ row.value }}</small>
+    </template>
+  </TableTBVTable>`
 })
-
 // named export Primary to create respective story
 export const TBVTable = PrimaryTemplate.bind({})
+TBVTable.args = {
+  items: [
+    {
+      user: 'user',
+      depotId: 'depot',
+      clientId: 'client',
+      productId: 'product1',
+      type: 'Product',
+      actionRequest: 'setup'
+    },
+    {
+      user: 'user',
+      depotId: 'depot',
+      clientId: 'client2',
+      productId: 'product1',
+      type: 'Product',
+      actionRequest: 'setup'
+    },
+    {
+      user: 'user',
+      depotId: 'depot',
+      clientId: 'client',
+      productId: 'product2',
+      actionRequest: 'setup',
+      type: 'Product'
+    },
+    {
+      user: 'user',
+      depotId: 'depot',
+      clientId: 'client',
+      productId: 'product1',
+      property: 'prop',
+      propertyValue: ['test'],
+      type: 'Product'
+    },
+    {
+      user: 'user2',
+      depotId: 'depot',
+      clientId: 'client',
+      productId: 'productX',
+      property: 'prop',
+      propertyValue: ['test'],
+      type: 'Product'
+    }
+  ]
+}
