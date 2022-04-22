12badf7797ca7320c58a6189d023d6c0b1191c7b
Jeena John <j.john@uib.de>
2022-04-22 12:37:08 +0200

    [chg] use bootstrap classes
    Former-commit-id: 8e5e420e938340110f210a8c921ae3f74b25e273

--- opsiweb/pages/login.vue
+++ opsiweb/pages/login.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="container-fluid">
-    <b-card class="login_content text-center bg-primary">
-      <div class="login_title_row">
-        <b-badge class="bg-light login_logo_container">
-          <img src="~/uib-components/assets/images/LogoOpsi.png" class="login_logo" alt="opsi logo">
+    <b-card class="text-center bg-primary mt-3 mx-auto login_card">
+      <div class="mb-2">
+        <b-badge class="d-inline-block bg-light">
+          <img src="~/uib-components/assets/images/LogoOpsi.png" height="35" alt="opsi logo">
         </b-badge>
-        <h1 class="login_title">
+        <h1 class="d-inline-block text-light">
           {{ getTitleUppercase() }}
         </h1>
       </div>
       <FormFLogin />
-      <!-- <ComponentsFormFLogin /> -->
     </b-card>
   </div>
 </template>
@@ -28,29 +27,7 @@ export default class PLogin extends Vue {
 </script>
 
 <style>
-
-.login_content {
-  margin-top: max(15px, 5%);
-  max-width: 450px;
-  margin: 0 auto;
-}
-.login_title_row {
-  margin-bottom: 5px;
-}
-
-.login_logo_container {
-  display: inline-block;
-  margin-left:5px;
-  margin-right:5p;
-  margin-bottom: 1rem !important;
-}
-.login_logo{
-  display: inline-block;
-  height: 35px !important;
-}
-
-.login_title{
-  display: inline-block;
-  color: var(--light);
+.login_card {
+  max-width: 35%;
 }
 </style>
