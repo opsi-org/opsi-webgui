8f9d10d08250cb3d45ed80f35a4c71ce42c57001
Anna Sucher <a.sucher@uib.de>
2022-04-25 10:00:20 +0000

    [fix] test+snap; Ref opsiweb-ui#297

--- components/tree/TSHostGroups.stories.js
+++ components/tree/TSHostGroups.stories.js
@@ -1,3 +1,9 @@
+import { mock, data } from '../../.utils/storybook/mock'
+mock.onGet('/api/opsidata/hosts/groups?selectedDepots=[<config-server-id>]&selectedClients=[]&parentGroup=root').reply(200, data.groups.hosts)
+mock.onGet('/api/opsidata/hosts/groups?selectedDepots=[]&selectedClients=[]&parentGroup=root').reply(200, data.groups.hosts)
+mock.onGet('/api/opsidata/depots/clients?selectedDepots=[<config-server-id>]').reply(200, data.clientIds)
+mock.onGet('/api/opsidata/depots/clients?selectedDepots=[]').reply(200, data.clientIds)
+
 export default {
   title: 'Tree/TS Host Groups',
   parameters: { docs: { description: { component: 'Treeselect for host groups with delayed loading children' } } }
