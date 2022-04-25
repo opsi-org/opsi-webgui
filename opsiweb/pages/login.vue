ab4a36dfd9f6842624eed791d3454a1d21ef4ff5
Jeena John <j.john@uib.de>
2022-04-25 17:43:42 +0200

    [chg] change card bg-secondary
    Former-commit-id: a7bf76cf537b14402806f1ea1f058953faeb7c86

--- opsiweb/pages/login.vue
+++ opsiweb/pages/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-    <b-card class="text-center bg-primary mt-3 mx-auto w-25">
+    <b-card class="text-center bg-secondary mt-3 mx-auto w-25">
       <IconIOpsiLogo :light="true" class="mb-3" height="35" />
       <h1 class="d-inline-block text-light">
         {{ getTitleUppercase() }}
