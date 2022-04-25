8fa34821e64ed385d4faacb9effa698921134abe
Anna Sucher <a.sucher@uib.de>
2022-04-25 17:08:35 +0000

    [chg] update packages

--- components/bar/BTop.test.unit.js
+++ components/bar/BTop.test.unit.js
@@ -2,7 +2,7 @@ const BootstrapVue = require('bootstrap-vue').BootstrapVue
 const BootstrapVueIcons = require('bootstrap-vue').BootstrapVueIcons
 const createLocalVue = require('@vue/test-utils').createLocalVue
 const mount = require('@vue/test-utils').mount
-const BTop = require('./BTop').default
+const BTop = require('./BTop.vue')
 
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
