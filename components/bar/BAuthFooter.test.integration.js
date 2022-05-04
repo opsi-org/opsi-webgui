1a4c2a898d63826d12992325836f26ba7639b702
Anna Sucher <a.sucher@uib.de>
2022-05-04 15:51:28 +0000

    [fix] snap

--- components/bar/BAuthFooter.test.integration.js
+++ components/bar/BAuthFooter.test.integration.js
@@ -10,5 +10,5 @@ test('bar bauthfooter snapshot', async ({ page }) => {
   await callStoryId(page, 'bar-b-auth-footer', 'b-auth-footer')
   const component = await page.locator('[data-testid="BarBAuthFooter"]')
   await component.evaluate(() => { document.querySelector('.BAuthFooter-version').innerHTML = 'x.x.x' })
-  expect(await page.screenshot()).toMatchSnapshot('BAuthFooter.png')
+  expect(await page.screenshot()).toMatchSnapshot('BAuthFooter.png', { threshold: 0.2 })
 })
