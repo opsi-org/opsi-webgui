619dec66ddcf2023c9bd2fb32501b28fde60425f
Anna Sucher <a.sucher@uib.de>
2022-05-04 15:18:52 +0000

    [fix] update snaps

--- components/icon/IDetails.stories.js
+++ components/icon/IDetails.stories.js
@@ -5,7 +5,16 @@ export default {
 
 const DefaultVisibleTemplateHead = (_args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  template: `<IconIDetails/>
+  template: `<div data-testid="IDetails-All">
+  depot version different <IconIDetails content="depot-unequal"/>
+  >= 1 depot doesnt have product <IconIDetails content="depot-wo-prod"/>
+  client outdated <IconIDetails content="client-outdated"/>
+  pp-value different for clients <IconIDetails content="ppv-client-different"/>
+  pp not on every depot <IconIDetails content="ppid-not-exists-on-depot"/>
+  just unequal <IconIDetails content="unequal"/>
+  something else <IconIDetails content="something else"/>
+  </div>
   `
 })
+
 export const IDetails = DefaultVisibleTemplateHead.bind({})
