2d05ea44d87cf3150c461257c32adcd9a45cf4c2
Jeena John <j.john@uib.de>
2022-05-16 12:37:09 +0200

    [new] refetch button

---
+++ components/button/BTNRefetch.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-button
+    v-if="!isLoading"
+    variant="outline-dark"
+    class="float-right"
+    @click="refetch"
+  >
+    Try Again
+  </b-button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { Constants } from '../../mixins/uib-mixins'
+
+@Component({ mixins: [Constants] })
+export default class BTNRefetch extends Vue {
+  iconnames:any
+  @Prop({ default: true }) isLoading?: boolean
+  @Prop({ default: () => { return () => { /* default */ } } }) refetch!: Function
+}
+</script>
