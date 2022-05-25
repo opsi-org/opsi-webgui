396f82d6c1f9bec0eeda1cb0049fc8f7c8367cbd
Jeena John <j.john@uib.de>
2022-05-25 19:37:52 +0200

    [fix] product table status icons styling; ref opsiweb-ui#410

--- components/table/cell/TCInstallationStatus.vue
+++ components/table/cell/TCInstallationStatus.vue
@@ -1,23 +1,23 @@
 <template>
   <b-badge v-if="text=='mixed'" :variant="variant">
-    <span class="h5"> &#8800; </span>
+    <span class="h6"> &#8800; </span>
   </b-badge>
   <b-badge v-else-if="text=='installed'" variant="success">
-    <span class="h5">
+    <span class="h6">
       <b-icon :icon="iconnames.productInstallationStatusInstalled" alt="installed" title="installed" />
     </span>
   </b-badge>
   <b-badge v-else-if="text=='unknown'" variant="warning" text-variant="dark">
-    <span class="h5">
+    <span class="h6">
       <b-icon :icon="iconnames.productInstallationStatusUnknown" alt="unknown" />
     </span>
   </b-badge>
   <b-badge v-else-if="$mq=='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent">
-    <span class="h5"> {{ ($mq=='mobile')? '-':'' }} </span>
+    <span class="h6"> {{ ($mq=='mobile')? '-':'' }} </span>
   </b-badge>
   <b-badge v-else-if="$mq!='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent" />
   <b-badge v-else>
-    <span class="h5"> {{ text }} </span>
+    <span class="h6"> {{ text }} </span>
   </b-badge>
 </template>
 
