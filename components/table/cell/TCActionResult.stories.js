e6fa9c8b95db7cff86bff1bf65c91f3d811b73c7
Jeena John <j.john@uib.de>
2022-07-08 16:32:58 +0200

    [new] add story for actionResult badge; ref opsiweb-ui#297

--- components/table/cell/TCActionResult.stories.js
+++ components/table/cell/TCActionResult.stories.js
@@ -3,9 +3,14 @@ export default {
   parameters: { docs: { description: { component: 'Table cell for actionResult' } } }
 }
 
-const PrimaryTemplate = () => ({
-  template: '<TableCellTCActionResult />'
+const PrimaryTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  computed: { args () { return args } },
+  template: '<TableCellTCActionResult v-bind="args" />'
 })
 
 // named export Primary to create respective story
 export const TCActionResult = PrimaryTemplate.bind({})
+TCActionResult.args = {
+  text: 'sample text'
+}
