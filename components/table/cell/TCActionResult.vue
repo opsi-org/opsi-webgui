891acf9fc29b5f9fa1cefced7b8a1ba557851339
Jeena John <j.john@uib.de>
2022-07-08 16:46:28 +0200

    [new] add tests and snapshots for actionresult; ref opsiweb-ui#297

--- components/table/cell/TCActionResult.vue
+++ components/table/cell/TCActionResult.vue
@@ -1,20 +1,22 @@
 <template>
-  <b-badge v-if="text=='mixed'" :variant="variant">
-    <span class="h6"> {{ $t('unequal') }} </span>
-  </b-badge>
-  <b-badge v-else-if="text=='successful'" variant="success">
-    <span class="h6"> <b-icon :icon="iconnames.productActionResultSuccessful" alt="successful" /> </span>
-  </b-badge>
-  <b-badge v-else-if="text=='failed'" variant="danger">
-    <span class="h6"> <b-icon :icon="iconnames.productActionResultFailed" alt="failed" /> </span>
-  </b-badge>
-  <b-badge v-else-if="$mq=='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent">
-    <span class="h6"> {{ ($mq=='mobile')? $t('empty'):'' }} </span>
-  </b-badge>
-  <b-badge v-else-if="$mq!='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent" />
-  <b-badge v-else :variant="variant">
-    <span class="h6"> {{ text }} </span>
-  </b-badge>
+  <div data-testid="TCActionResult">
+    <b-badge v-if="text=='mixed'" :variant="variant">
+      <span class="h6"> {{ $t('unequal') }} </span>
+    </b-badge>
+    <b-badge v-else-if="text=='successful'" variant="success">
+      <span class="h6"> <b-icon :icon="iconnames.productActionResultSuccessful" alt="successful" /> </span>
+    </b-badge>
+    <b-badge v-else-if="text=='failed'" variant="danger">
+      <span class="h6"> <b-icon :icon="iconnames.productActionResultFailed" alt="failed" /> </span>
+    </b-badge>
+    <b-badge v-else-if="$mq=='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent">
+      <span class="h6"> {{ ($mq=='mobile')? $t('empty'):'' }} </span>
+    </b-badge>
+    <b-badge v-else-if="$mq!='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent" />
+    <b-badge v-else :variant="variant">
+      <span class="h6"> {{ text }} </span>
+    </b-badge>
+  </div>
 </template>
 
 <script lang="ts">
@@ -22,7 +24,7 @@ import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import { Constants } from '../../../mixins/uib-mixins'
 
 @Component({ mixins: [Constants] })
-export default class TCSpan extends Vue {
+export default class TCActionResult extends Vue {
   $mq: any
   iconnames: any
   @Prop({ }) text!: string
