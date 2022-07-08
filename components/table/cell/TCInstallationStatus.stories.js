fb76987e202ee4d6a4163a5bcdeb66b2451bd7a5
Jeena John <j.john@uib.de>
2022-07-08 17:09:51 +0200

    [chg] update story for tablecell installation status; ref opsiweb-ui#297

--- components/table/cell/TCInstallationStatus.stories.js
+++ components/table/cell/TCInstallationStatus.stories.js
@@ -1,11 +1,16 @@
 export default {
   title: 'Tablecell/T C Installationstatus',
-  parameters: { docs: { description: { component: 'Tablecell for installationStatus' } } }
+  parameters: { docs: { description: { component: 'Table cell for installation status' } } }
 }
 
-const PrimaryTemplate = () => ({
-  template: '<TableCellTCInstallationStatus />'
+const PrimaryTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  computed: { args () { return args } },
+  template: '<TableCellTCInstallationStatus v-bind="args" />'
 })
 
 // named export Primary to create respective story
 export const TCInstallationStatus = PrimaryTemplate.bind({})
+TCInstallationStatus.args = {
+  text: 'sample installation status'
+}
--- components/table/cell/TCInstallationStatus.vue
+++ components/table/cell/TCInstallationStatus.vue
@@ -1,24 +1,26 @@
 <template>
-  <b-badge v-if="text=='mixed'" :variant="variant">
-    <span class="h6"> {{ $t('unequal') }} </span>
-  </b-badge>
-  <b-badge v-else-if="text=='installed'" variant="success">
-    <span class="h6">
-      <b-icon :icon="iconnames.productInstallationStatusInstalled" alt="installed" :title="text" />
-    </span>
-  </b-badge>
-  <b-badge v-else-if="text=='unknown'" variant="warning" text-variant="dark">
-    <span class="h6">
-      <b-icon :icon="iconnames.productInstallationStatusUnknown" :alt="text" />
-    </span>
-  </b-badge>
-  <b-badge v-else-if="$mq=='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent">
-    <span class="h6"> {{ ($mq=='mobile')? $t('empty'):'' }} </span>
-  </b-badge>
-  <b-badge v-else-if="$mq!='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent" />
-  <b-badge v-else>
-    <span class="h6"> {{ text }} </span>
-  </b-badge>
+  <div data-testid="TCActionResult">
+    <b-badge v-if="text=='mixed'" :variant="variant">
+      <span class="h6"> {{ $t('unequal') }} </span>
+    </b-badge>
+    <b-badge v-else-if="text=='installed'" variant="success">
+      <span class="h6">
+        <b-icon :icon="iconnames.productInstallationStatusInstalled" alt="installed" :title="text" />
+      </span>
+    </b-badge>
+    <b-badge v-else-if="text=='unknown'" variant="warning" text-variant="dark">
+      <span class="h6">
+        <b-icon :icon="iconnames.productInstallationStatusUnknown" :alt="text" />
+      </span>
+    </b-badge>
+    <b-badge v-else-if="$mq=='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent">
+      <span class="h6"> {{ ($mq=='mobile')? $t('empty'):'' }} </span>
+    </b-badge>
+    <b-badge v-else-if="$mq!='mobile'&&(text=='not_installed'||text==''||text=='none')" variant="transparent" />
+    <b-badge v-else :variant="variant">
+      <span class="h6"> {{ text }} </span>
+    </b-badge>
+  </div>
 </template>
 
 <script lang="ts">
@@ -26,7 +28,7 @@ import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import { Constants } from '../../../mixins/uib-mixins'
 
 @Component({ mixins: [Constants] })
-export default class TCSpan extends Vue {
+export default class TCInstallationStatus extends Vue {
   iconnames: any // from mixin
   $mq:any
 
