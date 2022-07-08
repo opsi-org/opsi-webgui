92a10912c6f4428e1b0d5fd1f53221f3ea1eda87
Jeena John <j.john@uib.de>
2022-07-08 13:58:31 +0200

    [fix] add mocks for store value; ref opsiweb-ui#297

--- components/icon/IReadOnly.stories.js
+++ components/icon/IReadOnly.stories.js
@@ -1,3 +1,4 @@
+import { customstores } from '../../.utils/storybook/mock'
 export default {
   title: 'Icon/I Read Only',
   parameters: { docs: { description: { component: 'Icon Read Only' } } }
@@ -6,9 +7,17 @@ export default {
 const DefaultVisibleTemplateHead = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   computed: { args () { return args } },
-  template: '<IconIReadOnly v-bind="args" />'
+  template: '<IconIReadOnly />',
+  store: customstores({
+    config: {
+      namespaced: true,
+      getters: {
+        config () { return { read_only: true } }
+      }
+    }
+  })
 })
 export const IReadOnly = DefaultVisibleTemplateHead.bind({})
-IReadOnly.args = {
-  config: { read_only: true }
-}
+// IReadOnly.args = {
+//   config: { read_only: true }
+// }
