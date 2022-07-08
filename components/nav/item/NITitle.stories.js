789bb4c5cb65595954387e4f15b0d55c38ea1e2b
Jeena John <j.john@uib.de>
2022-07-08 15:19:09 +0200

    [fix] fix nav title story; ref opsiweb-ui#297

--- components/nav/item/NITitle.stories.js
+++ components/nav/item/NITitle.stories.js
@@ -1,11 +1,17 @@
 export default {
-  title: 'Nav/NI Title',
-  parameters: { docs: { description: { component: 'Navitem Title' } } }
+  title: 'Navitem/N I Title',
+  parameters: { docs: { description: { component: 'Nav Title' } } }
 }
 
-const PrimaryTemplate = () => ({
-  template: '<NavItemNITitle />'
+const PrimaryTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  computed: { args () { return args } },
+  template: '<NavItemNITitle v-bind="args" />'
 })
 
 // named export Primary to create respective story
 export const NITitle = PrimaryTemplate.bind({})
+NITitle.args = {
+  expanded: true,
+  title: 'Manage'
+}
