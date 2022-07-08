70e57c6c413d273f5fbc1e1f89025508eb5649ef
Jeena John <j.john@uib.de>
2022-07-08 13:44:16 +0200

    [new] add story for trackChanges-modal; ref opsiweb-ui#297

---
+++ components/modal/MTrackChanges.stories.js
@@ -0,0 +1,29 @@
+import { customstores } from '../../.utils/storybook/mock'
+export default {
+  title: 'Modal/M Track Changes',
+  parameters: { docs: { description: { component: 'Modal for tracking changes. Currently shows only the product config changes, where user can reset or save the changes.' } } }
+}
+
+const PrimaryTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  computed: { args () { return args } },
+  template: '<ModalMTrackChanges v-bind="args" />',
+  store: customstores({
+    settings: {
+      namespaced: true,
+      getters:
+        { quicksave () { return false } }
+    },
+    changes: {
+      namespaced: true,
+      getters: {
+        changesProducts () { return [{ user: 'testuser', clientId: 'client1.domain.local', productId: 'opsi-client-agent', actionRequest: 'setup' }] }
+      }
+    }
+  })
+})
+
+export const MTrackChanges = PrimaryTemplate.bind({})
+MTrackChanges.args = {
+  username: 'testuser'
+}
