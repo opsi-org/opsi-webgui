aa4106ffd6717ec5d1af414cb809cc760e12ac43
Jeena John <j.john@uib.de>
2022-07-08 17:44:24 +0200

    [chg] update story for productVersion; ref opsiweb-ui#297

--- components/table/cell/TCProductVersionCell.stories.js
+++ components/table/cell/TCProductVersionCell.stories.js
@@ -1,11 +1,33 @@
+import { customstores } from '../../../.utils/storybook/mock'
 export default {
-  title: 'Tablecell/T C Productversion',
-  parameters: { docs: { description: { component: 'Table cell for ProductVersion' } } }
+  title: 'Tablecell/T C Product Version Cell',
+  parameters: { docs: { description: { component: 'Table cell for Productversion' } } }
 }
 
-const PrimaryTemplate = () => ({
-  template: '<TableCellTCProductVersionCell />'
+const PrimaryTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  computed: { args () { return args } },
+  template: '<TableCellTCProductVersionCell v-bind="args" />',
+  store: customstores({
+    selections: {
+      namespaced: true,
+      getters: {
+        selectionClients () { return ['client1.domain.local', 'client2.domain.local', 'client3.domain.local'] },
+        selectionDepots () { return ['depot1.domain.local', 'depot2.domain.local'] }
+      }
+    }
+  })
 })
 
 // named export Primary to create respective story
 export const TCProductVersionCell = PrimaryTemplate.bind({})
+TCProductVersionCell.args = {
+  row: {
+    item: {
+      productId: 'productId',
+      selectedDepots: []
+    }
+  },
+  type: 'version',
+  clients2depots: {}
+}
