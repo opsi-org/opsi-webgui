4c218426595d4f78119ab2106042e88702b665a9
Jeena John <j.john@uib.de>
2022-07-13 15:07:06 +0200

    [fix] fix all failed tests; ref opsiweb-ui#297

--- components/table/cell/TCInstallationStatus.test.integration.js
+++ components/table/cell/TCInstallationStatus.test.integration.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test')
 const { callStoryId } = require('../../../.utils/playwright/pw-story-call')
 
 test('tableCell installationStatus snapshot', async ({ page }) => {
-  await callStoryId(page, 'tablecell-t-c-installationstatus', 'tc-installation-status')
+  await callStoryId(page, 'tablecell-t-c-installation-status', 'tc-installation-status')
   const component = await page.locator('[data-testid="TCInstallationStatus"]')
   expect(await component.screenshot()).toMatchSnapshot('TCInstallationStatus.png')
 })
