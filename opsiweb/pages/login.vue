59c4a145f19021380bb864d4b0f07d01b225eb38
Jeena John <j.john@uib.de>
2022-07-19 14:52:44 +0200

    [chg] update login view
    Former-commit-id: 57454e408aedf44f3e2d7df98cff23545af13f4a

--- opsiweb/pages/login.vue
+++ opsiweb/pages/login.vue
@@ -1,25 +1,10 @@
 <template>
-  <b-card
-    class="text-center bg-primary mt-3 mx-auto"
-    :style="$mq === 'mobile'? 'width:100%;' : 'width:50%;max-width:400px;' "
-  >
-    <IconIOpsiLogo :light="true" class="mb-3" height="35" />
-    <h1 class="d-inline-block text-light">
-      {{ getTitleUppercase() }}
-    </h1>
-    <!-- <AlertAAlertLocal show> Wird in diesem Moment überarbeitet! Bitte habt Geduld :)</AlertAAlertLocal> -->
-    <FormFLogin />
-  </b-card>
+  <ViewVLogin />
 </template>
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 
 @Component({ layout: 'auth' })
 export default class PLogin extends Vue {
-  $t:any
-
-  getTitleUppercase () {
-    return (this.$t('title.project') as string).toUpperCase()
-  }
 }
 </script>
