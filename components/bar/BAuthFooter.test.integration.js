d1903217eee0167d0011d362b8c23ec195c99ea1
Jeena John <j.john@uib.de>
2022-07-22 14:56:15 +0200

    [chg] change in path for callstory

--- components/bar/BAuthFooter.test.integration.js
+++ components/bar/BAuthFooter.test.integration.js
@@ -8,6 +8,7 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 
 test('bar bauthfooter snapshot', async ({ page }) => {
   await callStoryId(page, 'bar-b-auth-footer', 'b-auth-footer')
+  // await callStory(page, '/story/' + 'bar-b-auth-footer--b-auth-footer')
   const component = await page.locator('[data-testid="BarBAuthFooter"]')
   await component.evaluate(() => { document.querySelector('.BAuthFooter-version').innerHTML = 'x.x.x' })
   expect(await page.screenshot()).toMatchSnapshot('BAuthFooter.png', { threshold: 0.2 })
