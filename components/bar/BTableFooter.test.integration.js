5403b5d66c4ff96322341778cf6357f8c64af02f
Jeena John <j.john@uib.de>
2022-07-22 17:26:25 +0200

    [fix] update test and snapshots for pagination component

--- components/bar/BTableFooter.test.integration.js
+++ components/bar/BTableFooter.test.integration.js
@@ -3,6 +3,6 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 
 test('bar btablefooter snapshot', async ({ page }) => {
   await callStoryId(page, 'bar-b-table-footer', 'b-table-footer')
-  const component = await page.locator('[data-testid="BTableFooter"]')
-  expect(await component.screenshot()).toMatchSnapshot('BTableFooter.png', { maxDiffPixelRatio: 0.2 })
+  // const component = await page.locator('[data-testid="BTableFooter"]')
+  expect(await page.screenshot()).toMatchSnapshot('BTableFooter.png', { maxDiffPixelRatio: 0.2 })
 })
