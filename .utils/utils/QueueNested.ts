a3e2916f66ce52907f62cb8dc54d88a2e4fbfa95
Anna Sucher <a.sucher@uib.de>
2022-08-05 12:50:17 +0000

    [chg] refactor

--- .utils/utils/QueueNested.ts
+++ .utils/utils/QueueNested.ts
@@ -18,17 +18,16 @@ export default class QueueNested {
   enqueueHead(pnr:number, element: Array<any>) {
     this.first_page_number = pnr
     let elements = [...this.elements]
-    elements.unshift(element); // add to front
+    elements.unshift(element); // add new page to front
     if (elements.length > this.max_elements){
-      elements.length = this.max_elements // cut tail
+      elements.length = this.max_elements // cut tail if more pages as capacity
       this.last_page_number = this.last_page_number - 1
     }
 
-    if (this.totalPages > 0 && this.last_page_number > this.totalPages)
+    if (this.totalPages > 0 && this.last_page_number > this.totalPages) {
       this.last_page_number = this.totalPages
-    // console.log('Queue elements ', elements)
+    }
 
-    console.log('pages in cache: ', this.first_page_number, this.last_page_number)
     this.elements = elements
   }
   enqueueTail(pnr: number, element: Array<any>) {
@@ -38,43 +37,19 @@ export default class QueueNested {
     elements.push(element)
     if (elements.length > this.max_elements) {
       elements.shift()
-      // this.first_page_number = pnr - elements.length#
       this.first_page_number = this.first_page_number + 1
       if (this.first_page_number <= 0) this.first_page_number = 0
     }
-    console.log('pages in cache: ', this.first_page_number, this.last_page_number)
     this.elements = elements
   }
-  // enqueueTail(pnr: number, element: Array<any>) {
-  //   let elements = [...this.elements]
-  //   this.last_page_number = pnr
-  //   if (elements.length >= this.max_elements) {
-  //     elements.shift()
-  //     console.log('Queue shift ', elements)
-  //     this.first_page_number = pnr - elements.length
-  //     if (this.first_page_number <= 0)
-  //       this.first_page_number = 0
-  //   }
-
-  //   if (elements.length < this.max_elements) {
-  //     console.log('Queue pushEnd ', element)
-  //     elements.push(element)
-  //   }
-  //   this.elements = elements
-  // }
 
   set(pnr:number, element: Array<any>) {
-    console.log('Queue set ', element)
     if (!element) return
     this.first_page_number = pnr
     this.last_page_number = pnr
     this.elements = [element]
-    // console.log('Queue elements ', this.elements)
-
-    console.log('pages in cache: ', this.first_page_number, this.last_page_number)
   }
   setAuto (pnr:number, element: Array<any>) {
-    console.log('Queue set ', element)
     if (!element) return
     if (pnr == this.first_page_number-1) {
       this.enqueueHead(pnr, element)
@@ -85,7 +60,6 @@ export default class QueueNested {
       this.last_page_number = pnr
       this.elements.length = 0
       this.elements.push(element)
-      console.log('Queue elements ', this.elements)
     }
   }
 
