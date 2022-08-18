710f30cd343b5f918ab0fa2cdf43273391600d4d
Jeena John <j.john@uib.de>
2022-08-18 11:25:53 +0000

    [new] tests for product action request
    Former-commit-id: 6c0b5f8595e979564609472ea0eb6ff3bab8316f

---
+++ opsiweb/screenshots4Docs/take_screenshots/productactionrequest.test.screenshot.js
@@ -0,0 +1,12 @@
+const { test } = require('@playwright/test')
+const { callStoryId } = require('../../uib-components/.utils/playwright/pw-story-call')
+// const en = require('../../uib-components/locale/en.json')
+// const de = require('../../uib-components/locale/de.json')
+test('Products Actionrequest', async ({ page }) => {
+  await callStoryId(page, 'dropdown-dd-product-request', 'dd-product-request-head')
+  await page.setViewportSize({ width: 300, height: 350 })
+  await page.click('[data-testid="DropdownDDProductRequest"]')
+  await (new Promise(resolve => setTimeout(resolve, 1000)))
+  await page.screenshot({ path: './screenshots/en/opsiweb_productactionrequest.png' })
+  await page.screenshot({ path: './screenshots/de/opsiweb_productactionrequest.png' })
+})
