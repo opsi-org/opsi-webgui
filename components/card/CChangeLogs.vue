684d90b2258a3253e4c773f153282d0f472eb333
Jeena John <j.john@uib.de>
2022-08-31 14:42:32 +0000

    [new] add login view with changelogs

---
+++ components/card/CChangeLogs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div data-testid="CChangeLogs">
+    <b-card :title="$t('title.whatsnew')"
+    class="text-center bg-primary mt-3 mx-auto"
+    :style="$mq === 'mobile'? 'width:100%;' : 'width:50%;max-width:400px;' "
+    >
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class CChangeLogs extends Vue {
+
+}
+</script>
+<style>
+</style>
---
+++ components/view/VLogin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div data-testid="VLogin">
+    <FormFLogin />
+    <CardCChangeLogs />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+export default class VLogin extends Vue {
+}
+</script>
+<style>
+</style>
