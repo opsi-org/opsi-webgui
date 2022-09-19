fbb3814ddd0151e92c2f69798d10f00233c27681
Jeena John <j.john@uib.de>
2022-09-19 14:09:48 +0000

    [fix] show alert for unsaved changes; ref  uib/opsiweb-ui#383

--- components/checkbox/CBQuickSave.vue
+++ components/checkbox/CBQuickSave.vue
@@ -1,17 +1,26 @@
 <template>
-  <b-form-checkbox
-    v-model="localquicksave"
-    data-testid="CBQuickSave"
-    switch
-    @change="changeSaveMode(localquicksave)"
-  >
-    <span class="sr-only">{{ localquicksave ? $t('form.quicksave.enable'): $t('form.quicksave.disable') }}</span>
-  </b-form-checkbox>
+  <div>
+    <b-alert v-if="changesProducts.filter(o => o.user === username).length>0 && !quicksave" show variant="danger">
+      {{ $t('message.error.unsavedChanges') }}
+    </b-alert>
+    <b-form-checkbox
+      v-else
+      v-model="localquicksave"
+      data-testid="CBQuickSave"
+      switch
+      @change="changeSaveMode(localquicksave)"
+    >
+      <span class="sr-only">{{ localquicksave ? $t('form.quicksave.enable'): $t('form.quicksave.disable') }}</span>
+    </b-form-checkbox>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, namespace, Vue } from 'nuxt-property-decorator'
+import { ChangeObj } from '../../.utils/types/tchanges'
 const settings = namespace('settings')
+const auth = namespace('auth')
+const changes = namespace('changes')
 
 @Component
 export default class CBQuickSave extends Vue {
@@ -19,6 +28,9 @@ export default class CBQuickSave extends Vue {
 
   @settings.Getter public quicksave!: boolean
   @settings.Mutation public setQuicksave!: (isQuicksave: boolean) => void
+  @auth.Getter public username!: string
+  @changes.Getter public changesProducts!: Array<ChangeObj>
+  $t: any
 
   beforeMount () {
     this.localquicksave = this.quicksave
