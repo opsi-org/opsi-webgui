02ae42e65a39faf9760dd575d9935007a2ec5f6f
Jeena John <j.john@uib.de>
2022-09-21 15:25:05 +0000

    [fix] performance issue in editable multivalue configs

--- components/select/SUnicodeParam.vue
+++ components/select/SUnicodeParam.vue
@@ -1,25 +1,15 @@
 <template>
   <div>
-    <span v-if="row.multiValue || row.editable"> {{ row }} OPTIONS::: {{ options }}</span>
-    <b-form-select v-else v-model="localval" :options="options" />
-    <!-- <b-form-select v-if="!row.multiValue && !row.editable" v-model="localval" :options="options" />
-    <span v-else> {{ row }}</span> -->
-    <!-- <treeselect :multiple="row.multiValue" :options="options" /> -->
-    <!-- <b-row>
-      <b-col>
-        <b-form-select v-model="localval" :multiple="row.multiValue" :options="row.possibleValues" />
-      </b-col>
-      <b-col v-if="row.editable">
-        <b-form-input v-model="newVal" :placeholder="$t('Type new value and press ENTER')" />
-      </b-col>
-    </b-row> -->
+    <b-input-group>
+      <b-form-select v-model="localval" :multiple="row.multiValue" :options="row.possibleValues" />
+      <b-button v-if="row.editable" v-b-tooltip.hover :pressed.sync="hide" variant="transparent" :title="$t('Add new value')">
+        {{ $t('+') }}
+      </b-button>
+      <b-form-input :class="hide ? 'd-none' : 'd-block'" :placeholder="$t('Type new value and press ENTER here')" />
+    </b-input-group>
+    <!-- <treeselect v-if="row.multiValue || row.editable" v-model="localval" :multiple="row.multiValue" :options="options" />
+    <b-form-select v-else v-model="localval" :options="options" /> -->
   </div>
-  <!-- <b-form-checkbox
-    v-model="localboolval"
-    data-testid="CBBoolParam"
-    class="CBBoolParam"
-    @change="$emit('update:boolval', localboolval)"
-  /> -->
 </template>
 
 <script lang="ts">
@@ -29,6 +19,7 @@ import { Component, Prop, Vue } from 'nuxt-property-decorator'
 export default class SUnicodeParam extends Vue {
   @Prop() row!: any
   @Prop() type!: String
+  hide: boolean = true
 
   get localval () {
     if (this.type === 'clients') {
@@ -47,18 +38,45 @@ export default class SUnicodeParam extends Vue {
     }
   }
 
-  get options () {
-    if (this.row.multiValue || this.row.editable) {
-      const possiblevalues: any = []
-      for (const val in this.row.possibleValues) {
-        const v = this.row.possibleValues[val]
-        possiblevalues.push({ id: v, label: v })
-      }
-      return possiblevalues
-    } else {
-      return this.row.possibleValues
-    }
-  }
+  // get localval () {
+  //   let values: any = []
+  //   if (this.type === 'clients') {
+  //     if (this.row.clients) {
+  //       values = Object.values(this.row.clients)[0]
+  //     } else {
+  //       values = this.row.defaultValue
+  //     }
+  //   } else {
+  //     values = this.row.value
+  //   }
+  //   if (this.row.multiValue || this.row.editable) {
+  //     const val: any = []
+  //     if (values.length > 1) {
+  //       for (const v in values) {
+  //         const value = this.row.possibleValues[v]
+  //         val.push({ id: value, label: value })
+  //       }
+  //       return val
+  //     } else {
+  //       return []
+  //     }
+  //   } else {
+  //     return values
+  //   }
+  // }
+
+  // get options () {
+  //   if (this.row.multiValue || this.row.editable) {
+  //     const possiblevalues: any = []
+  //     for (const val in this.row.possibleValues) {
+  //       const v = this.row.possibleValues[val]
+  //       possiblevalues.push({ id: v, label: v })
+  //     }
+  //     return possiblevalues
+  //   } else {
+  //     return this.row.possibleValues
+  //   }
+  // }
 }
 </script>
 
