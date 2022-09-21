f6c010c47a83054f0a8493db33d2cfa1ef639d0e
Jeena John <j.john@uib.de>
2022-09-21 11:27:28 +0000

    [chg] get options

--- components/select/SUnicodeParam.vue
+++ components/select/SUnicodeParam.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <!-- {{ row }} -->
-    <b-row>
+    <span v-if="row.multiValue || row.editable"> {{ row }} OPTIONS::: {{ options }}</span>
+    <b-form-select v-else v-model="localval" :options="options" />
+    <!-- <b-form-select v-if="!row.multiValue && !row.editable" v-model="localval" :options="options" />
+    <span v-else> {{ row }}</span> -->
+    <!-- <treeselect :multiple="row.multiValue" :options="options" /> -->
+    <!-- <b-row>
       <b-col>
         <b-form-select v-model="localval" :multiple="row.multiValue" :options="row.possibleValues" />
       </b-col>
       <b-col v-if="row.editable">
         <b-form-input v-model="newVal" :placeholder="$t('Type new value and press ENTER')" />
       </b-col>
-    </b-row>
+    </b-row> -->
   </div>
   <!-- <b-form-checkbox
     v-model="localboolval"
@@ -25,19 +29,34 @@ import { Component, Prop, Vue } from 'nuxt-property-decorator'
 export default class SUnicodeParam extends Vue {
   @Prop() row!: any
   @Prop() type!: String
+
   get localval () {
     if (this.type === 'clients') {
       if (this.row.clients) {
+        const val: any = Object.values(this.row.clients)[0]
         if (this.row.multiValue) {
-          return Object.values(this.row.clients)[0]
+          return val
         } else {
-          return Object.values(this.row.clients)[0][0]
+          return val[0]
         }
       } else {
         return this.row.defaultValue
       }
     } else {
       return this.row.value
+    }
+  }
+
+  get options () {
+    if (this.row.multiValue || this.row.editable) {
+      const possiblevalues: any = []
+      for (const val in this.row.possibleValues) {
+        const v = this.row.possibleValues[val]
+        possiblevalues.push({ id: v, label: v })
+      }
+      return possiblevalues
+    } else {
+      return this.row.possibleValues
     }
   }
 }
