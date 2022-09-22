42ebf58822fcd6779b6a439e0f450ee1400a074f
Jeena John <j.john@uib.de>
2022-09-22 10:39:55 +0000

    [new] add editable vue treeselect

--- components/select/SUnicodeParam.vue
+++ components/select/SUnicodeParam.vue
@@ -1,15 +1,12 @@
 <template>
-  <div>
-    <b-input-group>
-      <b-form-select v-model="localval" :multiple="row.multiValue" :options="row.possibleValues" />
-      <b-button v-if="row.editable" v-b-tooltip.hover :pressed.sync="hide" variant="transparent" :title="$t('Add new value')">
-        {{ $t('+') }}
-      </b-button>
-      <b-form-input :class="hide ? 'd-none' : 'd-block'" :placeholder="$t('Type new value and press ENTER here')" />
-    </b-input-group>
-    <!-- <treeselect v-if="row.multiValue || row.editable" v-model="localval" :multiple="row.multiValue" :options="options" />
-    <b-form-select v-else v-model="localval" :options="options" /> -->
-  </div>
+  <TreeTSDefaultWithAdding
+    v-model="localval"
+    :multiple="row.multiValue"
+    :editable="row.editable"
+    :options="options"
+    @new-node="addNewValue"
+    :no-results-text="row.editable? 'No results found... Press enter to add' : 'No results found'"
+  />
 </template>
 
 <script lang="ts">
@@ -20,8 +17,20 @@ export default class SUnicodeParam extends Vue {
   @Prop() row!: any
   @Prop() type!: String
   hide: boolean = true
+  options: Array<object> = []
+  localval:any
 
-  get localval () {
+  beforeMount() {
+    const possiblevalues: any = []
+    for (const val in this.row.possibleValues) {
+      const v = this.row.possibleValues[val]
+      possiblevalues.push({ id: v, label: v })
+    }
+    this.options = possiblevalues
+    this.localval = this.getlocalvalue()
+  }
+
+  getlocalvalue () {
     if (this.type === 'clients') {
       if (this.row.clients) {
         const val: any = Object.values(this.row.clients)[0]
@@ -37,46 +46,14 @@ export default class SUnicodeParam extends Vue {
       return this.row.value
     }
   }
-
-  // get localval () {
-  //   let values: any = []
-  //   if (this.type === 'clients') {
-  //     if (this.row.clients) {
-  //       values = Object.values(this.row.clients)[0]
-  //     } else {
-  //       values = this.row.defaultValue
-  //     }
-  //   } else {
-  //     values = this.row.value
-  //   }
-  //   if (this.row.multiValue || this.row.editable) {
-  //     const val: any = []
-  //     if (values.length > 1) {
-  //       for (const v in values) {
-  //         const value = this.row.possibleValues[v]
-  //         val.push({ id: value, label: value })
-  //       }
-  //       return val
-  //     } else {
-  //       return []
-  //     }
-  //   } else {
-  //     return values
-  //   }
-  // }
-
-  // get options () {
-  //   if (this.row.multiValue || this.row.editable) {
-  //     const possiblevalues: any = []
-  //     for (const val in this.row.possibleValues) {
-  //       const v = this.row.possibleValues[val]
-  //       possiblevalues.push({ id: v, label: v })
-  //     }
-  //     return possiblevalues
-  //   } else {
-  //     return this.row.possibleValues
-  //   }
-  // }
+  addNewValue(newval:any) {
+    // console.error('new value', newval)
+    if(this.row.editable){
+      const value = newval.text
+      this.options = [...this.options, { id: value, label: value }]
+      this.localval.push(value)
+    }
+  }
 }
 </script>
 
