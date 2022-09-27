899fd1582471c6bd72e368d0a4843d644a5d5006
Jeena John <j.john@uib.de>
2022-09-27 14:31:14 +0000

    [fix] performance issue; use local filter and grid component

--- components/table/THostParam.vue
+++ components/table/THostParam.vue
@@ -10,18 +10,17 @@
       <span v-for="v,k in hostParam" :key="k">
         <b-button v-b-toggle="'collapse-'+k" class="text-left font-weight-bold border-0" block variant="outline-primary">{{ k }}</b-button>
         <b-collapse :id="'collapse-'+k" :visible="filter === '' ? false : true">
-          <!-- <span v-for="item in v" :key="item.configId">
+          <span v-for="item in v" :key="item.configId" :class="{ 'd-none': !item.configId.includes(filter) }">
             <GridGFormItem>
               <template #label>
                 {{ item.configId }}
               </template>
               <template #value>
-                <CheckboxCBBoolParam v-if="item.type === 'BoolConfig'" :type="type" :row="item" />
-                <TreeTSUnicodeParam v-else :type="type" :row="item" />
+                <TableCellTCHostParam :configtype="item.type" :type="type" :row="item" @change="handleSelection" />
               </template>
             </GridGFormItem>
-          </span> -->
-          <LazyTableTDefault
+          </span>
+          <!-- <LazyTableTDefault
             v-if="v"
             :noheader="true"
             :fixed="true"
@@ -40,10 +39,8 @@
             </template>
             <template #cell(action)="row">
               <TableCellTCHostParam :configtype="row.item.type" :type="type" :row="row.item" @change="handleSelection" />
-              <!-- <CheckboxCBBoolParam v-if="row.item.type === 'BoolConfig'" :type="type" :row="row.item" />
-              <TreeTSUnicodeParam v-else :type="type" :row="row.item" /> -->
             </template>
-          </LazyTableTDefault>
+          </LazyTableTDefault> -->
         </b-collapse>
       </span>
     </DivDScrollResult>
