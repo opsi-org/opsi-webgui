ebcd7b3668ea964bdc93cc0a3a9a90a57d12aa52
Jeena John <j.john@uib.de>
2022-09-29 16:20:45 +0000

    [new] story for bool and unicode configs

---
+++ components/grid/cell/GCHostParam.stories.js
@@ -0,0 +1,46 @@
+export default {
+  title: 'Gridcell/G C Host Parameter value',
+  parameters: { docs: { description: { component: 'Grid cell for Host param value' } } }
+}
+
+const BoolConfig = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  computed: { args () { return args } },
+  template: '<GridCellGCHostParam v-bind="args" />'
+})
+
+export const GCHostParamValueBool = BoolConfig.bind({})
+GCHostParamValueBool.args = {
+  configtype: 'BoolConfig',
+  type: 'depots',
+  row: {
+    configId: 'license-management.use',
+    description: 'Activate license management',
+    type: 'BoolConfig',
+    value: true,
+    possibleValues: [false, true, true, false],
+    multiValue: false,
+    editable: false
+  }
+}
+
+const UnicodeConfig = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  computed: { args () { return args } },
+  template: '<GridCellGCHostParam v-bind="args" />'
+})
+
+export const GCHostParamValueUnicode = UnicodeConfig.bind({})
+GCHostParamValueUnicode.args = {
+  configtype: 'UnicodeConfig',
+  type: 'depots',
+  row: {
+    configId: 'product_sort_algorithm',
+    description: '',
+    type: 'UnicodeConfig',
+    value: 'algorithm2',
+    possibleValues: ['algorithm1', 'algorithm2'],
+    multiValue: false,
+    editable: false
+  }
+}
