7f6185f7498a21693187baafca32da3bc39f4c42
Jeena John <j.john@uib.de>
2022-10-12 10:59:08 +0000

    [fix] nuxt build errors

--- components/dropdown/DDTableColumnVisibility.vue
+++ components/dropdown/DDTableColumnVisibility.vue
@@ -247,13 +247,13 @@ export default class DDTableColumnVisibility extends BDropdown {
 
   onOver () {
     if (this.$refs.dropdown) {
-      this.$refs.dropdown.visible = true
+      (this.$refs.dropdown as any).visible = true
     }
   }
 
   onLeave () {
     if (this.$refs.dropdown) {
-      this.$refs.dropdown.visible = false
+      (this.$refs.dropdown as any).visible = false
     }
   }
 }
