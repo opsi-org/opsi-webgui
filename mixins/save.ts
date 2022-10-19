ea9a5f1825b24915b55f40eeb34e37e35c7c0116
Jeena John <j.john@uib.de>
2022-10-19 14:11:33 +0000

    [new] add mixins for saveParameter

---
+++ mixins/save.ts
@@ -0,0 +1,21 @@
+import { Component, namespace, Vue } from 'nuxt-property-decorator'
+import { makeToast } from '../.utils/utils/scomponents'
+
+const changes = namespace('changes')
+
+@Component export class CallLogout extends Vue {
+  @changes.Mutation public delFromChangesHostParam!: (s: object) => void
+  async saveParameters (url: string, request: any, item:any, tracking:boolean) {
+    // const ref = (this.$refs.configViewAlert as any)
+    await this.$axios.$post(url, request)
+      .then(() => {
+        if (tracking) {
+          this.delFromChangesHostParam(item)
+        }
+      }).catch((error) => {
+        const detailedError = ((error?.response?.data?.message) ? error.response.data.message : '') + ' ' + ((error?.response?.data?.details) ? error.response.data.details : '')
+        // ref.alert(this.$t('message.error.fetch') as string + ' Config', 'danger', detailedError)
+        makeToast(this, detailedError, this.$t('message.error.title') as string, 'danger', 8000)
+      })
+  }
+}
