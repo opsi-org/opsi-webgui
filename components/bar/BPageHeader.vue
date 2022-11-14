b2c5f0a4a9e410e9ff26a4e521cec9c9879e4f3f
Jeena John <j.john@uib.de>
2022-11-14 16:28:31 +0000

    [fix] add a minimal pageheader

--- components/bar/BPageHeader.vue
+++ components/bar/BPageHeader.vue
@@ -1,41 +1,61 @@
 <template>
-  <b-navbar class="pageheader_navbar" data-testid="BarBPageHeader" :variant="variant">
-    <span v-if="navbartype == 'collapse'">
-      <b-icon v-if="collapsed" :icon="iconnames.arrowDoubleDown" />
-      <b-icon v-else :icon="iconnames.arrowDoubleRight" />
-    </span>
-    <b v-if="title && bold">{{ title }}</b>
-    <p v-else-if="title && !bold">
-      {{ title }}
-    </p>
-    <span v-if="subtitle" class="font-italic ml-1">
-      {{ subtitle }}
-    </span>
-    <slot name="left" />
-    <b-navbar-nav class="ml-auto">
-      <slot name="right" />
-      <b-button v-if="closeroute" class="border-0" variant="outline-primary" :to="closeroute">
-        <span class="sr-only">{{ $t('button.close') }}</span>
-        <b-icon :icon="iconnames.x" />
-      </b-button>
-    </b-navbar-nav>
-  </b-navbar>
+  <div>
+    <b-navbar data-testid="BarBPageHeader" variant="transparent" class="pageheader_navbar pt-0">
+      <div v-b-toggle="'collapse' + tableid" :class="navbartype == 'collapse' ? 'btn w-75 text-left border-0 pl-0' : ''">
+        <template v-if="navbartype == 'collapse'">
+          <b-icon v-if="expanded" :icon="iconnames.arrowDoubleDown" />
+          <b-icon v-else :icon="iconnames.arrowDoubleRight" />
+        </template>
+        <b v-if="title">{{ title }}</b>
+        <span v-if="subtitle" class="font-italic ml-1">
+          {{ subtitle }}
+        </span>
+        <slot name="left" />
+      </div>
+      <b-navbar-nav class="ml-auto">
+        <slot name="right" />
+        <b-button v-if="closeroute" class="border-0" variant="outline-primary" :to="closeroute">
+          <span class="sr-only">{{ $t('button.close') }}</span>
+          <b-icon :icon="iconnames.x" />
+        </b-button>
+      </b-navbar-nav>
+    </b-navbar>
+    <b-collapse v-if="navbartype == 'collapse' || $mq == 'mobile'" :id="'collapse' + tableid" :visible="navbartype == 'expand'">
+      <b-navbar class="pageheader_navbar mb-1">
+        <template v-if="navbartype == 'collapse'">
+          <TreeTSDepots v-if="tableid !== 'Depots'" class="tableheader_depots" />
+          <TreeTSHostGroups v-if="tableid !== 'Depots'" class="tableheader_hostgroup" />
+          <TreeTSProductGroups v-if="tableid == 'products'" class="tableheader_productgroup" />
+        </template>
+        <InputIFilter v-if="$mq == 'mobile'" class="header_filter" :data="tableInfo" />
+        <b-navbar-nav v-if="$mq == 'mobile'" class="ml-auto">
+          <DropdownDDTableSorting :table-id="tableid" v-bind.sync="tableInfo" />
+          <DropdownDDTableColumnVisibility :table-id="tableid" :headers.sync="tableInfo.headerData" :sort-by="tableInfo.sortBy" :multi="true" />
+          <ButtonBTNRefetch :is-loading="isLoadingParent" :tooltip="$t('button.refresh', {id: tableid})" :refetch="fetch" />
+        </b-navbar-nav>
+      </b-navbar>
+    </b-collapse>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import { Constants } from '../../mixins/uib-mixins'
+import { ITableInfo } from '../../.utils/types/ttable'
 
 @Component({ mixins: [Constants] })
 export default class BPageHeader extends Vue {
-  iconnames:any
-  @Prop({ default: 'transparent' }) variant!: string
-  @Prop({ default: true }) bold!: boolean
-  @Prop({}) navbartype!: string
-  @Prop({}) collapsed!: boolean
+  @Prop({ default: 'expand' }) navbartype!: string
   @Prop({}) title!: string
   @Prop({}) subtitle!: string
   @Prop({}) closeroute!: string
+  @Prop({}) tableid!: string
+  @Prop({ default: () => { return {} } }) tableInfo!: ITableInfo
+  @Prop({ default: false }) isLoadingParent!: boolean
+  @Prop({ default: undefined }) fetch!: Function
+  iconnames:any
+  $mq: any
+  expanded: boolean = true
 }
 </script>
 
@@ -43,8 +63,8 @@ export default class BPageHeader extends Vue {
 .pageheader_navbar.navbar-expand {
   flex-flow: row wrap;
 }
-.pageheader_navbar {
+/* .pageheader_navbar {
   padding: 5px 0px !important;
   margin-bottom: 10px;
-}
+} */
 </style>
