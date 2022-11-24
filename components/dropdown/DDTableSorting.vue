fbeb460e201ba1774ad9647ac068be9e08a9011f
Jeena John <j.john@uib.de>
2022-11-24 15:46:33 +0000

    [chg] hoverable menu on contextmenu

--- components/dropdown/DDTableSorting.vue
+++ components/dropdown/DDTableSorting.vue
@@ -1,35 +1,43 @@
 <template>
-  <b-dropdown
-    v-bind="$props"
-    data-testid="DropdownDDTableSorting"
-    variant="outline-primary border-0"
-    :no-caret="incontextmenu == false"
-    :title="$t('button.sort.tablecolumns')"
-    :class="{ 'rightmenu': $mq == 'mobile' }"
+  <div
+    @mouseover="incontextmenu ? onOver($refs.dropdown) : null"
+    @mouseleave="incontextmenu ? onLeave($refs.dropdown) : null"
+    @focusin="incontextmenu ? onOver($refs.dropdown) : null"
+    @focusout="incontextmenu ? onLeave($refs.dropdown) : null"
   >
-    <template #button-content>
-      <b-icon :icon="(sortDesc)? iconnames.sortDesc: iconnames.sort" />
-      <small v-if="incontextmenu !== false">{{ $t('button.sort.tablecolumns.title') }}</small>
-    </template>
-    <b-dropdown-form
-      inline
-      @keydown.prevent="changeSortDirection()"
-      @click.prevent="changeSortDirection()"
+    <b-dropdown
+      v-bind="$props"
+      ref="dropdown"
+      data-testid="DropdownDDTableSorting"
+      variant="outline-primary border-0"
+      :no-caret="!incontextmenu"
+      :title="$t('button.sort.tablecolumns')"
+      :class="{ 'rightmenu': $mq == 'mobile' }"
     >
-      <b-form-checkbox :aria-label="$t('button.sort.tablecolumns.sortDirection')" :checked="sortDesc" />
-      <span class="sortDirection"> {{ $t('button.sort.tablecolumns.sortDirection') }} </span>
-    </b-dropdown-form>
-    <li class="li-delimiter" />
-    <b-dropdown-item
-      v-for="header in Object.values(headerData).filter(h=>h.sortable)"
-      :key="header.key"
-      :class="{'selectedSort': (sortBy==header.key)}"
-      @keydown="changeSortBy(header.key)"
-      @click="changeSortBy(header.key)"
-    >
-      {{ header.label }}
-    </b-dropdown-item>
-  </b-dropdown>
+      <template #button-content>
+        <b-icon :icon="(sortDesc)? iconnames.sortDesc: iconnames.sort" />
+        <small v-if="incontextmenu">{{ $t('button.sort.tablecolumns.title') }}</small>
+      </template>
+      <b-dropdown-form
+        inline
+        @keydown.prevent="changeSortDirection()"
+        @click.prevent="changeSortDirection()"
+      >
+        <b-form-checkbox :aria-label="$t('button.sort.tablecolumns.sortDirection')" :checked="sortDesc" />
+        <span class="sortDirection"> {{ $t('button.sort.tablecolumns.sortDirection') }} </span>
+      </b-dropdown-form>
+      <li class="li-delimiter" />
+      <b-dropdown-item
+        v-for="header in Object.values(headerData).filter(h=>h.sortable)"
+        :key="header.key"
+        :class="{'selectedSort': (sortBy==header.key)}"
+        @keydown="changeSortBy(header.key)"
+        @click="changeSortBy(header.key)"
+      >
+        {{ header.label }}
+      </b-dropdown-item>
+    </b-dropdown>
+  </div>
 </template>
 
 <script lang="ts">
@@ -40,15 +48,14 @@ import { Constants, HoverDropdown } from '../../mixins/uib-mixins'
 
 @Component({ mixins: [Constants, HoverDropdown] })
 export default class DDTableSorting extends BDropdown {
+  @Prop({ default: '' }) sortBy!: string
+  @Prop({ default: false }) sortDesc!: boolean
+  @Prop({ default: false }) incontextmenu!: boolean
+  @Prop({ default: () => { return () => { /* default */ } } }) headerData!: ITableHeader
   $mq:any
   iconnames: any
   onOver:any
   onLeave:any
-  @Prop({ default: '' }) sortBy!: string
-  @Prop({ default: false }) sortDesc!: boolean
-  @Prop({ default: false }) incontextmenu!: boolean
-  @Prop({ default: false }) onhover!: boolean
-  @Prop({ default: () => { return () => { /* default */ } } }) headerData!: ITableHeader
 
   changeSortDirection () { this.$emit('update:sortDesc', (!this.sortDesc)) }
   changeSortBy (key:string) { this.$emit('update:sortBy', key) }
