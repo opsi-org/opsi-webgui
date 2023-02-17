ee915519cb2a822b076b152742c9344adf1aa405
Jeena John <j.john@uib.de>
2023-02-17 13:01:47 +0000

    [new] testscript for logout button
    Former-commit-id: 076dcb5056fc07659b7b5fb4beb7fa7cc06bb448

---
+++ opsiweb/screenshots4Docs/take_screenshots/buttonlogout.test.screenshot.js
@@ -0,0 +1,10 @@
+const { test } = require('@playwright/test')
+const { callStoryId } = require('../../uib-components/.utils/playwright/pw-story-call')
+
+test('Button Logout', async ({ page }) => {
+  await callStoryId(page, 'button-btn-logout', 'btn-logout')
+  const component = await page.locator('[data-testid="ButtonBTNLogout"]')
+  await (new Promise(resolve => setTimeout(resolve, 1000)))
+  await component.screenshot({ path: './screenshots/en/opsiweb_buttonlogout.png' })
+  await component.screenshot({ path: './screenshots/de/opsiweb_buttonlogout.png' })
+})
