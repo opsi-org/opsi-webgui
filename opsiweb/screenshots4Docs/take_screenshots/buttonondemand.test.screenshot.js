17b2fe18932ef66c7d7ca94f6c33f594dc6e69c1
Jeena John <j.john@uib.de>
2023-02-21 14:22:36 +0000

    [chg] update scripts for screenshot automation
    Former-commit-id: ba00745fe356f396c1acbff7d53cacd27731e532

--- opsiweb/screenshots4Docs/take_screenshots/buttonondemand.test.screenshot.js
+++
@@ -1,9 +0,0 @@
-const { test } = require('@playwright/test')
-const { callStoryId } = require('../../uib-components/.utils/playwright/pw-story-call')
-test('Button on_demand', async ({ page }) => {
-  await callStoryId(page, 'button-btn-event', 'btn-event-on-demand-default')
-  const component = await page.locator('[data-testid="BTNEvent"]')
-  await (new Promise(resolve => setTimeout(resolve, 1000)))
-  await component.screenshot({ path: './screenshots/en/opsiweb_buttonondemand.png' })
-  await component.screenshot({ path: './screenshots/de/opsiweb_buttonondemand.png' })
-})
