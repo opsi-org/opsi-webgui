865ec391b92ab98c23966b51c120b59a96da8828
Jeena John <j.john@uib.de>
2023-02-23 16:07:42 +0000

    [chg] idselection for healthcheck page

--- components/view/VHealthCheck.vue
+++ components/view/VHealthCheck.vue
@@ -1,6 +1,11 @@
 <template>
   <div data-testid="VHealthCheck" class="VHealthCheck">
     <BarBPageHeader v-if="asChild" :title="$t('title.healthcheck') + ' - '" :subtitle="id" :closeroute="closeroute" />
+    <BarBPageHeader v-if="!asChild">
+      <template #left>
+        <slot name="IDSelection" />
+      </template>
+    </BarBPageHeader>
     <div> {{ $t('Health Check') }}</div>
   </div>
 </template>
