6e334ad72c36cf268c8400ff70a41ee2e3276428
Jeena John <j.john@uib.de>
2023-02-24 10:04:00 +0000

    [new] pages for clone client feature
    Former-commit-id: ecbaa7a8cd9fcd841e4f39b6b57c0ea19f800d37

---
+++ opsiweb/pages/clientsclone.vue
@@ -0,0 +1,15 @@
+<template>
+  <ViewVClientClone :id="id" :as-child="false">
+    <template slot="IDSelection">
+      <TreeTSClientsNotStored :id.sync="id" />
+    </template>
+  </ViewVClientClone>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+export default class PClientsClone extends Vue {
+  id: string = ''
+}
+</script>
