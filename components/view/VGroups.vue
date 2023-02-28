7817427bdf6e9cc2424baa9fe0e14ac64a72b71f
Jeena John <j.john@uib.de>
2023-02-28 15:09:25 +0000

    [chg] update groups overview

--- components/view/VGroups.vue
+++ components/view/VGroups.vue
@@ -11,119 +11,138 @@
 
   <!-- <IconILoading v-if="$fetchState.pending" :small="true" /> -->
   <div class="VGroups">
-    <!-- {{ dummygroup }} -->
-    <b-tabs data-testid="VGroups">
+    <IconILoading v-if="$fetchState.pending" :small="true" />
+    <b-tabs v-else data-testid="VGroups">
       <b-tab>
         <template #title>
           <span class="client"> {{ $t("form.clientgroups") }} </span>
         </template>
-        <b-row>
-          <b-col cols="4">
-            {{ $t('Select Server : ') }}<TreeTSDepotsNotStored :id.sync="depotId" /> <br>
-            {{ $t('Select Group or Client : ') }}<treeselect v-model="value" value-format="object" always-open :options="dummygroup" />
-          </b-col>
-          <b-col v-if="value">
-            <!-- {{ value }} -->
-            <div v-if="value.type == 'HostGroup'">
-              <span class="font-weight-bold">
-                {{ $t('Selected Group : ') }} {{ value.label }}
-              </span>
-              <b-tabs>
-                <b-tab>
-                  <template #title>
-                    <span class="client"> {{ $t("Add clients") }} </span>
-                  </template>
-                  {{ $t("Add clients to group") }} <br><br>
-                  <div>
-                    <span>{{ $t("Select Clients:   ") }}</span><TreeTSClientsNotStored :id.sync="clientIds" />
-                    <b-button variant="success">
-                      {{ $t("Add") }}
-                    </b-button>
+        <DivDScrollResult>
+          <b-row>
+            <b-col cols="4">
+              {{ $t('Select Server : ') }}<TreeTSDepotsNotStored :id.sync="depotId" /> <br>
+              {{ $t('Select Group or Client : ') }}
+              <treeselect
+                v-model="value"
+                class="treeselect_notstored treeselect_fullpage"
+                always-open
+                :default-expand-level="1"
+                :normalizer="normalizer"
+                value-format="object"
+                :options="group"
+              >
+                <div slot="option-label" slot-scope="{ node }">
+                  <div :ref="'tree-item-'+node.id">
+                    <b-icon v-if="node.isBranch" :icon="iconnames.group" />
+                    <b-icon v-else :icon="iconnames.client" />
+                    <small> {{ node.label }} </small>
                   </div>
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <span class="client"> {{ $t("Create subgroup") }} </span>
-                  </template>
-                  {{ $t("Create subgroup, doubt: along with clients ?") }} <br><br>
-                  <b-form inline>
-                    <b-form-input id="inline-form-input-username" :placeholder="$t('Enter Subgroup')" />
-                    <b-button variant="success">
-                      {{ $t("Create") }}
+                </div>
+              </treeselect>
+            </b-col>
+            <b-col v-if="value">
+              <!-- {{ value }} -->
+              <div v-if="value.type == 'HostGroup'">
+                <span class="font-weight-bold">
+                  {{ $t('Selected Group : ') }} {{ value.text }}
+                </span>
+                <b-tabs>
+                  <b-tab>
+                    <template #title>
+                      <span class="client"> {{ $t("Add clients") }} </span>
+                    </template>
+                    {{ $t("Add clients to group") }} <br><br>
+                    <div>
+                      <span>{{ $t("Select Clients:   ") }}</span><TreeTSClientsNotStored :id.sync="clientIds" />
+                      <b-button variant="success">
+                        {{ $t("Add") }}
+                      </b-button>
+                    </div>
+                  </b-tab>
+                  <b-tab>
+                    <template #title>
+                      <span class="client"> {{ $t("Create subgroup") }} </span>
+                    </template>
+                    {{ $t("Create subgroup, doubt: along with clients ?") }} <br><br>
+                    <b-form inline>
+                      <b-form-input id="inline-form-input-username" :placeholder="$t('Enter Subgroup')" />
+                      <b-button variant="success">
+                        {{ $t("Create") }}
+                      </b-button>
+                    </b-form>
+                  </b-tab>
+                  <b-tab>
+                    <template #title>
+                      <span class="client"> {{ $t("Edit properties") }} </span>
+                    </template>
+                    {{ $t("Edit group properties") }}
+                  </b-tab>
+                  <b-tab>
+                    <template #title>
+                      <span class="client"> {{ $t("Move group") }} </span>
+                    </template>
+                    {{ $t("Move group to different location") }}
+                    <div>
+                      <treeselect value-format="object" :options="group" />
+                      <b-button variant="primary">
+                        {{ $t("Move") }}
+                      </b-button>
+                    </div>
+                  </b-tab>
+                  <b-tab>
+                    <template #title>
+                      <span class="client"> {{ $t("Delete group") }} </span>
+                    </template>
+                    {{ $t("Delete group with subgroup and all client assignments") }}
+                    <b-button variant="danger">
+                      {{ $t("Delete") }}
                     </b-button>
-                  </b-form>
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <span class="client"> {{ $t("Edit properties") }} </span>
-                  </template>
-                  {{ $t("Edit group properties") }}
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <span class="client"> {{ $t("Move group") }} </span>
-                  </template>
-                  {{ $t("Move group to different location") }}
-                  <div>
-                    <treeselect value-format="object" :options="dummygroup" />
-                    <b-button variant="primary">
-                      {{ $t("Move") }}
+                  </b-tab>
+                  <b-tab>
+                    <template #title>
+                      <span class="client"> {{ $t("Delete client assignments") }} </span>
+                    </template>
+                    {{ $t("Only delete client assignments") }}
+                    <b-button variant="danger">
+                      {{ $t("Delete") }}
                     </b-button>
-                  </div>
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <span class="client"> {{ $t("Delete group") }} </span>
-                  </template>
-                  {{ $t("Delete group with subgroup and all client assignments") }}
-                  <b-button variant="danger">
-                    {{ $t("Delete") }}
-                  </b-button>
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <span class="client"> {{ $t("Delete client assignments") }} </span>
-                  </template>
-                  {{ $t("Only delete client assignments") }}
-                  <b-button variant="danger">
-                    {{ $t("Delete") }}
-                  </b-button>
-                </b-tab>
-              </b-tabs>
-            </div>
-            <div v-if="value.type == 'ObjectToGroup'">
-              <span class="font-weight-bold">
-                {{ $t('Selected Client : ') }} {{ value.label }}
-              </span>
-              <b-tabs>
-                <b-tab>
-                  <template #title>
-                    <span class="client"> {{ $t("Move") }} </span>
-                  </template>
-                  {{ $t("Move to another group") }}
-                  <div>
-                    <treeselect value-format="object" :options="dummygroup" />
-                    <b-button variant="primary">
-                      {{ $t("Move") }}
-                    </b-button>
-                  </div>
-                </b-tab>
-                <b-tab>
-                  <template #title>
-                    <span class="client"> {{ $t("Copy") }} </span>
-                  </template>
-                  {{ $t("Copy to another group") }}
-                  <div>
-                    <treeselect value-format="object" :options="dummygroup" />
-                    <b-button variant="primary">
-                      {{ $t("Copy") }}
-                    </b-button>
-                  </div>
-                </b-tab>
-              </b-tabs>
-            </div>
-          </b-col>
-        </b-row>
+                  </b-tab>
+                </b-tabs>
+              </div>
+              <div v-if="value.type == 'ObjectToGroup'">
+                <span class="font-weight-bold">
+                  {{ $t('Selected Client : ') }} {{ value.text }}
+                </span>
+                <b-tabs>
+                  <b-tab>
+                    <template #title>
+                      <span class="client"> {{ $t("Move") }} </span>
+                    </template>
+                    {{ $t("Move to another group") }}
+                    <div>
+                      <treeselect value-format="object" :options="group" />
+                      <b-button variant="primary">
+                        {{ $t("Move") }}
+                      </b-button>
+                    </div>
+                  </b-tab>
+                  <b-tab>
+                    <template #title>
+                      <span class="client"> {{ $t("Copy") }} </span>
+                    </template>
+                    {{ $t("Copy to another group") }}
+                    <div>
+                      <treeselect value-format="object" :options="group" />
+                      <b-button variant="primary">
+                        {{ $t("Copy") }}
+                      </b-button>
+                    </div>
+                  </b-tab>
+                </b-tabs>
+              </div>
+            </b-col>
+          </b-row>
+        </DivDScrollResult>
         <!-- {{ group }} -->
       </b-tab>
       <!-- <b-tab>
@@ -137,78 +156,82 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import { Constants } from '../../mixins/uib-mixins'
 // const selections = namespace('selections')
 // interface ClientRequest {
 //     selectedDepots: string
 // }
-@Component
+@Component({ mixins: [Constants] })
 export default class VGroups extends Vue {
+  iconnames: any
   depotId: string = ''
-  group: any
+  group: Array<object> = []
   value: any = null
   clientIds: any
-  dummygroup: any = [
-    {
-      id: 'groups',
-      label: 'groups',
-      type: 'HostGroup',
-      children: [
-        {
-          id: 'testgroup1;groups',
-          label: 'testgroup1',
-          type: 'HostGroup',
-          children: [
-            {
-              id: 'client1.uib.local;testgroup1',
-              label: 'client1.uib.local',
-              type: 'ObjectToGroup'
-            }
-          ]
-        },
-        {
-          id: 'testgroup2;groups',
-          label: 'testgroup2',
-          type: 'HostGroup',
-          children: [
-            {
-              id: 'client1.uib.local;testgroup2',
-              label: 'client1.uib.local',
-              type: 'ObjectToGroup'
-            },
-            {
-              id: 'client2.uib.local;testgroup2',
-              label: 'client2.uib.local',
-              type: 'ObjectToGroup'
-            },
-            {
-              id: 'client3.uib.local;testgroup2',
-              label: 'client3.uib.local',
-              type: 'ObjectToGroup'
-            }
-          ]
-        }
-      ]
-    },
-    {
-      id: 'clientdirectory',
-      label: 'clientdirectory',
-      type: 'HostGroup',
-      children: [{}]
+  $axios: any
+  node: any
+
+  normalizer (node: any) {
+    return {
+      id: node.id,
+      label: node.text,
+      children: node.children ? Object.values(node.children) : (node.type === 'HostGroup' ? [] : undefined)
     }
-  ]
+  }
 
-  // normalizer (node: any) {
-  //   return {
-  //     id: node.id,
-  //     label: node.text,
-  //     children: [node.children]
-  //   }
-  // }
-
-  // fetch () {
-  //   const result = await this.$axios.$get('/api/opsidata/hosts/groups')
-  //   this.group = result
-  // }
+  async fetch () {
+    const result = await this.$axios.$get('/api/opsidata/hosts/groups')
+    this.group = Object.values(result)
+    // this.group = [
+    //   {
+    //     id: 'groups',
+    //     label: 'groups',
+    //     type: 'HostGroup',
+    //     children: [
+    //       {
+    //         id: 'testgroup1;groups',
+    //         label: 'testgroup1',
+    //         type: 'HostGroup',
+    //         children: [
+    //           {
+    //             id: 'client1.uib.local;testgroup1',
+    //             label: 'client1.uib.local',
+    //             type: 'ObjectToGroup'
+    //           }
+    //         ]
+    //       },
+    //       {
+    //         id: 'testgroup2;groups',
+    //         label: 'testgroup2',
+    //         type: 'HostGroup',
+    //         children: [
+    //           {
+    //             id: 'client1.uib.local;testgroup2',
+    //             label: 'client1.uib.local',
+    //             type: 'ObjectToGroup'
+    //           },
+    //           {
+    //             id: 'client2.uib.local;testgroup2',
+    //             label: 'client2.uib.local',
+    //             type: 'ObjectToGroup'
+    //           },
+    //           {
+    //             id: 'client3.uib.local;testgroup2',
+    //             label: 'client3.uib.local',
+    //             type: 'ObjectToGroup'
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 'clientdirectory',
+    //     label: 'clientdirectory',
+    //     type: 'HostGroup',
+    //     children: [{}]
+    //   }
+    // ]
+  }
 }
 </script>
 <style>
