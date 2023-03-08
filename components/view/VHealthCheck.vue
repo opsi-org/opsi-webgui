829361e12bc94fe5e21853ccbbf39c8c474003ba
Jeena John <j.john@uib.de>
2023-03-08 14:54:15 +0000

    [new] fetch healthcheck data

--- components/view/VHealthCheck.vue
+++ components/view/VHealthCheck.vue
@@ -6,7 +6,9 @@
         <slot name="IDSelection" />
       </template>
     </BarBPageHeader>
-    <div> {{ $t('Health Check') }}</div>
+    <AlertAAlert ref="healthCheckAlert" />
+    <OverlayOLoading :is-loading="$fetchState.pending" />
+    <div> {{ healthcheckdata }}</div>
   </div>
 </template>
 
@@ -14,8 +16,28 @@
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
 @Component
 export default class VHealthCheck extends Vue {
+  $axios: any
+  $t:any
   @Prop({ }) id!: string
   @Prop({ default: false }) 'asChild'!: string
   @Prop({ default: false }) 'closeroute'!: string
+
+  healthcheckdata: Array<object> = []
+  errorText: string = ''
+
+  async fetch () {
+    const request = { id: 1, jsonrpc: '2.0', method: 'service_healthCheck', params: [] }
+    const host = window.location.hostname
+    const baseURL = 'https://' + host + ':4447/rpc'
+    await this.$axios.$post(baseURL, request)
+      .then((response) => {
+        this.healthcheckdata = response.result
+      }).catch((error) => {
+        const detailedError = ((error?.response?.data?.message) ? error.response.data.message : '') + ' ' + ((error?.response?.data?.details) ? error.response.data.details : '')
+        const ref = (this.$refs.healthCheckAlert as any)
+        ref.alert(this.$t('message.error.fetch') as string + 'Health Check', 'danger', detailedError)
+        this.errorText = this.$t('message.error.defaulttext') as string
+      })
+  }
 }
 </script>
