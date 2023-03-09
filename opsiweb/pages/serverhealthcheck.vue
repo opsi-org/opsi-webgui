c74a453232cf736befbc45111c76ea1f67686d8d
Jeena John <j.john@uib.de>
2023-03-09 15:20:43 +0000

    [fix] navigation issue
    Former-commit-id: 157db85038da07d8f7613b87f0308e882a00a79f

--- opsiweb/pages/serverhealthcheck.vue
+++ opsiweb/pages/serverhealthcheck.vue
@@ -5,6 +5,6 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 @Component
-export default class PHealthCheck extends Vue {
+export default class PServerHealthCheck extends Vue {
 }
 </script>
