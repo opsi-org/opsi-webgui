578a85f3789fecbb6fc6d64ab94a09f7abd4292e
Fabian Kalweit <f.kalweit@uib.de>
2023-03-13 11:58:55 +0100

    [chg] add get diagnostic and health check
    - #561
    - GET /api/opsidata/server/diagnostic
    - GET /api/opsidata/server/health
    
    
    Former-commit-id: ffe544acf7186260c41ef55c16b7f7c9b86ac1ae

---
+++ backend/addon/webgui/python/server.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# opsiconfd is part of the desktop management solution opsi http://www.opsi.org
+# Copyright (c) 2020-2021 uib GmbH <info@uib.de>
+# All rights reserved.
+# License: AGPL-3.0
+"""
+webgui server methods
+"""
+
+from fastapi import APIRouter, Request, status
+
+
+from opsiconfd.logging import logger
+from opsiconfd.rest import (
+	OpsiApiException,
+	RESTResponse,
+	rest_api,
+)
+
+from .utils import backend
+
+
+server_router = APIRouter()
+
+
+@server_router.get("/api/opsidata/server/health")
+@rest_api
+def get_health_check(request: Request) -> RESTResponse:  # pylint: disable=unused-argument
+	"""
+	get server health
+	"""
+
+	try:
+		server_health = backend.service_healthCheck()
+
+	except Exception as err:  # pylint: disable=broad-except
+		logger.error("Could not get health check.")
+		logger.error(err)
+		raise OpsiApiException(
+			message="Could not get health check.",
+			http_status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+			error=err,
+		) from err
+
+	return RESTResponse(http_status=200, data=server_health)
+
+
+@server_router.get("/api/opsidata/server/diagnostic")
+@rest_api
+def get_diagnostic_data(request: Request) -> RESTResponse:  # pylint: disable=unused-argument
+	"""
+	get server diagnostic data
+	"""
+
+	try:
+		diagnostic_data = backend.service_getDiagnosticData()
+	except Exception as err:  # pylint: disable=broad-except
+		logger.error("Could not get diagnostic data.")
+		logger.error(err)
+		raise OpsiApiException(
+			message="Could not get diagnostic data.",
+			http_status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+			error=err,
+		) from err
+
+	return RESTResponse(http_status=200, data=diagnostic_data)
