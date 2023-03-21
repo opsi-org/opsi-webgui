8ebfd9a3dd9a2ca2109cc1d396f98f980e561909
Jeena John <j.john@uib.de>
2023-03-21 14:52:48 +0000

    [chg] change group actions

--- components/view/VGroups.vue
+++ components/view/VGroups.vue
@@ -1,15 +1,4 @@
 <template>
-  <!-- TODO: Fetch Hostgroups in given format, not lazy loaded. -->
-  <!-- TODO: If group selected: Add clients to group -->
-  <!-- TODO: If group selected: Move group to different location -->
-  <!-- TODO: If group selected: Create subgroup -->
-  <!-- TODO: If group selected: Edit group properties -->
-  <!-- TODO: If group selected: Delete group with subgroup and all client assignments -->
-  <!-- TODO: If group selected: Only delete client assignments -->
-  <!-- TODO: If client selected: Move to another group -->
-  <!-- TODO: If client selected: Copy to another group -->
-
-  <!-- <IconILoading v-if="$fetchState.pending" :small="true" /> -->
   <div class="VGroups">
     <OverlayOLoading :is-loading="$fetchState.pending" />
     <BarBPageHeader>
@@ -20,15 +9,13 @@
     <b-tabs data-testid="VGroups">
       <b-tab>
         <template #title>
-          <span class="client"> {{ $t("form.clientgroups") }} </span>
+          <span> {{ $t("form.clientgroups") }} </span>
         </template>
         <DivDScrollResult>
           <b-row>
             <b-col cols="4">
-              <!-- {{ $t('Select Server : ') }}<TreeTSDepotsNotStored :id.sync="depotId" /> <br> -->
-              <!-- {{ $t('Select Group or Client : ') }} -->
               <treeselect
-                v-model="value"
+                v-model="selectedvalue"
                 class="treeselect_notstored treeselect_fullpage"
                 always-open
                 :default-expand-level="1"
@@ -45,103 +32,137 @@
                 </div>
               </treeselect>
             </b-col>
-            <b-col v-if="value">
-              <!-- {{ value }} -->
-              <div v-if="value.type == 'HostGroup'">
+            <b-col v-if="selectedvalue">
+              <div v-if="selectedvalue.type == 'HostGroup'">
                 <span class="font-weight-bold">
-                  {{ $t('Selected Group : ') }} {{ value.text }}
+                  {{ $t('Selected Group : ') }} {{ selectedvalue.text }}
                 </span>
                 <b-tabs>
                   <b-tab>
                     <template #title>
-                      <span class="client"> {{ $t("Add clients") }} </span>
+                      <span v-b-tooltip.hover :title="$t('Add client to the selected group.')">
+                        {{ $t("Add client") }}
+                      </span>
                     </template>
-                    {{ $t("Add clients to group") }} <br><br>
-                    <div>
-                      <span>{{ $t("Select Clients:   ") }}</span><TreeTSClientsNotStored :id.sync="clientIds" />
-                      <b-button variant="success">
+                    <br>
+                    <b-form>
+                      <b-form-select v-b-tooltip.hover :title="$t('Select a Client')" :options="clientIds">
+                        <template #first>
+                          <b-form-select-option :value="null" disabled>
+                            {{ $t('Select a Client') }}
+                          </b-form-select-option>
+                        </template>
+                      </b-form-select>
+                      <b-button class="float-right" variant="success">
                         {{ $t("Add") }}
                       </b-button>
-                    </div>
+                    </b-form>
                   </b-tab>
                   <b-tab>
                     <template #title>
-                      <span class="client"> {{ $t("Create subgroup") }} </span>
+                      <span v-b-tooltip.hover :title="$t('Create subgroup for the selected group.')">
+                        {{ $t("Create subgroup") }}
+                      </span>
                     </template>
-                    {{ $t("Create subgroup, doubt: along with clients ?") }} <br><br>
-                    <b-form inline>
-                      <b-form-input id="inline-form-input-username" :placeholder="$t('Enter Subgroup')" />
-                      <b-button variant="success">
+                    <br>
+                    <b-form>
+                      <b-form-input v-b-tooltip.hover :title="$t('Subgroup ID')" :placeholder="$t('Subgroup ID')" />
+                      <b-form-input v-b-tooltip.hover :title="$t('Description')" :placeholder="$t('Description')" />
+                      <b-form-input v-b-tooltip.hover :title="$t('Notes')" :placeholder="$t('Notes')" />
+                      <b-button class="float-right" variant="success">
                         {{ $t("Create") }}
                       </b-button>
                     </b-form>
                   </b-tab>
                   <b-tab>
                     <template #title>
-                      <span class="client"> {{ $t("Edit properties") }} </span>
+                      <span v-b-tooltip.hover :title="$t('Move the selected group to a new parent or update its attributes.')">
+                        {{ $t("Update group") }}
+                      </span>
                     </template>
-                    {{ $t("Edit group properties") }}
-                  </b-tab>
-                  <b-tab>
-                    <template #title>
-                      <span class="client"> {{ $t("Move group") }} </span>
-                    </template>
-                    {{ $t("Move group to different location") }}
+                    <br>
                     <div>
-                      <treeselect value-format="object" :options="group" />
-                      <b-button variant="primary">
-                        {{ $t("Move") }}
+                      <treeselect
+                        v-b-tooltip.hover
+                        :title="$t('Parent Group ID')"
+                        :placeholder="$t('Parent Group ID')"
+                        value-format="object"
+                        :options="group"
+                        :normalizer="normalizer"
+                      />
+                      <b-form-input v-b-tooltip.hover :title="$t('Description')" :placeholder="$t('Description')" />
+                      <b-form-input v-b-tooltip.hover :title="$t('Notes')" :placeholder="$t('Notes')" />
+                      <b-button class="float-right" variant="success">
+                        {{ $t("Update") }}
                       </b-button>
                     </div>
                   </b-tab>
                   <b-tab>
                     <template #title>
-                      <span class="client"> {{ $t("Delete group") }} </span>
+                      <span v-b-tooltip.hover :title="$t('Delete selected group along with subgroups and client assignments.')">
+                        {{ $t("Delete group") }}
+                      </span>
                     </template>
-                    {{ $t("Delete group with subgroup and all client assignments") }}
-                    <b-button variant="danger">
+                    <br>
+                    <span> {{ $t('Are you sure you want to delete the selected group along with its subgroups and client assignments?') }}</span>
+                    <b-button class="float-right" variant="danger">
                       {{ $t("Delete") }}
                     </b-button>
                   </b-tab>
                   <b-tab>
                     <template #title>
-                      <span class="client"> {{ $t("Delete client assignments") }} </span>
+                      <span v-b-tooltip.hover :title="$t('Remove client from the selected group.')">
+                        {{ $t("Remove clients") }}
+                      </span>
                     </template>
-                    {{ $t("Only delete client assignments") }}
-                    <b-button variant="danger">
-                      {{ $t("Delete") }}
+                    <br>
+                    <span> {{ $t('Are you sure you want to delete client assignments of the selected group?') }}</span>
+                    <b-button class="float-right" variant="danger">
+                      {{ $t("Remove") }}
                     </b-button>
                   </b-tab>
                 </b-tabs>
               </div>
-              <div v-if="value.type == 'ObjectToGroup'">
+              <div v-if="selectedvalue.type == 'ObjectToGroup'">
                 <span class="font-weight-bold">
-                  {{ $t('Selected Client : ') }} {{ value.text }}
+                  {{ $t('Selected Client : ') }} {{ selectedvalue.text }}
                 </span>
                 <b-tabs>
                   <b-tab>
                     <template #title>
-                      <span class="client"> {{ $t("Move") }} </span>
+                      <span v-b-tooltip.hover :title="$t('Add the selected client to groups.')">
+                        {{ $t("Add") }}
+                      </span>
                     </template>
-                    {{ $t("Move to another group") }}
-                    <div>
-                      <treeselect value-format="object" :options="group" />
-                      <b-button variant="primary">
-                        {{ $t("Move") }}
-                      </b-button>
-                    </div>
+                    <treeselect
+                      v-b-tooltip.hover
+                      :title="$t('Select groups to add the selected client')"
+                      :placeholder="$t('Select groups to add the selected client')"
+                      value-format="object"
+                      :options="group"
+                      :normalizer="normalizer"
+                    />
+                    <b-button class="float-right" variant="success">
+                      {{ $t("Add") }}
+                    </b-button>
                   </b-tab>
                   <b-tab>
                     <template #title>
-                      <span class="client"> {{ $t("Copy") }} </span>
+                      <span v-b-tooltip.hover :title="$t('Remove the selected client from groups.')">
+                        {{ $t("Remove") }}
+                      </span>
                     </template>
-                    {{ $t("Copy to another group") }}
-                    <div>
-                      <treeselect value-format="object" :options="group" />
-                      <b-button variant="primary">
-                        {{ $t("Copy") }}
-                      </b-button>
-                    </div>
+                    <treeselect
+                      v-b-tooltip.hover
+                      :title="$t('Select groups to remove the selected client assignment')"
+                      :placeholder="$t('Select groups to remove the selected client assignment')"
+                      value-format="object"
+                      :options="group"
+                      :normalizer="normalizer"
+                    />
+                    <b-button class="float-right" variant="danger">
+                      {{ $t("Remove") }}
+                    </b-button>
                   </b-tab>
                 </b-tabs>
               </div>
@@ -152,7 +173,7 @@
       </b-tab>
       <!-- <b-tab>
         <template #title>
-          <span class="product"> {{ $t('form.productgroups') }} </span>
+          <span> {{ $t('form.productgroups') }} </span>
         </template>
       </b-tab> -->
     </b-tabs>
@@ -162,21 +183,16 @@
 <script lang="ts">
 import { Component, namespace, Watch, Vue } from 'nuxt-property-decorator'
 import { Constants } from '../../mixins/uib-mixins'
-// const selections = namespace('selections')
-// interface ClientRequest {
-//     selectedDepots: string
-// }
 const selections = namespace('selections')
 @Component({ mixins: [Constants] })
 export default class VGroups extends Vue {
   iconnames: any
-  depotId: string = ''
-  group: Array<object> = []
-  value: any = null
-  clientIds: any
   $axios: any
   node: any
   $fetch: any
+  group: Array<object> = []
+  selectedvalue: any = null
+  clientIds: Array<string> = []
 
   @selections.Getter public selectionDepots!: Array<string>
 
@@ -189,73 +205,31 @@ export default class VGroups extends Vue {
   }
 
   @Watch('selectionDepots', { deep: true }) async selectionDepotChanged () {
-    await this.$fetch()
+    await this.fetchGroups()
   }
 
-  // @Watch('depotId', { deep: true }) async depotsChanged () {
-  //   await this.$fetch()
-  // }
-
   async fetch () {
+    await this.fetchGroups()
+    await this.fetchClients()
+  }
+
+  async fetchGroups () {
     const result = await this.$axios.$get(`/api/opsidata/hosts/groups?selectedDepots=[${this.selectionDepots}]`)
-    // this.group = Object.values(result)
-    if (result) {
-      const customgroups: any = result.groups
-      console.log(JSON.stringify(customgroups.children.clientdirectory))
-      // delete customgroups.children.clientdirectory
+    this.group = Object.values(result)
+    // if (result) {
+    //   const customgroups: any = result.groups
+    //   // console.log(JSON.stringify(customgroups.children.clientdirectory))
+    //   // delete customgroups.children.clientdirectory
 
-      this.group = [customgroups, result.groups.children.clientdirectory]
-    }
+    //   this.group = [customgroups, result.groups.children.clientdirectory]
+    // }
+  }
 
-    // this.group = [
-    //   {
-    //     id: 'groups',
-    //     label: 'groups',
-    //     type: 'HostGroup',
-    //     children: [
-    //       {
-    //         id: 'testgroup1;groups',
-    //         label: 'testgroup1',
-    //         type: 'HostGroup',
-    //         children: [
-    //           {
-    //             id: 'client1.uib.local;testgroup1',
-    //             label: 'client1.uib.local',
-    //             type: 'ObjectToGroup'
-    //           }
-    //         ]
-    //       },
-    //       {
-    //         id: 'testgroup2;groups',
-    //         label: 'testgroup2',
-    //         type: 'HostGroup',
-    //         children: [
-    //           {
-    //             id: 'client1.uib.local;testgroup2',
-    //             label: 'client1.uib.local',
-    //             type: 'ObjectToGroup'
-    //           },
-    //           {
-    //             id: 'client2.uib.local;testgroup2',
-    //             label: 'client2.uib.local',
-    //             type: 'ObjectToGroup'
-    //           },
-    //           {
-    //             id: 'client3.uib.local;testgroup2',
-    //             label: 'client3.uib.local',
-    //             type: 'ObjectToGroup'
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     id: 'clientdirectory',
-    //     label: 'clientdirectory',
-    //     type: 'HostGroup',
-    //     children: [{}]
-    //   }
-    // ]
+  async fetchClients () {
+    await this.$axios.$get(`/api/opsidata/depots/clients?selectedDepots=[${this.selectionDepots}]`)
+      .then((response) => {
+        this.clientIds = response.sort()
+      })
   }
 }
 </script>
