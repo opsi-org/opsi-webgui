b705c08eb03bc11ef2dde84c75b37879d7afe149
anna sucher <anna.sucher@stud.tu-darmstadt.de>
2023-03-22 14:34:31 +0000

    [fix] rm cloning components repo
    Former-commit-id: 7ce7d8c0716afe39a380eded106b1ee2c28c327e

--- .devcontainer/devenv.sh
+++ .devcontainer/devenv.sh
@@ -13,17 +13,17 @@ echo "OPSILICSRV_TOKEN=$OPSILICSRV_TOKEN"  >> .env
 
 
 
-if [ -d .devcontainer ]; then # current dir: /workspace
-	git clone git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git opsiweb/uib-components
-	echo "Please check if opsiweb/uib-components directory is filled. "
-elif [ -f devcontainer.json ]; then # current dir: /workspace/.devcontainer
-	git clone --branch=$(git branch --show-current) git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git ../opsiweb/uib-components
-	echo "Please check if opsiweb/uib-components directory is filled. "
-else
-	echo ""
-	echo "Attention! "
-	echo "Please clone repo as opsiweb/uib-components. E.g. with"
-	echo "\tgit clone --branch=$(git branch --show-current) git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git opsiweb/uib-components"
-fi
+# if [ -d .devcontainer ]; then # current dir: /workspace
+# 	git clone git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git opsiweb/uib-components
+# 	echo "Please check if opsiweb/uib-components directory is filled. "
+# elif [ -f devcontainer.json ]; then # current dir: /workspace/.devcontainer
+# 	git clone --branch=$(git branch --show-current) git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git ../opsiweb/uib-components
+# 	echo "Please check if opsiweb/uib-components directory is filled. "
+# else
+# 	echo ""
+# 	echo "Attention! "
+# 	echo "Please clone repo as opsiweb/uib-components. E.g. with"
+# 	echo "\tgit clone --branch=$(git branch --show-current) git@gitlab.uib.gmbh:uib/opsiweb-ui-components.git opsiweb/uib-components"
+# fi
 
-cp -f ../opsiweb/uib-components/.gitlab/hooks/* ../opsiweb/uib-components/.git/hooks/*
+# cp -f ../opsiweb/uib-components/.gitlab/hooks/* ../opsiweb/uib-components/.git/hooks/*
