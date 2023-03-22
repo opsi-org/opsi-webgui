c5d8fb4e63b6946f8496518d3334f0bf2eff1cc7
Jeena John <j.john@uib.de>
2023-03-22 10:37:04 +0000

    [new] tests and snapshots for healthcheck

---
+++ components/view/VHealthCheck.test.component.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test')
+const { callStoryId } = require('../../.utils/playwright/pw-story-call')
+
+test('view-healthcheck snapshot', async ({ page }) => {
+  await callStoryId(page, 'view-v-healthcheck', 'v-healthcheck')
+  const component = await page.locator('[data-testid="VHealthCheck"]')
+  await (new Promise(resolve => setTimeout(resolve, 1000)))
+  expect(await component.screenshot()).toMatchSnapshot('VHealthCheck.png')
+})
