3630be6d63873976039ba2fb3dd1d7e6c553dd56
Jeena John <j.john@uib.de>
2023-04-12 12:44:23 +0000

    [fix] component tests
    Former-commit-id: e47e2d82956c43d58433b355b4ccb78be1c59ffe

--- opsiweb/uib-components/components/nav/item/NIItem.test.component.js
+++ opsiweb/uib-components/components/nav/item/NIItem.test.component.js
@@ -3,6 +3,6 @@ const { callStoryId } = require('../../../.utils/playwright/pw-story-call')
 
 test('navitem snapshot', async ({ page }) => {
   await callStoryId(page, 'navitem-n-i-item', 'ni-item')
-  const component = await page.locator('[data-testid="NIItem"]')
-  expect(await component.screenshot()).toMatchSnapshot('NIItem.png', { maxDiffPixelRatio: 0.2 })
+  const component = await page.locator('[data-testid="NIItem-Servers"]')
+  expect(await component.screenshot()).toMatchSnapshot('NIItemServers.png', { maxDiffPixelRatio: 0.2 })
 })
--- opsiweb/uib-components/components/table/.TInfiniteScroll.test.component.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test')
-const { callStoryId } = require('../../.utils/playwright/pw-story-call')
-
-test('table infinite scroll snapshot', async ({ page }) => {
-  await callStoryId(page, 'table-t-infinite-scroll', 't-infinite-scroll')
-  // const component = await page.locator('[data-testid="TInfiniteScroll"]')
-  expect(await page.screenshot()).toMatchSnapshot('TInfiniteScroll.png', { maxDiffPixelRatio: 0.2 })
-})
-// todo fix stories and add tests for scrolling
