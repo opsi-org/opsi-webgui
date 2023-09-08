a82fbaa1e8549d6d39b3f5b3ac4b878db6b96501
sucher <a.sucher@uib.de>
2023-09-08 10:23:35 +0000

    [fix] mocking data
    Former-commit-id: 49529239d167162d1721ac8262272c931a69a186

--- opsiweb/uib-components/components/tree/TSHostGroups.stories.js
+++ opsiweb/uib-components/components/tree/TSHostGroups.stories.js
@@ -1,8 +1,11 @@
+// import { moveSyntheticComments } from 'typescript'
 import { mock, data } from '../../.utils/storybook/mock'
 mock.onGet('/api/opsidata/hosts/groups?selectedDepots=[<config-server-id>]&selectedClients=[]&parentGroup=root').reply(200, data.groups.hosts)
-mock.onGet('/api/opsidata/hosts/groups?selectedDepots=[]&selectedClients=[]&parentGroup=root').reply(200, data.groups.hosts)
+mock.onGet('/api/opsidata/hosts/groups?selectedDepots=[]&selectedClients=[]&parentGroup=root').reply(200, data.groups.hostgroups)
 mock.onGet('/api/opsidata/depots/clients?selectedDepots=[<config-server-id>]').reply(200, data.clientIds)
 mock.onGet('/api/opsidata/depots/clients?selectedDepots=[]').reply(200, data.clientIds)
+mock.onGet('/api/opsidata/hosts/groups-dynamic?selectedDepots=[]&selectedClients=[]&parentGroup=root').reply(200, { groups: data.hostgroups })
+mock.onGet('**/api/opsidata/groups-dynamic**').reply(200, { groups: data.hostgroups })
 
 export default {
   title: 'Tree/TS Host Groups',
