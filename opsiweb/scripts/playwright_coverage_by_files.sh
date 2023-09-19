07a4d0542c74cd3068d83d3847aed2414bc55029
sucher <a.sucher@uib.de>
2023-09-19 11:30:21 +0000

    [fix] add missed file
    Former-commit-id: e8f04bbe7f7eaecf58b523579ddce15071e72ef1

--- opsiweb/scripts/playwright_coverage_by_files.sh
+++ opsiweb/scripts/playwright_coverage_by_files.sh
@@ -1,36 +1,55 @@
+#!/bin/bash
+
 
 # echo "----------------------- ALL COMPONENTS which should have a test (all .vue files in components/): "
 ls -1 uib-components/components/*/*.vue | sort > .tmp_RESULT_FILESFOUND_VUE
 # cat .tmp_RESULT_FILESFOUND
 # echo $RES_ALL | tr " " "\n"
 NUM_FILES_VUE=$(cat .tmp_RESULT_FILESFOUND_VUE | wc -l)
-echo "number components: $NUM_FILES_VUE"
 # echo "----------------------- ALL-we: "
 cat .tmp_RESULT_FILESFOUND_VUE | cut -d '.' -f 1 > .tmp_X_RESULT_FILEIDS_FOUND_VUE
 # cat .tmp_X_RESULT_FILEIDS_FOUND
-rm .tmp_RESULT_FILESFOUND_VUE
+rm -f .tmp_RESULT_FILESFOUND_VUE
 
 # echo "----------------------- ALL COMPONENTS which should have a test (all .vue files in components/): "
 ls -1 uib-components/components/*/*.test.component.js | sort > .tmp_RESULT_FILESFOUND_PW
 # cat .tmp_RESULT_FILESFOUND
 # echo $RES_ALL | tr " " "\n"
 NUM_FILES_PW=$(cat .tmp_RESULT_FILESFOUND_PW | wc -l)
-echo "number components: $NUM_FILES_PW"
 # echo "----------------------- ALL-we: "
 cat .tmp_RESULT_FILESFOUND_PW | cut -d '.' -f 1 > .tmp_X_RESULT_FILEIDS_PW
 # cat .tmp_X_RESULT_FILEIDS_FOUND
-rm .tmp_RESULT_FILESFOUND_PW
+rm -f .tmp_RESULT_FILESFOUND_PW
 
-
+COLOR_YELLOW='\e[1;33m'
 COLOR_RED="\033[31m"
+COLOR_BLUE='\e[0;34m'
 COLOR_NORMAL="\033[0;39m"
-echo "----------------------- FILES WITHOUT A SCREENSHOT TEST"
-printf $COLOR_RED
+# printf $COLOR_BLUE
+echo "--------------------------------- FILES WITHOUT A SCREENSHOT TEST"
+printf $COLOR_YELLOW
 comm -23 .tmp_X_RESULT_FILEIDS_FOUND_VUE .tmp_X_RESULT_FILEIDS_PW
-printf $COLOR_NORMAL
-echo "==== COVERAGE REPORT SCREENSHOTS ==========="
+printf $COLOR_BLUE
 PERCENTAGE_COVERAGE=$(expr $NUM_FILES_PW \* 100 / $NUM_FILES_VUE)
-printf "FILES WITH A TESTFILE\t\t: $PERCENTAGE_COVERAGE%% ($NUM_FILES_PW/$NUM_FILES_VUE)\n"
-echo "============================================"
+printf "FILES WITH A TESTFILE\t: $PERCENTAGE_COVERAGE%% ($NUM_FILES_PW/$NUM_FILES_VUE) (in general)\n"
+printf $COLOR_NORMAL
+
+if [ "$1" ]; then
+    # printf $COLOR_BLUE
+    echo "--------------------------------- FAILED TESTS"
+    # grep -w '\[.*✓.*\] \› uib-components\/components\/*' $1  > .tmp_RESULTFILE_SUCCESS
+    grep -w '\[.*✘.*\] \› uib-components\/components\/*' $1  > .tmp_RESULTFILE_FAILED
+    # RES_NAMES_SUCC=$(cat .tmp_RESULTFILE_SUCCESS | cut -d ' ' -f 8 | cut -d ' ' -f 5 | tr " " "\n" )
+    # RES_NAMES_FAIL=$(cat .tmp_RESULTFILE_FAILED | cut -d ' ' -f 8 | cut -d ':' -f 1 | tr " " "\n" )
+    RES_NAMES_FAIL=$(cat .tmp_RESULTFILE_FAILED | cut -d ' ' -f 8 | tr " " "\n" )
+    rm -f .tmp_RESULTFILE_FAILED #.tmp_RESULTFILE_SUCCESS
+    printf $COLOR_RED
+    echo $RES_NAMES_FAIL | tr " " "\n"
+    printf $COLOR_NORMAL
+    printf "for details run: "
+    printf $COLOR_BLUE
+    printf "cat $1\n"
+    printf $COLOR_NORMAL
+fi
 
 # grep -w '\[.*[✘|✓].*\] \› uib-components\/components\/*' RESULTFILE
