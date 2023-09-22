b04cfe85f156b4f4afc628a00662af76a7a66c5d
Jeena John <j.john@uib.de>
2023-09-22 15:31:04 +0000

    [chg] update story+test+snapshot for group initial selection
    Former-commit-id: b9f9d3efcda78a28c24ce78493c569bbf9e24a55

---
+++ opsiweb/uib-components/components/tree/TSGroupInitSelection.stories.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'Tree/TS Group Init Selection',
+  parameters: { docs: { description: { component: 'Treeselect for group selection' } } }
+}
+
+const PrimaryTemplate = () => ({
+  template: '<TreeTSGroupInitSelection />'
+})
+
+export const TSGroupInitSelection = PrimaryTemplate.bind({})
