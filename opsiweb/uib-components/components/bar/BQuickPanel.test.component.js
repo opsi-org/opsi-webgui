85e6d58c18a03e773fb2e2351ee182a40f038ae0
Jeena John <j.john@uib.de>
2023-09-22 14:22:37 +0000

    [fix] tests
    Former-commit-id: e7aa672350865cb39f6445b4e1d399eda26a0b6f

--- opsiweb/uib-components/components/bar/BQuickPanel.test.component.js
+++ opsiweb/uib-components/components/bar/BQuickPanel.test.component.js
@@ -3,6 +3,7 @@ const { callStoryId } = require('../../.utils/playwright/pw-story-call')
 
 test('quickpanel', async ({ page }) => {
   await callStoryId(page, 'bar-b-quick-panel', 'b-quick-panel')
+  await (new Promise(resolve => setTimeout(resolve, 1000)))
   const component = page.locator('[data-testid="BarBQuickPanel"]')
   expect(await component.screenshot()).toMatchSnapshot('BarBQuickPanel.png')
 })
