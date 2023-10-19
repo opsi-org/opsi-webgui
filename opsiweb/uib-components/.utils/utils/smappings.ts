e8186782053058ee7b25c0c85dc505caf39e2984
sucher <a.sucher@uib.de>
2023-10-19 10:08:18 +0000

    [fix] badges/tooltip for mixed/not mixed product states
    Former-commit-id: fc69a19ffa626a1d146b7c8572c8451454ff7a08

--- opsiweb/uib-components/.utils/utils/smappings.ts
+++ opsiweb/uib-components/.utils/utils/smappings.ts
@@ -20,13 +20,15 @@ export function mapValues2Value (values: Array<string>, objects: Array<string>, 
       return values[0]
     }
     if (objectsorigin.length > 1 && values.length === 1) {
-      return (values[0] === defvalue) ? defvalue : 'mixed'
+      if (objects.length !== objectsorigin.length) return 'mixed'
+      return values[0]
     }
     if (objectsorigin.length === objects.length) {
       if (values.every(val => val === values[0])) {
         return values[0]
       }
     }
+
   }
   if (values.every(val => val === defvalue)) {
     return defvalue
@@ -35,21 +37,27 @@ export function mapValues2Value (values: Array<string>, objects: Array<string>, 
   return 'mixed'
 }
 export function mapValues2Objects (values: Array<string>, objects: Array<string>, objectsorigin: Array<string>, defaultValue: string) {
-  // if (!values && !values[0]) {
-  //   return {}
-  // }
   if (objects === undefined || objects === null) {
     return {}
   }
   const client2value: IObjectString2String = {}
   for (const o in objectsorigin) {
+
     const i = objects.indexOf(objectsorigin[o])
+    console.log(o, i, objects[i])
+    if (i >= 0 && values.length === 1) {
+      console.log('0...', client2value[objects[i]])
+      client2value[objects[i]] = values[0]
+    } else
     if (i >= 0 && values) {
       client2value[objects[i]] = values[i]
+      console.log('1...', client2value[objects[i]])
     } else if (i >= 0) {
       client2value[objects[i]] = defaultValue
+      console.log('2...', client2value[objects[i]])
     } else {
       client2value[objectsorigin[o]] = defaultValue
+      console.log('3...',  client2value[objects[i]])
     }
   }
   return client2value
