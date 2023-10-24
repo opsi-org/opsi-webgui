478c01591d14c8b3c8f0f44a8569ccaa70fc344e
sucher <a.sucher@uib.de>
2023-10-24 15:38:51 +0000

    [fix] get result as object
    Former-commit-id: db29768c78e56201d54b65f61120b921a5a6ef85

--- frontend/components/test/Fetch.vue
+++ frontend/components/test/Fetch.vue
@@ -1,30 +1,15 @@
 <template>
-  <div ref="target">
-    <p v-if="fetchResult.pending">Fetching...</p>
-    <pre v-else-if="fetchResult.error">Could not load data: {{ error.data }}</pre>
-    <figure v-else>
-      Result {{ fetchResult.value.result }} <br />
-    </figure>
+  <p v-if="fetchResult.pending">Fetching...</p>
+  <pre v-else-if="fetchResult.error">Could not load data: {{ error.data }}</pre>
+  <div v-else>
+    Result: {{ fetchResult.value.result }} <br />
   </div>
 </template>
 
 <script setup>
-// const result = ref()
-// onMounted( async () => {
-//   // console.log(`the component is now mounted.`)
-//   const { data, error, pending, status } = await useApiFetch('/user/opsiserver', { }).get()
-//   result.value = data
-// })
-// const result = computed({ get: () => data ? JSON.parse(data) : data })
-
 const fetchResult = ref({});
-
 onMounted( async () => {
   const { data: result } = await $fetch('/user/opsiserver').get().json()
-  // const { data } = await useApiFetch('/user/opsiserver').get()
-//   // const { data: response } = await useAsyncData('directories', () => $fetch('/api/s3-get-directories'));
   fetchResult.value = result;
-  console.log("fetch local result", result)
-  console.log("fetch global result", fetchResult)
 });
 </script>
