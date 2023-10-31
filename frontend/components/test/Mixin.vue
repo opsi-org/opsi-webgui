86db213a8f91f6a2e240cbc3cc0c89064beb1c35
sucher <a.sucher@uib.de>
2023-10-31 14:42:47 +0000

    [fix] comment unsued code (keep just as refenece)
    Former-commit-id: 670ab124a34f1638d53bedbe6a37ecd89752d302

--- frontend/components/test/Mixin.vue
+++ frontend/components/test/Mixin.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <!-- Global mixin: {{ hello }} <br /> -->
-    Local mixin: {{ bye }}
+    <!-- Local mixin: {{ bye }} -->
   </div>
 </template>
 
 <script setup>
-import { useBye } from '~/composables/mixins/useBye'
-// const hello = useHello() // mixin - global (automatically imported in each component)
-const bye = useBye()  // mixin - local => needs to be imported manually
+// import { useBye } from '~/composables/mixins/useBye'
+// // const hello = useHello() // mixin - global (automatically imported in each component)
+// const bye = useBye()  // mixin - local => needs to be imported manually
 </script>
