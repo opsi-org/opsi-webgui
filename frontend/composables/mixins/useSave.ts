8156d5e83617de90eaf12e283b89b2413619b449
sucher <a.sucher@uib.de>
2023-10-31 13:23:08 +0000

    [fix] mixin save
    Former-commit-id: fa2df2d300e965cad187b0c1a06dd15eb83a027c

--- frontend/composables/mixins/useSave.ts
+++ frontend/composables/mixins/useSave.ts
@@ -1,82 +1,148 @@
-import { Component, namespace, Vue } from 'nuxt-property-decorator'
-import { AlertToast } from './component'
+import { useNotification } from './useComponent'
 
-const changes = namespace('changes')
-const errors = namespace('errors')
-@Component({ mixins: [AlertToast] }) export class SaveParameters extends Vue {
-  showToastSuccess: any // from mixin AlertToast
-  showToastError: any // from mixin AlertToast
-  @changes.Mutation public delFromChangesHostParam!: (s: object) => void
-  @errors.Mutation public pushToErrorsHostParam!: (s: object) => void
-  async saveParameters (url: string, request: any, deleteitem:any, showalert:boolean) {
-    await this.$axios.$post(url, request)
-      .then(() => {
-        if (deleteitem) {
-          this.delFromChangesHostParam(deleteitem)
-        } else {
-          this.showToastSuccess(this.$t('message.success.save.parameters'))
-        }
-      }).catch((error) => {
-        if (showalert) {
-          this.showToastError(error)
-        } else {
+export const useSaveParameters = () => {
+  const { t } = useI18n()
+  const delFromChangesHostParam = storeChanges().delFromChangesHostParam
+  const pushToErrorsHostParam = storeErrors().pushToErrorsHostParam
+
+  async function saveParameters (url: string, request: any, deleteitem:any, showalert:boolean) {
+
+    const { data, error } = await useApiPOST(url, request)
+    if (error) {
+      if (showalert) {
+        console.log("error", error)
+        useNotification().error(error)
+      } else {
           const errorObj = deleteitem
           errorObj.error = error?.response?.data
-          this.pushToErrorsHostParam(errorObj)
-        }
-      })
+          pushToErrorsHostParam(errorObj)
+      }
+      return
+    }
+
+    if (deleteitem) {
+      delFromChangesHostParam(deleteitem)
+    } else {
+      useNotification().success(t('message.success.save.parameters'))
+    }
+    // await this.$axios.$post(url, request)
+    //   .then(() => {
+    //     if (deleteitem) {
+    //       this.delFromChangesHostParam(deleteitem)
+    //     } else {
+    //       this.showToastSuccess(this.$t('message.success.save.parameters'))
+    //     }
+    //   }).catch((error) => {
+    //     if (showalert) {
+    //       this.showToastError(error)
+    //     } else {
+    //       const errorObj = deleteitem
+    //       errorObj.error = error?.response?.data
+    //       this.pushToErrorsHostParam(errorObj)
+    //     }
+    //   })
+  }
+  return {
+    delFromChangesHostParam,
+    pushToErrorsHostParam,
+    saveParameters
   }
 }
 
-@Component({ mixins: [AlertToast] }) export class SaveProductActionRequest extends Vue {
-  showToastSuccess: any // from mixin AlertToast
-  showToastError: any // from mixin AlertToast
-  @changes.Mutation public delFromChangesProducts!: (s: object) => void
-  @errors.Mutation public pushToErrorsProducts!: (s: object) => void
-  async saveProdActionRequest (change : object, deleteitem:any, showalert:boolean) {
-    await this.$axios.$post('/api/opsidata/clients/products', change)
-      .then(async () => {
-        if (deleteitem) {
-          this.delFromChangesProducts(deleteitem)
-        } else {
-          this.showToastSuccess(this.$t('message.success.save.prodActionRequest'))
-          await this.$nuxt.refresh()
-        }
-      }).catch((error) => {
-        if (showalert) {
-          this.showToastError(error)
-        } else {
-          const errorObj = deleteitem
-          errorObj.error = error?.response?.data
-          this.pushToErrorsProducts(errorObj)
-        }
-      })
+export const useSaveProductActionRequest = () => {
+  const { t } = useI18n()
+  const delFromChangesProducts = storeChanges().delFromChangesProducts
+  const pushToErrorsProducts = storeErrors().pushToErrorsProducts
+
+  async function saveProdActionRequest (change : object, deleteitem:any, showalert:boolean) {
+    const { data, error } = await useApiPOST('/opsidata/clients/products', change)
+    if (error) {
+      if (showalert) {
+        console.log("error", error)
+        useNotification().error(error)
+      } else {
+        const errorObj = deleteitem
+        errorObj.error = error?.response?.data
+        pushToErrorsProducts(errorObj)
+      }
+      return
+    }
+
+    if (deleteitem) {
+      delFromChangesProducts(deleteitem)
+    } else {
+      useNotification().success(t('message.success.save.prodActionRequest'))
+      // await $nuxt.refresh()
+      // TODO how to refresh nuxt ??? https://stackoverflow.com/questions/77387776/nuxt-3-reload-refresh-the-page
+    }
+
   }
+
+  //   await this.$axios.$post('/api/opsidata/clients/products', change)
+  //     .then(async () => {
+  //       if (deleteitem) {
+  //         this.delFromChangesProducts(deleteitem)
+  //       } else {
+  //         this.showToastSuccess(this.$t('message.success.save.prodActionRequest'))
+  //         await this.$nuxt.refresh()
+  //       }
+  //     }).catch((error) => {
+  //       if (showalert) {
+  //         this.showToastError(error)
+  //       } else {
+  //         const errorObj = deleteitem
+  //         errorObj.error = error?.response?.data
+  //         this.pushToErrorsProducts(errorObj)
+  //       }
+  //     })
+  // }
+  return { delFromChangesProducts, pushToErrorsProducts, saveProdActionRequest}
 }
 
-@Component({ mixins: [AlertToast] }) export class SaveProductProperties extends Vue {
-  showToastSuccess: any // from mixin AlertToast
-  showToastError: any // from mixin AlertToast
-  @changes.Mutation public delFromChangesProducts!: (s: object) => void
-  @errors.Mutation public pushToErrorsProducts!: (s: object) => void
 
-  async saveProdProperties (id: string, change: any, deleteitem:any, showalert:boolean) {
-    await this.$axios.$post(`/api/opsidata/products/${id}/properties`, change)
-      .then(() => {
-        if (deleteitem) {
-          this.delFromChangesProducts(deleteitem)
-        } else {
-          this.$emit('refetch', true)
-          this.showToastSuccess(this.$t('message.success.save.productproperty', { id: Object.keys(change.properties) }))
-        }
-      }).catch((error) => {
-        if (showalert) {
-          this.showToastError(error)
-        } else {
-          const errorObj = deleteitem
-          errorObj.error = error?.response?.data
-          this.pushToErrorsProducts(errorObj)
-        }
-      })
+export const useSaveProductProperties = () => {
+  const { t } = useI18n()
+  const delFromChangesProducts = storeChanges().delFromChangesProducts
+  const pushToErrorsProducts = storeErrors().pushToErrorsProducts
+  const $emit = defineEmits(['refetch'])
+  async function saveProdProperties (id: string, change: any, deleteitem:any, showalert:boolean) {
+    const { data, error } = await useApiPOST(`/opsidata/products/${id}/properties`, change)
+    if (error) {
+      if (showalert) {
+        console.log("error", error)
+        useNotification().error(error)
+      } else {
+        const errorObj = deleteitem
+        errorObj.error = error?.response?.data
+        pushToErrorsProducts(errorObj)
+      }
+      return
+    }
+
+    if (deleteitem) {
+      delFromChangesProducts(deleteitem)
+    } else {
+      $emit('refetch', true)
+      useNotification().success(t('message.success.save.productproperty', { id: Object.keys(change.properties) }))
+    }
+
+    // await this.$axios.$post(`/api/opsidata/products/${id}/properties`, change)
+    //   .then(() => {
+    //     if (deleteitem) {
+    //       this.delFromChangesProducts(deleteitem)
+    //     } else {
+    //       this.$emit('refetch', true)
+    //       this.showToastSuccess(this.$t('message.success.save.productproperty', { id: Object.keys(change.properties) }))
+    //     }
+    //   }).catch((error) => {
+    //     if (showalert) {
+    //       this.showToastError(error)
+    //     } else {
+    //       const errorObj = deleteitem
+    //       errorObj.error = error?.response?.data
+    //       this.pushToErrorsProducts(errorObj)
+    //     }
+    //   })
   }
+  return { delFromChangesProducts, pushToErrorsProducts, saveProdProperties}
 }
