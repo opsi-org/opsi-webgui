594e23fdab0709eeffcc7532efcaa5009fed707a
sucher <a.sucher@uib.de>
2023-10-31 13:00:33 +0000

    [fix] useFormat mixin
    Former-commit-id: dd54b04d9ecfc043da001436b065374b7c8f838e

--- frontend/composables/mixins/useFormat.ts
+++ frontend/composables/mixins/useFormat.ts
@@ -1,11 +1,12 @@
-import { Component, Vue } from 'nuxt-property-decorator'
 
-@Component export class Format extends Vue {
-  date (value:any) {
+export const useFormat = () => {
+// @Component export class Format extends Vue {
+  function date (value:any) {
     if (typeof value === 'object') {
       return value
     } else if (value !== '' || value !== undefined || value !== null) {
       return new Date(value).toString()
     } else { return '' }
   }
+  return { date }
 }
