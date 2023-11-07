cdc1b61880a4442cf728921412ebb97c45972b9b
Jeena John <j.john@uib.de>
2023-11-07 14:24:16 +0000

    [chg] test Element plus treeselect
    Former-commit-id: 29d78d9720e69d0629e0a67e3e0171abf8d84a09

---
+++ frontend/pages/tree.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    testing element plus treeselect component
+    <TestEPTreeSelect />
+  </div>
+</template>
+
+<script setup>
+
+// const { locale } = useI18n()
+</script>
+
+<style>
+</style>
