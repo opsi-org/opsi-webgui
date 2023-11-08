093cbcd0cd0c074c9d9fe83f0f5f36cb7a223561
Jeena John <j.john@uib.de>
2023-11-08 15:52:01 +0000

    [chg] add navigation
    Former-commit-id: ceeb292aafe28a672a2d9a9ef8e3b17c2a4c062e

---
+++ frontend/components/bar/BSide.vue
@@ -0,0 +1,109 @@
+<template>
+  <!-- <b-sidebar
+    id="sidemenu"
+    visible
+  >
+    <b-nav vertical>
+      <span v-for="item in navItems" :key="item.title">
+        <template v-if="item.submenu">
+          <b-nav-item
+            :data-testid="'NIItem-'+item.title"
+            v-b-toggle="'collapse-navitem-'+item.title"
+            :title="$t(item.title)" class="NItem-nav-item"
+            :to="item.route"
+          >
+              {{ $t(item.title) }} collapse
+          </b-nav-item>
+          <b-collapse :id="'collapse-navitem-'+item.title" accordion="sidebarAccordion">
+            <b-nav vertical>
+              <b-nav-item
+                v-for="sub in item.submenu"
+                :key="sub.title"
+                :data-testid="'NICollapsible-submenu-'+item.title+sub.title"
+                :to="sub.route"
+              >
+                  {{ $t(sub.title) }}
+              </b-nav-item>
+            </b-nav>
+          </b-collapse>
+        </template>
+        <template v-else>
+          <b-nav-item :data-testid="'NIItem-'+item.title" :title="$t(item.title)" class="NItem-nav-item" :to="item.route">
+              {{ $t(item.title) }}
+          </b-nav-item>
+        </template>
+      </span>
+    </b-nav>
+  </b-sidebar> -->
+  <el-row class="tac">
+    <el-col :span="12">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+const mq = useMQ()
+
+const navItems = [
+  {
+    title: 'title.depots', route: '/depots/',
+    submenu: [
+      { title: 'title.allDepots', route: '/depots/' },
+      { title: 'title.config', route: '/depotsconfig' }
+    ]
+  },
+  {
+    title: 'title.clients', route: '/clients/',
+    submenu: [
+      { title: 'title.allClients', route: '/clients/' },
+      { title: 'title.addNew', route: '/clientscreation' },
+      { title: 'title.config', route: '/clientsconfig' },
+      { title: 'title.log', route: '/clientslog' }
+    ]
+  },
+  { title: 'title.products', route: '/products/' },
+  { title: 'title.groups', route: '/groups/' }
+]
+</script>
+
+<style scoped>
+#sidemenu {
+  top: calc(var(--height-navbar) - 2px) !important;
+  width: 200px;
+  height: 100% !important;
+}
+</style>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,7 +2,11 @@
 <template>
   <div :class="'webgui-theme-'+color">
     Some default structure shared across all pages. Current theme: {{ settings.theme }}
-    <slot />
+    <BarBSide class="sidebar_content" />
+    <div class="main_content">
+      <slot />
+    </div>
+
     after slot
   </div>
 </template>
@@ -11,3 +15,15 @@
 const settings = storeSettings()
 const color = useColorMode();
 </script>
+
+<style>
+.main_content{
+  position:absolute;
+  margin-top: var(--margin-top-maincontent);
+  margin-left: var(--margin-left-maincontent);
+  overflow-x: auto;
+  overflow-y: hidden;
+  width: calc(100% - 2 * var(--margin-left-maincontent));
+  min-height: -webkit-fill-available;
+}
+</style>
