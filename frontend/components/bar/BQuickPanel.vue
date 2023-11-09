5c250a84e090b3308533e587a8a467167f244efb
Jeena John <j.john@uib.de>
2023-11-09 16:20:38 +0000

    [chg] update topbar
    Former-commit-id: c6a8bb13ff7fda3c317fbfeaf79d3c6b2618966e

---
+++ frontend/components/bar/BQuickPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-menu >
+    Quick Panel
+  </el-menu>
+</template>
+
+<script setup lang="ts">
+const mq = useMQ()
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/components/bar/BTop.vue
@@ -0,0 +1,24 @@
+<template>
+ <el-menu
+    class="el-menu-demo"
+    mode="horizontal"
+  >
+    <el-menu-item index="0">
+      <img src="../../assets/images/opsi.png" alt="OPSI Logo">
+    </el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="1">
+      <el-checkbox-button>
+        Open Quick Panel
+      </el-checkbox-button>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup lang="ts">
+const mq = useMQ()
+</script>
+
+<style scoped>
+
+</style>
