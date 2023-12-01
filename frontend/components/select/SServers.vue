1ecf9779f40ed803ee7ccbec45acc664112fbcfe
john <j.john@uib.de>
2023-12-01 15:14:55 +0000

    [new] add server selection dropdown

---
+++ frontend/components/select/SServers.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-select v-model="value" clearable placeholder="Select a Server">
+    <el-option
+      v-for="item in fetchedData"
+      :key="item"
+      :label="item"
+      :value="item"
+    />
+  </el-select>
+</template>
+
+<script setup lang="tsx">
+import { useNotification } from '~/composables/mixins/useComponent';
+import { useDepot } from '~/composables/mixins/useGet';
+const $t = useI18n().t
+let fetchedData = ref<Array<any>>([])
+const value = ref('')
+
+onMounted(async ()=> await fetch())
+
+async function fetch() {
+  const {data, error} = await useDepot().getDepotIdList()
+  fetchedData = data
+}
+</script>
