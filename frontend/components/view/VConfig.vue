45ec98135f348d98c323c8d5ca5d860a9ddaca50
sucher <a.sucher@uib.de>
2023-12-13 11:52:14 +0000

    [fix] id changes

--- frontend/components/view/VConfig.vue
+++ frontend/components/view/VConfig.vue
@@ -1,27 +1,34 @@
 
 <template>
 
-  <SelectSServers :id="props.id" />
+  <SelectSServers :id="currentId" @change="setId" />
   <el-tabs v-model="activeName" class="demo-tabs">
     <el-tab-pane
-      :label="props.id ? $t('title.hostparam') : $t('title.hostparam.defaults')"
+      :label="currentId ? $t('title.hostparam') : $t('title.hostparam.defaults')"
       name="config"
       active :disabled="!(type == 'clients' || type == 'depots')"
     >
-        Config
+        Config of {{ currentId }}
       </el-tab-pane>
     <el-tab-pane :label="$t('title.hostattr')" name="attr">
-      <FormFHostAttributes :id="props.id" />
+      <FormFHostAttributes :id="currentId" />
     </el-tab-pane>
   </el-tabs>
 </template>
 
 <script setup lang="ts">
 const activeName = ref('attr')
+const currentId = ref<string|undefined>('')
 const props = defineProps({
   id: { type: String, default: undefined },
   type: { type: String, default: 'depots' }
 })
+watch(()=>props.id, ()=>{
+  currentId.value = props.id
+})
+function setId(id:string) {
+  currentId.value = id
+}
 </script>
 
 
