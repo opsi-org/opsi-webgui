510c39bd22b45ca290dd3ca33adb796efc13895e
john <j.john@uib.de>
2024-01-15 16:12:34 +0000

    [chg] move pages

---
+++ frontend/pages/clientslog.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- <ViewVClientsLog :id.sync="id" :as-child="false">
+    <template slot="IDSelection">
+      <TreeTSClientsNotStored :id.sync="id" />
+    </template>
+  </ViewVClientsLog> -->
+</template>
+
+<script lang="ts">
+// import { Component, Vue } from 'nuxt-property-decorator'
+// @Component
+// export default class PClientsLog extends Vue {
+//   id: string = ''
+// }
+</script>
