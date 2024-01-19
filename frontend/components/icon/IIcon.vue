a7cc2ca3c9361bb7317eb395846be60a267f6296
john <j.john@uib.de>
2024-01-19 15:38:52 +0000

    [chg] update icon styles

--- frontend/components/icon/IIcon.vue
+++ frontend/components/icon/IIcon.vue
@@ -1,8 +1,11 @@
 <template>
-  <Icon v-bind="$attrs as any" class="mr-1 ml-1"/>
+  <Icon v-bind="$attrs as any" :class="props.class" />
 </template>
 
 <script setup lang="ts">
 import { Icon } from '@iconify/vue';
+const props = defineProps({
+  class: { type: String, default: '' }
+})
 </script>
 
