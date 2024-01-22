a42baa2a70a4077a39eaf799264316705590cf85
sucher <a.sucher@uib.de>
2024-01-22 10:59:33 +0000

    [fix] add story for terminal

---
+++ frontend/components/view/VAdminTerminal.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { loginlogout } from '~/histoire/histoire-utils'
+import { useConfigserver } from '~/composables/mixins/useGet'
+
+const store = ref<any>({ selectionDepots: [] })
+async function init(data: any) {
+  await loginlogout(data)
+  await useConfigserver(true, store)
+}
+</script>
+<template>
+  <Story :setup-app="init">
+    <Variant title="mobile" :meta="{ wrapperMobile: true }" responsive-disabled>
+      <ViewVAdminTerminal />
+    </Variant>
+    <Variant title="desktop" responsive-disabled>
+      <ViewVAdminTerminal />
+    </Variant>
+  </Story>
+</template>
