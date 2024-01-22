faed4797682bf6e9450a2709554bb587ba958c3a
sucher <a.sucher@uib.de>
2024-01-22 14:50:10 +0000

    [fix] stories

---
+++ frontend/components/form/FTerminalSettings.story.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+// import { useConfigserver } from '~/composables/mixins/useGet';
+// import { loginlogout } from '~/histoire/histoire-utils';
+// const store = ref<any>({ selectionDepots: [] })
+// async function init(data: any) {
+//   await loginlogout(data)
+//   await useConfigserver(true, store)
+// }
+const terminalId = ref<string>('f40dbaa4-dc9f-46c0-9dc5-186a87a3eee5')
+const terminalChannel = ref<string>('service:config:terminal')
+function connect () {}
+function disconnect () {}
+</script>
+<template>
+  <Story>
+  <!-- <Story :setup-app="init"> -->
+    <Variant title="mobile-enabled" :meta="{ wrapperMobile: true }" responsive-disabled>
+      <FormFTerminalSettings
+        v-model:terminalId="terminalId"
+        v-model:terminalChannel="terminalChannel"
+        :disabled="false"
+        @click-connect="connect"
+        @click-disconnect="disconnect"
+      />
+    </Variant>
+    <Variant title="mobile-disabled" :meta="{ wrapperMobile: true }" responsive-disabled>
+      <FormFTerminalSettings
+        v-model:terminalId="terminalId"
+        v-model:terminalChannel="terminalChannel"
+        :disabled="true"
+        @click-connect="connect"
+        @click-disconnect="disconnect"
+      />
+    </Variant>
+
+    <Variant title="desktop-enabled">
+      <FormFTerminalSettings
+        v-model:terminalId="terminalId"
+        v-model:terminalChannel="terminalChannel"
+        :disabled="false"
+        @click-connect="connect"
+        @click-disconnect="disconnect"
+      />
+    </Variant>
+    <Variant title="desktop-disabled">
+      <FormFTerminalSettings
+        v-model:terminalId="terminalId"
+        v-model:terminalChannel="terminalChannel"
+        :disabled="true"
+        @click-connect="connect"
+        @click-disconnect="disconnect"
+      />
+    </Variant>
+  </Story>
+</template>
