80efac6ad97fabb6fbebf99258c55b7aae5b77b7
sucher <a.sucher@uib.de>
2024-01-22 14:58:43 +0000

    [fix] story

---
+++ frontend/components/card/CSupport.story.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// import { useConfigserver } from '~/composables/mixins/useGet';
+// import { loginlogout } from '~/histoire/histoire-utils';
+// const store = ref<any>({ selectionDepots: [] })
+async function init(data: any) {
+  // await loginlogout(data)
+  // await useConfigserver(true, store)
+}
+const supportItem = {
+    title: 'supportPage.forum.title',
+    description: 'supportPage.forum.description',
+    buttonname: 'supportPage.forum.button',
+    link: 'https://forum.opsi.org/index.php'
+  }
+</script>
+<template>
+  <Story :setup-app="init">
+    <Variant title="mobile" :meta="{ wrapperMobile: true }" responsive-disabled>
+      <CardCSupport :item="supportItem" />
+    </Variant>
+    <Variant title="desktop">
+      <CardCSupport :item="supportItem" />
+    </Variant>
+  </Story>
+</template>
