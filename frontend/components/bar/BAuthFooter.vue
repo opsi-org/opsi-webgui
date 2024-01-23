972021e3f9ec08a417f40afce580d9bf3733936e
sucher <a.sucher@uib.de>
2024-01-23 13:30:01 +0000

    [fix] styling of text on bg primary

--- frontend/components/bar/BAuthFooter.vue
+++ frontend/components/bar/BAuthFooter.vue
@@ -2,14 +2,14 @@
   <el-footer class="bg-secondary p-0 max-h-10">
     <el-row class="row-bg bg-primary ml-0 h-100 pl-2 pr-2" justify="space-between">
       <el-col :span="3" class="flex items-stretch">
-        <el-text class="text-xs">{{ staticStrings.t_fixed('uib') }}</el-text>
+        <el-text class="text-xs text-on-primary">{{ staticStrings.t_fixed('uib') }}</el-text>
       </el-col>
       <el-col :span="3" class="flex justify-center">
         <FormitemDDLang :footer="true" /><FormitemDDTheme />
       </el-col>
       <el-col :span="4" class="flex justify-end space-x-1">
-        <el-text class="text-xs">{{ $t('label.version') }}</el-text>
-        <el-text class="text-xs">{{ $config.public.packageVersion }}</el-text>
+        <el-text class="text-xs text-on-primary">{{ $t('label.version') }}</el-text>
+        <el-text class="text-xs text-on-primary">{{ $config.public.packageVersion }}</el-text>
       </el-col>
     </el-row>
   </el-footer>
@@ -25,9 +25,9 @@ const mq = useMQ()
 <style scoped>
 
 
-html.light .el-text,
+/* html.light .el-text,
 body[data-bs-theme="light"] .el-text {
   --el-text-color-regular: var(--el-bg-color);
   color: var(--el-text-color-regular);
-}
+} */
 </style>
--- frontend/components/bar/BTop.vue
+++ frontend/components/bar/BTop.vue
@@ -1,8 +1,8 @@
 <template>
- <el-menu v-if="useMQ().isMobile.value" mode="horizontal" class="border-0"
+ <el-menu v-if="useMQ().isMobile.value" mode="horizontal" class="border-0 text-on-primary"
     :ellipsis="false">
     <el-menu-item index="0" @click="()=>emit('toggleLeft')">
-      <IconIIcon :icon="icons.navmenu" />
+      <IconIIcon :icon="icons.navmenu" class="text-on-primary"/>
     </el-menu-item>
 
     <div class="flex-grow" />
@@ -20,7 +20,7 @@
     > -->
 
       <el-menu-item index="2" type="text" @click="()=>emit('toggleRight')" data-testid="menu-quickpanel">
-        <IconIIcon :icon="icons.quickpanel" />
+        <IconIIcon :icon="icons.quickpanel" class="text-on-primary"/>
         <!-- <div class="flex-grow" /> -->
         <!-- <p class="ml-1">QuickPanel</p> -->
       </el-menu-item>
@@ -30,7 +30,7 @@
     <!-- </el-sub-menu> -->
   </el-menu>
 
- <el-menu v-else mode="horizontal" :ellipsis="false">
+ <el-menu v-else mode="horizontal" :ellipsis="false" class="text-on-primary">
     <el-menu-item index="0" @click="useRouter().push('/clients/')">
       <IconIOpsiLogo class="max-h-full h-full"/>
     </el-menu-item>
@@ -38,7 +38,7 @@
     <div class="flex-grow" />
 
     <el-menu-item index="1" type="text" @click="()=>emit('toggleRight')" data-testid="menu-quickpanel">
-      <IconIIcon :icon="icons.quickpanel" />
+      <IconIIcon :icon="icons.quickpanel" class="text-on-primary"/>
     </el-menu-item>
     <ButtonBTNLogout index="2" :is-menu-item="true"/>
   </el-menu>
