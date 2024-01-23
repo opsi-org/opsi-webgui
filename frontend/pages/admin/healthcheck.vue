c6159bd78e36c7db96ccacdb09d5323c2c5862b1
john <j.john@uib.de>
2024-01-23 17:53:21 +0000

    [chg] update healthcheck page

--- frontend/pages/admin/healthcheck.vue
+++ frontend/pages/admin/healthcheck.vue
@@ -1,3 +1,14 @@
 <template>
   <h1> {{useRoute().fullPath}} </h1>
+  <TabsTServerHealth />
 </template>
+<!-- <template>
+  <TabsTServerHealth />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+export default class PAdminServerHealthCheck extends Vue {
+}
+</script> -->
