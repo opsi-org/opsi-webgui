6f0d5e123a06cc2ade948e9ff6428103ef0826d7
john <j.john@uib.de>
2024-01-23 17:53:42 +0000

    [chg] update health check

--- frontend/components/tabs/TServerHealth.vue
+++ frontend/components/tabs/TServerHealth.vue
@@ -1,4 +1,12 @@
 <template>
+  <el-tabs>
+    <el-tab-pane :label="$t('title.healthcheck')">
+      {{ $t('title.healthcheck') }}
+    </el-tab-pane>
+    <el-tab-pane :label="$t('title.diagnostics')">
+      {{ $t('title.diagnostics') }}
+    </el-tab-pane>
+  </el-tabs>
   <!-- <div data-testid="TServerHealth">
     <b-tabs small lazy>
       <b-tab :title="$t('title.healthcheck')" active>
@@ -11,7 +19,7 @@
   </div> -->
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 // import { Component, Vue } from 'nuxt-property-decorator'
 // import { Icons } from '../../mixins/icons'
 // @Component({ mixins: [Icons] })
