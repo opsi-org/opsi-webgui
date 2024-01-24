9bce7fb5fb4ab09a988026db3f74260e18ea9acb
john <j.john@uib.de>
2024-01-24 11:30:11 +0000

    [chg] add table for healthcheck data

--- frontend/components/tabs/TServerHealth.vue
+++ frontend/components/tabs/TServerHealth.vue
@@ -1,17 +1,26 @@
 <template>
   <IconILoading v-if="isLoading" />
+
   <el-tabs>
     <el-tab-pane :label="$t('title.healthcheck')">
-      {{ fetchedData.health_check }}
+      <!-- {{ fetchedData.health_check }} -->
+      <el-table :data="fetchedData.health_check">
+        <el-table-column prop="check_name" label="Check Name" width="180" />
+        <el-table-column prop="check_description" label="Description" width="180" />
+        <el-table-column prop="check_status" label="Status" />
+      </el-table>
     </el-tab-pane>
     <el-tab-pane :label="$t('title.diagnostics')">
-      {{ {...fetchedData, health_check: void(0)} }}
+      <!-- {{ {...fetchedData, health_check: void(0)} }} -->
+      <pre>{{ JSON.stringify({...fetchedData, health_check: void(0)}, null, 2) }}</pre>
     </el-tab-pane>
   </el-tabs>
 </template>
 
 <script setup lang="ts">
 import { useNotification } from '~/composables/mixins/useComponent';
+import {useIcons} from '../../composables/mixins/useIcons'
+const icons = useIcons()
 const isLoading = ref(false)
 let fetchedData = ref<any>([])
 // let healthCheck: any[] = []
