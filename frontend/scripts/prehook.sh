e6c0eea5256a1ec59596fbfee0b7b9610f87b5b6
sucher <a.sucher@uib.de>
2024-01-24 14:27:41 +0000

    [chg] pre hook

---
+++ frontend/scripts/prehook.sh
@@ -0,0 +1,11 @@
+cd /workspace/frontend/
+file=".tmp_prepush_result_changed_files.log"
+echo "====================================================== Check test files"
+cd /workspace/frontend
+bash ./frontend/scripts/playwright_coverage_by_files.sh $file
+# rm $file
+echo "======================================================"
+echo ""
+cd ..
+echo "exitcode $exit_code"
+exit $exit_code
