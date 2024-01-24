7f96d6aee5d113c6f2e7aef03b02bb8ce4b470e7
john <j.john@uib.de>
2024-01-24 10:49:21 +0000

    [chg] add loading status

--- frontend/components/tabs/TServerHealth.vue
+++ frontend/components/tabs/TServerHealth.vue
@@ -1,36 +1,38 @@
 <template>
+  <IconILoading v-if="isLoading" />
   <el-tabs>
     <el-tab-pane :label="$t('title.healthcheck')">
-      {{ $t('title.healthcheck') }}
-      {{ healthCheck }}
+      {{ fetchedData.health_check }}
     </el-tab-pane>
     <el-tab-pane :label="$t('title.diagnostics')">
-      {{ $t('title.diagnostics') }}
-      {{ diagnostics }}
+      {{ {...fetchedData, health_check: void(0)} }}
     </el-tab-pane>
   </el-tabs>
 </template>
 
 <script setup lang="ts">
 import { useNotification } from '~/composables/mixins/useComponent';
-let fetchedData = ref<any>()
-let healthCheck: any[] = []
-let diagnostics: any[] = []
+const isLoading = ref(false)
+let fetchedData = ref<any>([])
+// let healthCheck: any[] = []
+// let diagnostics: any[] = []
 
 onMounted(async ()=> {
   await fetch()
 })
 
 async function fetch() {
+  isLoading.value = true
   const {data, error} = await useApiGETBody('/opsidata/server/diagnostic')
   if (error) {
     console.log(error)
     useNotification().error(error)
+    isLoading.value = false
     return
   }
-  fetchedData.value = data.value
-  healthCheck = data?.value?.health_check
-  diagnostics = {...data?.value, health_check: void(0)}
-  // const { health_check, ...diagnostics } = data.value
+  fetchedData.value = data?.value
+  isLoading.value = false
+  // healthCheck = data?.value?.health_check
+  // diagnostics = {...data?.value, health_check: void(0)}
 }
 </script>
