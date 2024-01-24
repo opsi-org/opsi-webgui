5bbcea9e1616df579a16ab1856815c9813029487
john <j.john@uib.de>
2024-01-24 10:41:40 +0000

    [chg] use single fetch for both healthcheck and diagnostics

--- frontend/components/tabs/TServerHealth.vue
+++ frontend/components/tabs/TServerHealth.vue
@@ -2,27 +2,35 @@
   <el-tabs>
     <el-tab-pane :label="$t('title.healthcheck')">
       {{ $t('title.healthcheck') }}
+      {{ healthCheck }}
     </el-tab-pane>
     <el-tab-pane :label="$t('title.diagnostics')">
       {{ $t('title.diagnostics') }}
+      {{ diagnostics }}
     </el-tab-pane>
   </el-tabs>
-  <!-- <div data-testid="TServerHealth">
-    <b-tabs small lazy>
-      <b-tab :title="$t('title.healthcheck')" active>
-        <ViewVHealthCheck />
-      </b-tab>
-      <b-tab :title="$t('title.diagnostics')">
-        <ViewVDiagnostics />
-      </b-tab>
-    </b-tabs>
-  </div> -->
 </template>
 
 <script setup lang="ts">
-// import { Component, Vue } from 'nuxt-property-decorator'
-// import { Icons } from '../../mixins/icons'
-// @Component({ mixins: [Icons] })
-// export default class TServerHealth extends Vue {
-// }
+import { useNotification } from '~/composables/mixins/useComponent';
+let fetchedData = ref<any>()
+let healthCheck: any[] = []
+let diagnostics: any[] = []
+
+onMounted(async ()=> {
+  await fetch()
+})
+
+async function fetch() {
+  const {data, error} = await useApiGETBody('/opsidata/server/diagnostic')
+  if (error) {
+    console.log(error)
+    useNotification().error(error)
+    return
+  }
+  fetchedData.value = data.value
+  healthCheck = data?.value?.health_check
+  diagnostics = {...data?.value, health_check: void(0)}
+  // const { health_check, ...diagnostics } = data.value
+}
 </script>
