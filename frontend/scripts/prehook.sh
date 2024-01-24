f9b777da1a9b49a1b6052e78d88fa77a7a6eec7b
sucher <a.sucher@uib.de>
2024-01-24 14:28:22 +0000

    [chg] pre hook

--- frontend/scripts/prehook.sh
+++ frontend/scripts/prehook.sh
@@ -4,6 +4,7 @@ echo "====================================================== Check test files"
 cd /workspace/frontend
 bash ./frontend/scripts/playwright_coverage_by_files.sh $file
 # rm $file
+
 echo "======================================================"
 echo ""
 cd ..
