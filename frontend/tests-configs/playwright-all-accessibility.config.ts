ea19381f2eb08756ccfc0603cbe36cea9d219d68
sucher <a.sucher@uib.de>
2024-01-29 08:24:41 +0000

    [chg] add caller for accessibility tests (they do not work)

--- frontend/tests-configs/playwright-all-accessibility.config.ts
+++ frontend/tests-configs/playwright-all-accessibility.config.ts
@@ -5,9 +5,10 @@ import generalconfig from './playwright/config/playwright-config'
 
 // /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config: any = {
-  testDir: '../', // /workspace/opsiweb
+  testDir: '../tests/accessibility/', // /workspace/opsiweb
   // testDir: '../../../', // /workspace/opsiweb/uib-components
-  testMatch: '*.test.*(accessibility).js',
+  testMatch: '*',
+  // testMatch: '*.test.accessibility.js',
   reporter: [
     ['list'],
     ['junit', { outputFile: 'test-result-pw-reporter-accessibility.xml' }]
