db7497911d4f963d25660d3c09c9835acae7f33e
Jeena John <j.john@uib.de>
2024-02-07 13:27:18 +0000

    [new,pub] Added feature to create product groups.

--- opsiweb/uib-components/components/view/VProdGroupActions.vue
+++ opsiweb/uib-components/components/view/VProdGroupActions.vue
@@ -2,6 +2,16 @@
   <div class="VGroups" data-testid="VGroups">
     <OverlayOLoading :is-loading="$fetchState.pending" />
     <AlertAAlert ref="groupAlert" data-testid="groupAlert" />
+    <b-button
+      class="border-0 ml-auto"
+      variant="outline-primary"
+      size="sm"
+      :title="$t('group.addSubgroup')"
+      block
+      @click="$bvModal.show('MCreateProdGroup')"
+    >
+      <IconIIcon :icon="icon.group" /><IconIIcon :icon="icon.add" font-scale="0.8" /> {{ $t('group.addProdgroup') }}
+    </b-button>
     <b-row>
       <b-col>
         <treeselect
@@ -18,7 +28,7 @@
             <template v-if="node.isBranch">
               {{ node.label }}
               <div class="float-right">
-                <b-button
+                <!-- <b-button
                   class="border-0"
                   variant="outline-primary"
                   size="sm"
@@ -26,7 +36,7 @@
                   @click="showChild('editGroup')"
                 >
                   <IconIIcon :icon="icon.pencil" />
-                </b-button>
+                </b-button> -->
                 <b-button
                   class="border-0"
                   variant="outline-primary"
@@ -54,15 +64,6 @@
                 >
                   <IconIIcon :icon="icon.product" /><IconIIcon :icon="icon.add" font-scale="0.8" />
                 </b-button>
-                <!-- <b-button
-                  class="border-0"
-                  variant="outline-primary"
-                  size="sm"
-                  :title="$t('group.addSubgroup')"
-                  @click="showChild('addSubgroup')"
-                >
-                  <IconIIcon :icon="icon.group" /><IconIIcon :icon="icon.add" font-scale="0.8" />
-                </b-button> -->
               </div>
             </template>
             <template v-else>
@@ -82,43 +83,50 @@
           </div>
         </treeselect>
       </b-col>
+      <b-modal
+        id="MCreateProdGroup"
+        data-testid="MCreateProdGroup"
+        :title="$t('group.addProdgroup')"
+        centered
+        scrollable
+        hide-footer
+      >
+        <b-form>
+          <b-form-input
+            v-model="subgroup.groupId"
+            size="sm"
+            trim
+            :placeholder="$t('group.prodgroupname')"
+            :state="subgroup.groupId.length > 0 && subgroup.groupId.length < 255"
+            @keydown.enter.prevent="createSubGroup"
+          />
+          <b-form-input
+            v-model="subgroup.description"
+            size="sm"
+            trim
+            :placeholder="$t('table.fields.description')"
+            :state="subgroup.description.length >= 0 && subgroup.description.length < 100"
+            @keydown.enter.prevent="createSubGroup"
+          />
+          <b-form-input
+            v-model="subgroup.notes"
+            size="sm"
+            trim
+            :placeholder="$t('table.fields.notes')"
+            :state="subgroup.notes.length >= 0 && subgroup.notes.length < 500"
+            @keydown.enter.prevent="createSubGroup"
+          />
+          <b-button class="float-right" size="sm" variant="success" data-testid="createSubGroup" @click="createSubGroup">
+            {{ $t("button.create") }}
+          </b-button>
+        </b-form>
+      </b-modal>
       <b-col v-if="action && selectedvalue">
         <span class="text-small"><b> {{ title + t_fixed('keep-english.title.delimiter') }}</b><i>{{ selectedvalue.text }}</i></span>
         <b-button class="float-right border-0" variant="outline-primary" size="sm" @click="action = ''">
           <IconIIcon :icon="icon.x" />
         </b-button>
         <br><br>
-        <!-- <template v-if="action == 'addSubgroup'">
-          <b-form>
-            <b-form-input
-              v-model="subgroup.groupId"
-              size="sm"
-              trim
-              :placeholder="$t('group.subgroupname')"
-              :state="subgroup.groupId.length > 0 && subgroup.groupId.length < 255"
-              @keydown.enter.prevent="createSubGroup"
-            />
-            <b-form-input
-              v-model="subgroup.description"
-              size="sm"
-              trim
-              :placeholder="$t('table.fields.description')"
-              :state="subgroup.description.length >= 0 && subgroup.description.length < 100"
-              @keydown.enter.prevent="createSubGroup"
-            />
-            <b-form-input
-              v-model="subgroup.notes"
-              size="sm"
-              trim
-              :placeholder="$t('table.fields.notes')"
-              :state="subgroup.notes.length >= 0 && subgroup.notes.length < 500"
-              @keydown.enter.prevent="createSubGroup"
-            />
-            <b-button class="float-right" size="sm" variant="success" data-testid="createSubGroup" @click="createSubGroup">
-              {{ $t("button.create") }}
-            </b-button>
-          </b-form>
-        </template> -->
         <template v-if="action == 'addToGroup'">
           <b-form-select
             v-model="selectedProducts"
@@ -137,7 +145,7 @@
             {{ $t("group.add") }}
           </b-button>
         </template>
-        <template v-else-if="action == 'editGroup'">
+        <!-- <template v-else-if="action == 'editGroup'">
           <b-form>
             <treeselect
               v-model="updategroupparent"
@@ -163,7 +171,7 @@
               {{ $t("button.update") }}
             </b-button>
           </b-form>
-        </template>
+        </template> -->
         <template v-else-if="action == 'deletegroup'">
           <small> {{ $t('group.deletegroup.confirm', {type: 'product'}) }}</small>
           <b-button class="float-right" size="sm" variant="danger" data-testid="deleteGroup" @click="deleteGroup">
@@ -209,7 +217,7 @@ export default class VGroups extends Vue {
   productIds: Array<string> = []
   selectedProducts: Array<string> = []
   group: Array<object>|undefined = undefined
-  updategroupparent: any = null
+  // updategroupparent: any = null
   selectedvalue: any = null
   subgroup: any = {
     parentGroupId: '',
@@ -218,11 +226,11 @@ export default class VGroups extends Vue {
     notes: ''
   }
 
-  updategroup = {
-    parent: '',
-    description: '',
-    notes: ''
-  }
+  // updategroup = {
+  //   parent: '',
+  //   description: '',
+  //   notes: ''
+  // }
 
   @selections.Getter public selectionDepots!: Array<string>
 
@@ -241,14 +249,14 @@ export default class VGroups extends Vue {
     }
   }
 
-  normalizerUpdateGroup (node: any) {
-    return {
-      id: node.id,
-      label: node.text,
-      isDisabled: node.type === 'ObjectToGroup',
-      children: node.children ? Object.values(node.children) : {}
-    }
-  }
+  // normalizerUpdateGroup (node: any) {
+  //   return {
+  //     id: node.id,
+  //     label: node.text,
+  //     isDisabled: node.type === 'ObjectToGroup',
+  //     children: node.children ? Object.values(node.children) : {}
+  //   }
+  // }
 
   async fetch () {
     await this.fetchGroups()
@@ -264,7 +272,7 @@ export default class VGroups extends Vue {
   async fetchProducts () {
     await this.$axios.$get(`/api/opsidata/depots/products?productType=LocalbootProduct&selectedDepots=${this.selectionDepots}`)
       .then((response) => {
-        this.productIds = response.map(function (item) {
+        this.productIds = response.map(function (item: { productId: string }) {
           return item.productId
         })
       }).catch((error) => {
@@ -285,28 +293,29 @@ export default class VGroups extends Vue {
   }
 
   async createSubGroup () {
-    this.subgroup.parentGroupId = this.selectedvalue.text
+    // this.subgroup.parentGroupId = this.selectedvalue.text
     await this.$axios.$post('/api/opsidata/products/groups', this.subgroup)
       .then(async () => {
         this.showToastSuccess(this.$t('message.success.save.create.group', { group: this.subgroup.groupId }))
         await this.reloadGroup()
+        this.$bvModal.hide('MCreateProdGroup')
       })
       .catch((error) => {
         this.showToastError(error)
       })
   }
 
-  async updateGroup () {
-    this.updategroup.parent = this.updategroupparent ? this.updategroupparent.text : ''
-    await this.$axios.$put(`/api/opsidata/products/groups/${this.selectedvalue.text}`, this.updategroup)
-      .then(async () => {
-        this.showToastSuccess(this.$t('message.success.save.update.group', { group: this.selectedvalue.text }))
-        await this.reloadGroup()
-      })
-      .catch((error) => {
-        this.showToastError(error)
-      })
-  }
+  // async updateGroup () {
+  //   this.updategroup.parent = this.updategroupparent ? this.updategroupparent.text : ''
+  //   await this.$axios.$put(`/api/opsidata/products/groups/${this.selectedvalue.text}`, this.updategroup)
+  //     .then(async () => {
+  //       this.showToastSuccess(this.$t('message.success.save.update.group', { group: this.selectedvalue.text }))
+  //       await this.reloadGroup()
+  //     })
+  //     .catch((error) => {
+  //       this.showToastError(error)
+  //     })
+  // }
 
   async addProducts () {
     await this.$axios.$post(`/api/opsidata/products/groups/${this.selectedvalue.text}/products`, this.selectedProducts)
