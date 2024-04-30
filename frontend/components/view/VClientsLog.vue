293e0243e30ee5b5db3d017adf3f5094ea13bf0b
john <j.john@uib.de>
2024-04-30 15:00:55 +0000

    [chg] Refactor Client Logs

--- frontend/components/view/VClientsLog.vue
+++ frontend/components/view/VClientsLog.vue
@@ -1,6 +1,6 @@
 <template>
-  <el-form :inline="true" label-position="top" class="mt-2">
-    <el-form-item :label="$t('form.clientId')" v-if="props.isChild === false">
+  <el-form :inline="true" label-position="top" class="mt-2" v-loading="isLoading">
+    <el-form-item :label="$t('form.clientId')" v-if="!props.isChild">
       <SelectSHosts :type="type" @change="setId" :id="logrequest.selectedClient" />
     </el-form-item>
     <el-form-item :label="$t('form.logtype')">
@@ -10,21 +10,21 @@
     </el-form-item>
     <template v-if="fetchedData.length > 1">
       <el-form-item :label="$t('form.filter.logs')">
-        <el-input v-model="filterQuery" @input="filterLogByQuery" clearable style="width: 200px" />
+        <el-input v-model="filterQuery" clearable style="width: 200px" @input="filterLogByQuery" />
       </el-form-item>
       <el-form-item :label="$t('form.loglevel')">
         <el-slider v-model="loglevel" show-stops :max="8" style="min-width: 200px" />
       </el-form-item>
     </template>
   </el-form>
-  <el-scrollbar height="79vh" v-if="fetchedData.length > 1" v-loading="isLoading">
+  <el-scrollbar height="79vh" v-if="fetchedData.length > 1">
     <span
-      v-for="(log, index) in filteredData"
-      :key="index"
+      v-for="log in filteredData"
+      :key="log"
       :style="{ color: getColorBasedOnLoglevel(log) }"
       :class="{ 'd-none': !isLoglevelSmaller(log, loglevel) }"
     >
-    {{ index }} - {{ log }} <br>
+    {{ log }} <br>
     </span>
   </el-scrollbar>
   <el-alert v-else :title="$t('message.info.nologs')" type="info" show-icon :closable="false" />
@@ -35,7 +35,6 @@ import { useNotification } from '~/composables/mixins/useComponent';
 import type { T_ClientLog } from '~/types/APItypes';
 
 const $t = useI18n().t
-// TODO: messagebus event:log_updated
 
 const props = defineProps({
   id: { type: String, default: '' },
@@ -46,44 +45,29 @@ const props = defineProps({
 let fetchedData = ref<Array<string>>([])
 let filteredData = ref<Array<string>>([])
 const isLoading = ref(false)
-let logrequest = { selectedClient: '', selectedLogType: '' }
+let logrequest = { selectedClient: props.id, selectedLogType: 'instlog' }
 const logTypes = ['bootimage', 'clientconnect', 'instlog', 'opsiconfd', 'userlogin']
 const loglevel = ref(5)
 const logtype = ref('instlog')
 const filterQuery = ref('')
-
-onMounted(async ()=> {
-  if (props.id!= ''){ await fetch() }
-})
-
-watch(()=>logrequest.selectedClient, ()=>{
-  fetch()
-})
-
-watch(()=>logtype.value, ()=>{
-  fetch()
-})
+// TODO: messagebus event:log_updated
+watch([()=>props.id, ()=>logtype.value], fetch, { immediate: true })
 
 async function fetch() {
   isLoading.value = true
-  logrequest.selectedClient = props.id
-  if(logtype.value!=''){
-    logrequest.selectedLogType = logtype.value
-  } else { logrequest.selectedLogType = 'instlog' }
-  const {data, error} = await useApiGETBody<T_ClientLog>('/opsidata/log', logrequest )
-  if (error) {
+  logrequest.selectedLogType = logtype.value
+  try {
+    const {data, error} = await useApiGETBody<T_ClientLog>('/opsidata/log', logrequest)
+    if (error) throw error
+    if (!data.value) throw new Error($t('message.error.empty-response'))
+    fetchedData.value = data.value.result
+    filteredData.value = fetchedData.value
+  } catch (error) {
     console.error(error)
     useNotification($t).error(error)
+  } finally {
     isLoading.value = false
-    return
-  } else if (!data.value) {
-    useNotification($t).error($t('message.error.empty-response'))
-    isLoading.value = false
-    return
   }
-  fetchedData.value = data.value.result
-  filteredData.value = fetchedData.value
-  isLoading.value = false
 }
 
 function filterLogByQuery() {
@@ -91,50 +75,18 @@ function filterLogByQuery() {
 }
 
 function isLoglevelSmaller (logrow:string, loglevel:number) {
-  // match charakters in beginning with [<=loglevel] or not [0-9]
   const rxSelf2 = new RegExp('^((\\[[0-' + loglevel + ']\\])|[^\\[0-9\\]])', 'g')
   const result = RegExp(rxSelf2).exec(logrow)
   return !!result
 }
 
-function setId(id:string) {
-  logrequest.selectedClient = id
-}
-
 function getColorBasedOnLoglevel(log:string) {
   const logLevel = parseInt(log.charAt(1), 10);
-  let color;
-  switch (logLevel) {
-      case 1:
-          color = 'var(--opsi-log-essential)';
-          break;
-      case 2:
-          color = 'var(--opsi-log-critical)';
-          break;
-      case 3:
-          color = 'var(--opsi-log-error)';
-          break;
-      case 4:
-          color = 'var(--opsi-log-warning)';
-          break;
-      case 5:
-          color = 'var(--opsi-log-notice)';
-          break;
-      case 6:
-          color = 'var(--opsi-log-info)';
-          break;
-      case 7:
-          color = 'var(--opsi-log-debug)';
-          break;
-      case 8:
-          color = 'var(--opsi-log-trace)';
-          break;
-      case 9:
-          color = 'var(--opsi-log-secret)';
-          break;
-      default:
-          color = 'var(--color)';
-  }
-  return color;
+  const colors = ['var(--opsi-log-essential)', 'var(--opsi-log-critical)', 'var(--opsi-log-error)', 'var(--opsi-log-warning)', 'var(--opsi-log-notice)', 'var(--opsi-log-info)', 'var(--opsi-log-debug)', 'var(--opsi-log-trace)', 'var(--opsi-log-secret)'];
+  return colors[logLevel] || 'var(--color)';
+}
+
+function setId(id:string) {
+  logrequest.selectedClient = id
 }
 </script>
