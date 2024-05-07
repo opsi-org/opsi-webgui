f877aa46369af80be0099e04dfb8fc78c941e720
sucher <a.sucher@uib.de>
2024-05-07 13:03:22 +0000

    [fix] update index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,24 @@
 <template>
-  <!-- <b-card class="text-center index-card"> -->
-  <el-card style="max-width: 480px">
-    <h1 class="title">
-      {{ $t("uib") }} <br>
-      {{ $t("button.login") }} <br>
-    </h1>
+  <div class="max-w-[480px] index-card m-auto  ">
+    <div class="grid place-items-center">
 
+    <h1 class="title m-auto">
+      {{ t_fixed("title.project") }} <br/>
+    </h1>
+    <!-- <h2 class="m-auto">
+      OPSICONFD PORT {{ $config.public.OPSICONFD_PORT }}
+    </h2> -->
+    <IconIOpsiLogo
+      :short="false"
+      :light="storeSettings().isLight"
+      classes="w-100 index-opsi-logo"
+    />
+    <IconIUibLogo
+      :short="false"
+      :light="storeSettings().isLight"
+      classes="w-50 index-uib-logo "
+      />
+    <!--
     <TestEPButton />
     <FormitemDDTheme />
     <br /> screen: {{  mq.$mq }}
@@ -13,15 +26,27 @@
 
     <TestMixin />
     <IconILoading animation="cylon" />
-    <TestFetch />
+    <TestFetch /> -->
     <!-- <br /> isPreferredDark: {{  mq.isPreferredDark }} -->
+    <!-- <IconELILoading animation="cylon" /> -->
+    </div>
 
-      <!-- <IconELILoading animation="cylon" /> -->
-  </el-card>
+  </div>
 </template>
 
 <script setup>
+import { useStrings } from '~/composables/mixins/useStrings';
+import { useMBus } from '~/composables/mixins/useMessagebus';
+const $config = useRuntimeConfig()
 const mq = useMQ()
+const $t = useI18n().t
+const { t_fixed } = useStrings()
+
+const msgbus = useMBus(wsBusMsgObjectChanged, false, $t)
+
+function wsBusMsgObjectChanged(msg) {
+  console.log('wsBusMsgObjectChanged', msg)
+}
 </script>
 
 <style>
@@ -30,4 +55,13 @@ const mq = useMQ()
   background-color: rgb(56, 56, 56);
   color: blue;
 } */
+.index-card .index-opsi-logo {
+  max-width: 700px;
+}
+.index-card .index-uib-logo {
+  max-width: 400px;
+}
+.index-card .title {
+  font-size: xx-large;
+}
 </style>
