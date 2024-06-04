2395ab8299fa4350d213eb2b7b6ced5d4fae27fd
john <j.john@uib.de>
2024-06-04 09:27:43 +0000

    [fix] Minified version of DefaultLayout with SideMenu and QuickPanel

--- frontend/components/bar/BSide.vue
+++ frontend/components/bar/BSide.vue
@@ -1,63 +1,32 @@
 <template>
-  <div data-testid="BSide"
-
-  >
-    <el-menu router
-      :default-active="router.currentRoute.value?.fullPath"
-      :collapse="!mq.isMobile.value && isCollapse"
-      class="el-menu-mywrapper overflow-hidden max-w-full"
-      :class="{'max-height-side': !mq.isMobile.value }"
-      style="--el-menu-icon-width: 244px;"
-      type="primary"
-    >
-    <!-- :class="{collapse: !isCollapse}" -->
+  <div data-testid="BSide">
+    <el-menu router :default-active="router.currentRoute.value?.fullPath" :collapse="isCollapse"
+      class="el-menu-mywrapper overflow-hidden max-w-full" :class="{'max-height-side': !mq.isMobile.value }"
+      style="--el-menu-icon-width: 244px;" type="primary">
       <span v-for="item in navItems" :key="item.title">
-        <template v-if="item.submenu">
-          <!-- menus with children -->
-          <el-sub-menu :index="item.route" :route="item.route"
-          popper-class="text-on-primary" class="text-on-primary"
-          :data-testid="'NSidebar-' + item.title"
-          >
-          <!-- :expand-open-icon="isCollapse ? ' ' : ''" :expand-close-icon="isCollapse ? ' ' : ''" -->
-            <template #title>
-              <div
-              class="text-on-primary"
-              :class="{
-                'contents': true,
-                'selected': item.routeSubpath && router.currentRoute.value?.fullPath.includes(item.routeSubpath),
-              }"
-              >
-                <IconIIcon v-if="item.icon" :icon="item.icon" class="mr-1"/>
-                <span v-if="mq.isMobile.value || !isCollapse" >{{ $t(item.title) }}</span>
-                <div class="min-w-full"></div>
-                <!-- <div class="flex-grow" />
-                  <IconIIcon v-if="item.icon" :icon="icons.arrowDoubleDown"/> -->
-                </div>
-            </template>
-
-            <!-- sub menus -->
-            <span v-for="sub in item.submenu" :key="sub.title" >
-              <el-menu-item :disabled="sub.disabled" :index="sub.route" :route="sub.route"
-              :data-testid="'NICollapsible-submenu-' + sub.title" class="text-on-primary">
-                <span class="text-on-primary">{{ $t(sub.title) }}</span>
-              </el-menu-item>
-            </span>
-
-          </el-sub-menu>
-        </template>
-        <template v-else>
-          <!-- menus without children -->
-          <el-menu-item :index="item.route" :route="item.route" class="text-on-primary"
-          :data-testid="'NSidebar-' + item.title">
-            <IconIIcon v-if="item.icon" :icon="item.icon" class="mr-1" />
-              <span v-if="mq.isMobile.value || !isCollapse">{{ $t(item.title) }}</span>
+        <el-sub-menu v-if="item.submenu" :index="item.route" :route="item.route"
+          popper-class="text-on-primary" class="text-on-primary" :data-testid="'NSidebar-' + item.title">
+          <template #title>
+            <div class="text-on-primary" :class="{'contents': true, 'selected': isSelected(item)}">
+              <IconIIcon v-if="item.icon" :icon="item.icon" class="mr-1"/>
+              <span v-if="showTitle">{{ $t(item.title) }}</span>
+            </div>
+          </template>
+          <el-menu-item v-for="sub in item.submenu" :key="sub.title" :disabled="sub.disabled" :index="sub.route" :route="sub.route"
+            :data-testid="'NICollapsible-submenu-' + sub.title" class="text-on-primary">
+            <span class="text-on-primary">{{ $t(sub.title) }}</span>
           </el-menu-item>
-        </template>
+        </el-sub-menu>
+        <el-menu-item v-else :index="item.route" :route="item.route" class="text-on-primary"
+          :data-testid="'NSidebar-' + item.title">
+          <IconIIcon v-if="item.icon" :icon="item.icon" class="mr-1" />
+          <span v-if="showTitle">{{ $t(item.title) }}</span>
+        </el-menu-item>
       </span>
     </el-menu>
-    <div v-if="!mq.isMobile.value" class="menu-footer relative inset-x-0 bottom-0 w-full text-on-primary">
+    <div v-if="!mq.isMobile.value" class="menu-footer relative inset-x-0 bottom-10 w-full text-on-primary">
       <el-checkbox-button v-model="isCollapse" class="w-full text-on-primary" type="">
-          {{ isCollapse ? '>>' : 'Collapse' }}
+        {{ isCollapse ? '>>' : 'Collapse' }}
       </el-checkbox-button>
     </div>
   </div>
@@ -69,7 +38,6 @@ import {useIcons} from '../../composables/mixins/useIcons'
 interface INavItem {
   title: string
   route: string
-  routeSubpath?: string
   icon?: string
   disabled?: boolean
   submenu?: Array<INavItem>
@@ -78,18 +46,14 @@ interface INavItem {
 const icons = useIcons()
 const router = useRouter()
 const mq = useMQ()
-
 const settings = storeSettings()
 const { menuCollapsed } = storeToRefs(settings)
 const {config} = storeToRefs(storeConfigapp())
-
 const isCollapse = ref(menuCollapsed.value)
 const emit = defineEmits(['changeSmall'])
-const { selectionDepots } = storeSelections()
 const navItems = computed<Array<INavItem>>(() => [
   {
     title: 'title.depots',
-    routeSubpath: '/servers/',
     route: '/servers/',
     icon: icons.depots,
     submenu: [
@@ -100,7 +64,6 @@ const navItems = computed<Array<INavItem>>(() => [
   {
     title: 'title.clients',
     route: '/clients/',
-    routeSubpath: '/clients/',
     icon: icons.client,
     submenu: [
       { title: 'title.allClients', route: '/clients/'},
@@ -112,11 +75,9 @@ const navItems = computed<Array<INavItem>>(() => [
   },
   { title: 'title.products', icon: icons.product, route: '/products/LocalbootProduct' },
   { title: 'title.groups', icon: icons.group, route: '/groups/' },
-
   {
     title: 'title.administration',
     route: (config.value?.['terminal.forbidden'] === true) ? '/admin/' : '/admin/terminal',
-    routeSubpath: '/admin',
     icon: icons.admin,
     submenu: [
       { title: 'title.adminterminal', route: '/admin/terminal', disabled: (config.value?.['terminal.forbidden'] === true) },
@@ -126,86 +87,16 @@ const navItems = computed<Array<INavItem>>(() => [
     ]
   },
   { title: 'title.support', icon: icons.support, route: '/support' }
-]
-)
+])
+
 watch(isCollapse, (val: boolean) => {
   emit('changeSmall', val)
 })
+
+const isSelected = (item: INavItem) => item.route && router.currentRoute.value?.fullPath.includes(item.route)
+const showTitle = computed(() => mq.isMobile.value || !isCollapse.value)
 </script>
 
 <style scoped>
-.contents {
-  display: contents !important;
-  /* max-width: 100px !important; */
-}
-.selected {
-  color: var(--el-color-primary);
-  color: var(--el-menu-active-color);
-}
-
-.max-height-side {
-  height: calc(100vh - 72px);
-}
-:deep(.el-menu-item.is-active path) {
-  color: var(--opsi-genral-white) !important;
-  color: var(--el-menu-active-color) !important;
-}
-/* :deep(.el-sub-menu__icon-arrow) {
-color: white !important;
-} */
-
-:deep(.el-menu-item.is-active),
-  :deep(.el-menu-item.is-active svg),
-  :deep(.el-sub-menu.is-active > .el-sub-menu__title)
-   {
-    --el-menu-active-color: var(--opsi-genral-white);
-    color: var(--opsi-genral-white);
-    background-color: var(--primary-color-dark);
-  }
-  .el-menu-item.text-on-primary:hover,
-  .el-menu-item.text-on-primary:hover .text-on-primary,
-  :deep(.el-sub-menu__title:hover .text-on-primary),
-  :deep(.el-sub-menu__title:hover .el-sub-menu__icon-arrow)
-  {
-    --el-text-color-regular: var(--fg-color) !important;
-    color: var(--el-text-color-regular) !important;
-  }
-  :deep(.el-sub-menu__icon-arrow) {
-  /* .el-menu--collapse >>> .el-sub-menu__icon-arrow { */
-    /* display: none !important; */
-    margin-right: -10px !important;
-
-    --el-text-color-regular: var(--opsi-genral-white);
-    color: var(--el-text-color-regular);
-  }
-
-  :deep(.el-checkbox-button__inner) {
-  /* .menu-footer >>> .el-checkbox-button__inner{ */
-    width: 100% !important;
-    background-color: var(--el-checkbox-button-bg-color);
-    border: 0px;
-    border-top: 1px solid var(--el-menu-border-color);
-    /* border-top: 1px solid var(--el-checkbox-button-checked-text-color); */
-  }
-
-  /* .el-aside >>> */
-  .el-menu-mywrapper  {
-    border: 0 !important;
-    /* border-color: green !important; */
-  }
-  :deep(.iconify) {
-    min-width: 40px;
-  }
-  :deep(.el-sub-menu__title) {
-    max-width: 100% !important;
-    padding: 5px !important;
-    text-align: left;
-  }
-  .el-menu > span > .el-menu-item {
-    padding-left: 50px !important;
-  }
-  .el-menu-mywrapper > span > .el-menu-item {
-    padding-left: 5px !important;
-  }
-
+.contents{display:contents!important}.selected{color:var(--el-color-primary);color:var(--el-menu-active-color)}.max-height-side{height:calc(100vh - 72px)}:deep(.el-menu-item.is-active path){color:var(--opsi-genral-white)!important;color:var(--el-menu-active-color)!important}:deep(.el-menu-item.is-active),:deep(.el-menu-item.is-active svg),:deep(.el-sub-menu.is-active > .el-sub-menu__title){--el-menu-active-color:var(--opsi-genral-white);color:var(--opsi-genral-white);background-color:var(--primary-color-dark)}.el-menu-item.text-on-primary:hover,.el-menu-item.text-on-primary:hover .text-on-primary,:deep(.el-sub-menu__title:hover .text-on-primary),:deep(.el-sub-menu__title:hover .el-sub-menu__icon-arrow){--el-text-color-regular:var(--fg-color)!important;color:var(--el-text-color-regular)!important}:deep(.el-sub-menu__icon-arrow){margin-right:-10px!important;--el-text-color-regular:var(--opsi-genral-white);color:var(--el-text-color-regular)}:deep(.el-checkbox-button__inner){width:100%!important;background-color:var(--el-checkbox-button-bg-color);border:0;border-top:1px solid var(--el-menu-border-color)}.el-menu-mywrapper{border:0!important}:deep(.iconify){min-width:40px}:deep(.el-sub-menu__title){max-width:100%!important;padding:5px!important;text-align:left}.el-menu > span > .el-menu-item{padding-left:50px!important}.el-menu-mywrapper > span > .el-menu-item{padding-left:5px!important}
 </style>
--- frontend/components/layout/LDefault.vue
+++ frontend/components/layout/LDefault.vue
@@ -1,100 +1,40 @@
 <template>
-  <div :class="{
-    'is-mobile': mq.isMobile.value,
-    'is-not-mobile': !mq.isMobile.value,
-  }" >
-    <el-container class="h-screen w-screen">
-      <el-header class="min-w-screen max-h-10 p-0 m-0 border-0">
-        <BarBTop
-          class="max-h-full max-w-full"
-          @toggle-left="()=>toggleSide('left')"
-          @toggle-right="()=>toggleSide('right')"
-        />
-      </el-header>
-      <el-container
-       class="h-screen max-h-screen overflow-hidden"
-       :class="{
-        'left-collapsed': !mq.isMobile.value && leftSideIsSmall,
-        'left-opened': !mq.isMobile.value && !leftSideIsSmall,
-        'right-opened': !mq.isMobile.value && rightSideVisible,
+  <el-container>
+    <el-header>
+      <BarBTop @toggle-left="toggleSide('left')" @toggle-right="toggleSide('right')" />
+    </el-header>
+    <el-container>
+      <el-aside v-if="leftSideVisible" class="el-aside-left"
+      :class="{
+        'aside-left-collapsed': !mq.isMobile.value && leftSideIsSmall,
        }"
       >
-        <!-- LEFT SIDE -->
-        <el-aside
-          v-if="!mq.isMobile.value || leftSideVisible"
-          class="el-aside-left"
-          :class="{
-            'absolute z-20 grid w-screen': mq.isMobile.value
-            }"
-        >
-          <div
-            :class="{
-              'hidden': !mq.isMobile.value,
-              'fixed bg-color opacity-70 z-10 w-screen h-full': mq.isMobile.value,
-              }"
-            @click.self="toggleSide('left')"
-          ></div>
-          <el-scrollbar
-            style="border-right: 1px solid var(--el-border-color)"
-            :class="{
-            'border-0 border-r': true,
-            // 'w-48': !mq.isMobile.value && !leftSideIsSmall,
-            'max-w-full': true,
-            'w-16': !mq.isMobile.value && leftSideIsSmall,
-            ' z-40 bg-color opacity-100': mq.isMobile.value,
-          }">
-            <BarBSide @change-small="setLeftCollapse"/>
-          </el-scrollbar>
-        </el-aside>
+        <el-scrollbar>
+          <BarBSide @change-small="setLeftCollapse"/>
+        </el-scrollbar>
+      </el-aside>
 
-        <!-- MAIN CONTENT -->
-        <el-main class="z-0 p-2"
-          :class="{ 'el-overlay': mq.isMobile.value && (leftSideVisible || rightSideVisible) }"
-        >
-          <el-scrollbar
-          class="p-0 m-0"
-          wrap-class="p-0 m-0"
-          view-class="p-0 m-0"
-          >
-            <BreadcrumbBPageNavigation />
-            <br />
-            <slot />
-          </el-scrollbar>
-        </el-main>
+      <el-main class="main-content">
+        <el-scrollbar>
+          <BreadcrumbBPageNavigation />
+          <slot />
+        </el-scrollbar>
+      </el-main>
 
-        <!-- RIGHT SIDE -->
-        <el-aside
-          v-if="rightSideVisible"
-          style="border-left: 1px solid var(--el-border-color)"
-          :class="{
-            'el-aside-right': true,
-            'p-0 w-full': !mq.isMobile.value,
-            'absolute right-0 z-20 grid': mq.isMobile.value
-            }"
-        >
-          <div
-            :class="{
-              'hidden': !mq.isMobile.value,
-              'fixed bg-color left-0 opacity-70 z-30 w-screen h-screen max-w-screen': mq.isMobile.value
-            }"
-            @click.self="toggleSide('right')"
-          ></div>
-          <el-scrollbar :class="{
-            'w-full max-w-full right-0 opacity-100 justify-self-end qp-background border-0 p-2': true,
-            // 'w-80': !mq.isMobile.value,
-            'max-w-full z-30': mq.isMobile.value,
-            }">
-            <BarBQuickPanel />
-          </el-scrollbar>
-        </el-aside>
-      </el-container>
+      <el-aside v-if="rightSideVisible" class="el-side-right">
+        <el-scrollbar class="quickpanel">
+          <BarBQuickPanel />
+        </el-scrollbar>
+      </el-aside>
     </el-container>
-  </div>
+  </el-container>
 </template>
 
 <script setup lang="ts">
-import { useNotification } from '~/composables/mixins/useComponent';
+import { ref } from 'vue'
+import { useNotification } from '~/composables/mixins/useComponent'
 import type { T_DisaledFeatures, T_configuration } from '~/types/APItypes'
+
 const { notifyError } = useNotification()
 const $t = useI18n().t
 const mq = useMQ()
@@ -104,59 +44,32 @@ const leftSideIsSmall = ref<boolean>(false)
 const leftSideVisible = ref<boolean>(!mq.isMobile.value)
 const rightSideVisible = ref<boolean>(!mq.isMobile.value)
 
-
-watch(()=> mq.$mq.value, (newVal, oldVal) => {
-  settings.setIsMobile(mq.$mq.value === 'mobile')
-  leftSideVisible.value = !mq.isMobile.value
-  rightSideVisible.value = !mq.isMobile.value
+onMounted(async ()=>{
+  await checkConfig()
+  settings.initColormode()
 })
-
-watch(useRouter().currentRoute, () => {
-  if (mq.isMobile.value) { toggleSide('left') }
-})
-
-// onMounted(async ()=>{
-await checkConfig()
-settings.initColormode()
-
-leftSideIsSmall.value = false
-if (settings.menuCollapsed && !mq.isMobile.value) {
-  leftSideIsSmall.value = true
-}
-
-rightSideVisible.value = false
-if (settings.quickpanelOpened && !mq.isMobile.value) {
-  rightSideVisible.value = true
-}
-// })
 
 function setLeftCollapse (v: boolean) {
   leftSideIsSmall.value = v
   settings.setMenuCollapsed(v)
-  // (v: any) => leftSideIsSmall = v
 }
+
 function toggleSide (side: string) {
   if (side === 'left') {
-    rightSideVisible.value = false
+    if (mq.isMobile.value) { rightSideVisible.value = false }
     leftSideVisible.value = !leftSideVisible.value
   } else if (side === 'right') {
-    leftSideVisible.value = false
+    if (mq.isMobile.value) { leftSideVisible.value = false }
     rightSideVisible.value = !rightSideVisible.value
     settings.setQuickpanelOpened(rightSideVisible.value)
   }
 }
 
-// interface ApiResConf { data: Ref<T_configuration>, error: string, headers: Headers }
-// interface ApiResDF { data: Ref<Array<string>>, error: string, headers: Headers }
 async function checkConfig () {
   const result = await useApiGET<T_configuration>('/user/configuration')
   if (result.error) {
     console.error(result.error)
     notifyError({ title: $t('message.error.fetch'), message: result.error })
-    return
-  } else if (!result.data.value) {
-    console.error('No data in response')
-    notifyError({ title: $t('message.error.fetch'), message: 'No data in response' })
     return
   }
   const forbidden = await useApiGET<T_DisaledFeatures>('/opsidata/server/disabled-features')
@@ -164,123 +77,43 @@ async function checkConfig () {
     console.error(forbidden.error)
     notifyError({ title: $t('message.error.fetch'), message: forbidden.error })
     return
-  } else if (!forbidden.data.value) {
-    console.error('No data in response')
-    notifyError({ title: $t('message.error.fetch'), message: 'No data in response' })
-    return
   }
-
-  const _config = { ...result.data.value.configuration }
-  forbidden.data.value.forEach((forbElem:string) => {
+  const _config = { ...(result.data.value?.configuration ?? {}) }
+  forbidden.data.value?.forEach((forbElem:string) => {
     _config[forbElem + '.forbidden'] = true
   })
   configapp.setConfig(_config)
-
-  //   try {
-  //     const response = (await this.$axios.$get('/api/user/configuration')).configuration
-  //     const forbiddenList = (await this.$axios.get('/api/opsidata/server/disabled-features')).data
-  //     const _config = { ...response }
-  //     forbiddenList.forEach((forbElem:string) => {
-  //       _config[forbElem + '.forbidden'] = true
-  //     })
-  //     this.setConfig(_config)
-  //   } catch (error: any) {
-  //     const detailedError = ((error?.response?.data?.message) ? error.response.data.message : '') + ' ' + ((error?.response?.data?.detail) ? error.response.data.detail : '')
-  //     const ref = (this.$refs.errorAlert as any)
-  //     ref.alert(detailedError, 'danger')
-  //   }
-  // }
 }
 </script>
 
-
 <style scoped>
+.el-header {
+  background-color: var(--opsi-general-blue);
+}
 
-/* Quickpanel uses the same colors as the main content to reduce visual clutter */
-/* If users require the same colors as the navigation bar, the options can be made available later in the GUI Settings feature. */
-.qp-background {
+.main-content {
+  border-right: 1px solid var(--el-border-color);
+}
+
+.el-aside {
+  background-color: var(--opsi-general-blue);
+}
+
+.el-aside-left {
+  width: 170px;
+}
+
+.aside-left-collapsed {
+  width: 70px;
+}
+
+.el-aside-right {
+  width: 285px;
+}
+
+.quickpanel {
   background-color: var(--bg-color) !important;
   color: var(--fg-color) !important;
-}
-
-.el-header {
-  position: relative;
-  height: calc(var(--el-header-height) + 1px);
-  background-color: var(--opsi-general-blue);
-  --el-color: green;
-}
-
-.el-main {
-  --minus-width: 0px; /* will be overwritten */
-  --main-width: 100vw;
-  width: calc(var(--main-width) - var(--minus-width));
-  min-width: calc(var(--main-width) - var(--minus-width));
-  max-width: calc(var(--main-width) - var(--minus-width));
-}
-:deep(main.el-main .el-main.mycol ) {
-  max-width: 100% !important;
-  overflow: hidden;
-}
-
-/*  BOTH SIDES */
-.el-aside>.el-scrollbar {
-  --width: 100%;
-  max-width: var(--width);
-  min-width: var(--width);
-}
-.el-aside {
-  --height: 100%;
-  --minus-height: 0px;
-  min-height: calc(var(--height) - var(--minus-height));
-  height: calc(var(--height) - var(--minus-height));
-  max-height: calc(var(--height) - var(--minus-height));
-
-  background-color: var(--opsi-general-blue);
-  --width: 100%; /* fallback */
-  --minus-width: 0px; /* fallback */
-  width: calc(var(--width) - var(--minus-width));
-  min-width: calc(var(--width) - var(--minus-width));
-  max-width: calc(var(--width) - var(--minus-width));
-}
-
-
-/*  LEFT SIDE */
-.el-aside-left { /*mobile*/
-  --width: 150px;
-}
-
-
-/*  RIGHT SIDE */
-.el-aside-right {
-  --width: 285px;
-}
-
-/* BOTH SIDES */
-.is-mobile {
-  .el-aside { --minus-height: 40px; }
-  .el-aside-left { --width: 60%; }
-  .el-aside-right { --width: 70%; }
-}
-.is-not-mobile {
-  .left-opened .el-aside-left {
-    --width: 250px;
-  }
-  .left-collapsed .el-aside-left {
-    --width: 65px;
-  }
-}
-
-.el-overlay {
-  background-color: var(--el-overlay-color-lighter) !important;
-}
-
-.left-opened:not(.right-opened) .el-main { --minus-width: 250px; }
-.left-opened.right-opened .el-main { --minus-width: 545px; }
-.left-collapsed.right-opened .el-main { --minus-width: 350px; }
-.left-collapsed:not(.right-opened) .el-main { --minus-width: 70px; }
-
-/* OTHER */
-.border-r {
-  border-color: var(--el-border-color)
+  padding: 5px;
 }
 </style>
