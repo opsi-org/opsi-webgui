563ba4b5fbe85791bd49fc247425c2b45c317341
sucher <a.sucher@uib.de>
2024-06-11 15:32:17 +0000

    [fix] missing file: error page

---
+++ frontend/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout :error="true">
+    <div>
+      <h1>{{ error?.statusCode }} {{ error?.statusMessage }}</h1>
+      <NuxtLink to="/">{{ $t('message.error.back_to_home') }}</NuxtLink>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+</script>
