877a1f9774c323b9dc78e8149e7282b1c549781e
john <j.john@uib.de>
2024-07-23 14:10:09 +0000

    [chg] Add save feature for Host Attributes

--- frontend/components/form/FHostAttributes.vue
+++ frontend/components/form/FHostAttributes.vue
@@ -1,15 +1,21 @@
 <template>
   <div data-testid="FHostAttributes">
-    <el-alert v-if="!props.id" type="warning"> Please select item</el-alert>
-    <el-alert v-else-if="!hostAttributes" type="warning"> Data is not available</el-alert>
+    <el-alert v-if="!props.id" type="warning">Please select an item</el-alert>
     <el-form v-else label-width="200px" class="w-full">
       <div v-for="(value, label) in hostAttributes[0]" :key="label">
-        <el-form-item :label="$t('table.fields.' + label)">
-          <el-input v-model="hostAttributes[label.toString()]" />
+        <el-form-item :label="`${$t('table.fields.' + label)}`">
+          <el-checkbox v-if="typeof value === 'boolean'" v-model="hostAttributes[0][label]" :value="value" />
+          <el-input
+            v-else-if="isInputPasswordLabel(label)"
+            v-model="hostAttributes[0][label]"
+            :value="value"
+            show-password />
+          <el-input v-else-if="isInputDateLabel(label)" :value="useFormat().date(value)" disabled />
+          <el-input v-else v-model="hostAttributes[0][label]" :value="value" />
         </el-form-item>
       </div>
-      <el-form-item>
-        <el-button @click="resetForm()"> {{ $t('button.reset') }}</el-button>
+      <el-form-item v-if="hostAttributes[0].type !== 'OpsiDepotserver'">
+        <el-button @click="resetForm">{{ $t('button.reset') }}</el-button>
         <el-button type="success" @click="saveHostAttributes">{{ $t('button.save') }}</el-button>
       </el-form-item>
     </el-form>
@@ -17,68 +23,68 @@
 </template>
 
 <script setup lang="ts">
-import { useNotification } from '~/composables/mixins/useComponent';
-import { useSetUEFI } from '~/composables/mixins/usePost';
+import { useNotification } from '~/composables/mixins/useComponent'
+import { useSetUEFI } from '~/composables/mixins/usePost'
+import { useFormat } from '~/composables/mixins/useFormat'
 import type {T_ServerAttr, T_ClientAttr} from '~/types/APItypes'
+
 const $t = useI18n().t
-const { notifyError } = useNotification()
+
+const { notifySuccess, notifyError } = useNotification()
 const hostAttributes = ref<Array<T_ServerAttr|T_ClientAttr>>([])
+
 const props = defineProps({
   id: { type: String, default: undefined },
   type: { type: String, default: 'servers' },
   isChild: {type: Boolean, default: false }
 })
 
-onMounted(async ()=> {
-  if (props.id)
-    await fetchData(props.id)
-})
-watch(()=>props.id, ()=>{
-  if (props.id)
-    fetchData(props.id)
+watchEffect(() => {
+  if (props.id) fetchData()
 })
 
-async function fetchData(id:string) {
+const isInputPasswordLabel = (label: string) => {
+  return ['opsiHostKey', 'oneTimePassword'].includes(label);
+}
+
+const isInputDateLabel = (label: string) => {
+  return ['created', 'lastSeen'].includes(label);
+}
+
+async function fetchData() {
+  const url = props.type === 'servers' ? `/opsidata/servers?servers=[${props.id}]` : `/opsidata/hosts?hosts=${props.id}`
   try {
-    const url = props.type === 'servers' ? `/opsidata/servers?servers=[${id}]` : `/opsidata/hosts?hosts=${id}`
-    const {data, error} = await useApiGETBody<Array<T_ServerAttr|T_ClientAttr>>(url)
-      if (error) {
-      notifyError({ message: error?.response?.data?.message || $t('message.error.generic') })
-      return
-    }
-    if (data.value == undefined) {
-      notifyError({ message: $t('message.error.empty-response', { details: "HostAttributes" }) })
-      return
-    }
-    hostAttributes.value = data.value
+    const { data, error } = await useApiGETBody<Array<T_ServerAttr | T_ClientAttr>>(url);
+    if (error) throw new Error(error.response?.data?.message || $t('message.error.generic'));
+    if (!data.value) throw new Error($t('message.error.empty-response', { details: "HostAttributes" }));
+    hostAttributes.value = data.value;
   } catch (error) {
-    notifyError({ message: $t('message.error.unexpected') })
+    notifyError({ message: error || $t('message.error.unexpected') });
   }
 }
 
 async function resetForm () {
-  if (props.id)
-    await fetchData(props.id)
+  if (props.id) fetchData()
 }
 
 async function saveHostAttributes() {
-  console.log('saveHostAttributes')
-  const hostAttr = hostAttributes.value
-  delete hostAttr.type
-  delete hostAttr.created
-  delete hostAttr.lastSeen
-  delete hostAttr.systemUUID
+  const hostAttr = { ...hostAttributes.value[0] }
+
   if (props.type === 'clients') {
-    useSetUEFI.setUEFI(hostAttr.hostId, hostAttr.uefi.toString())
-    delete hostAttr.uefi
+    await useSetUEFI($t).setUEFI(hostAttr.hostId, hostAttr['uefi'].toString())
   }
-  const endPoint = props.type === 'clients' ? `/api/opsidata/clients/${hostAttr.hostId}` : `/api/opsidata/servers/${hostAttr.hostId}`
-  const {error} = await useApiPUT(endPoint, hostAttr)
-  if (error) {
-    notifyError({ message: error?.response?.data?.message || $t('message.error.generic') })
-    return
+
+  const attrsToDelete = ['type', 'created', 'lastSeen', 'systemUUID', 'uefi'];
+  attrsToDelete.forEach(attrKey => delete hostAttr[attrKey]);
+
+  try {
+    const endPoint = `/api/opsidata/${props.type}/${hostAttr.hostId}`;
+    const { error } = await useApiPUT(endPoint, hostAttr);
+    if (error) throw new Error(error.response?.data?.message || $t('message.error.generic'));
+    notifySuccess({ message: $t('message.success.save.hostattributes', { host: hostAttr.hostId }) });
+    fetchData()
+  } catch (error) {
+    notifyError({ message: error || $t('message.error.unexpected') });
   }
-  notifySuccess({ message: $t('message.success.save.hostattributes', { host: hostAttr.hostId }) })
-  await fetchData(props.id)
 }
 </script>
