f41956075b57566de7b0d3686df91a2b85c27759
john <j.john@uib.de>
2024-08-23 13:00:57 +0000

    [fix] Format vue files

--- opsiweb/uib-components/components/grid/GProductProperties.vue
+++ opsiweb/uib-components/components/grid/GProductProperties.vue
@@ -2,32 +2,21 @@
   <div data-testid="GProductProperties">
     <OverlayOLoading :is-loading="isLoading" />
     <div v-if="!errorText && selectionClients.length <= 0">
-      <AlertAAlertLocal
-        show
-        variant="warning"
-        class="noClientsSelectedShowDepot"
-      >
-        {{ $t("message.warning.noClientsSelectedShowDepot") }}
+      <AlertAAlertLocal show variant="warning" class="noClientsSelectedShowDepot">
+        {{ $t('message.warning.noClientsSelectedShowDepot') }}
       </AlertAAlertLocal>
     </div>
     <div v-else-if="!errorText && $mq == 'mobile'">
       <AlertAAlertLocal show variant="primary">
-        {{ $t("table.fields.clientsIds") + t_fixed("keep-english.colon") }}
+        {{ $t('table.fields.clientsIds') + t_fixed('keep-english.colon') }}
         {{ selectionClients.length }}
       </AlertAAlertLocal>
     </div>
-    <div
-      v-if="
-        !errorText &&
-        Object.values(properties.productVersions).filter((n) => n).length !==
-          selectionDepots.length
-      "
-    >
+    <div v-if="!errorText && Object.values(properties.productVersions).filter((n) => n).length !== selectionDepots.length">
       <AlertAAlertLocal show variant="warning" class="notOnEachDepot">
         {{
-          $t("message.warning.notOnEachDepot", {
-            count: Object.values(properties.productVersions).filter((n) => n)
-              .length,
+          $t('message.warning.notOnEachDepot', {
+            count: Object.values(properties.productVersions).filter((n) => n).length,
             countall: selectionDepots.length,
           })
         }}
@@ -38,14 +27,11 @@
         !errorText &&
         Object.values(properties.productVersions)
           .filter((n) => n)
-          .some(
-            (v) =>
-              v != Object.values(properties.productVersions).filter((n) => n)[0]
-          )
+          .some((v) => v != Object.values(properties.productVersions).filter((n) => n)[0])
       "
     >
       <AlertAAlertLocal show variant="warning">
-        {{ $t("message.warning.differentProductVersions") }}
+        {{ $t('message.warning.differentProductVersions') }}
       </AlertAAlertLocal>
     </div>
     <p v-if="errorText">
@@ -55,46 +41,26 @@
       <GridGFormItem variant="longlabel" :new-row-for-value="$mq !== 'desktop'">
         <template #label>
           <div class="d-inline-flex">
-            <GridCellGCProductPropertyId
-              :row="item"
-              :product-versions="properties.productVersions"
-            />
-            <IconIIcon
-              :id="`property_${item.propertyId}`"
-              class="mt-1 ml-1"
-              :icon="icon.info"
-            />
+            <GridCellGCProductPropertyId :row="item" :product-versions="properties.productVersions" />
+            <IconIIcon :id="`property_${item.propertyId}`" class="mt-1 ml-1" :icon="icon.info" />
           </div>
           <b-tooltip :target="`property_${item.propertyId}`" triggers="hover">
-            <b-container
-              :class="`TProductProperties_row_details TProductProperties_row_details_${item.propertyId}`"
-              class="text-left"
-            >
+            <b-container :class="`TProductProperties_row_details TProductProperties_row_details_${item.propertyId}`" class="text-left">
               <b-form>
-                <b-form-group
-                  :label="$t('table.details.productproperty.defaults')"
-                >
+                <b-form-group :label="$t('table.details.productproperty.defaults')">
                   <b v-if="item.default !== $t('values.mixed')">
-                    {{
-                      t_fixed("keep-english.[content]").replace(
-                        "content",
-                        item.default
-                      )
-                    }}
+                    {{ t_fixed('keep-english.[content]').replace('content', item.default) }}
                   </b>
                   <div v-else>
                     <p v-for="(v, k) in item.defaultDetails" :key="k">
-                      {{ k + t_fixed("keep-english.colon") }} <b>{{ v }}</b>
+                      {{ k + t_fixed('keep-english.colon') }} <b>{{ v }}</b>
                     </p>
                   </div>
                 </b-form-group>
 
-                <b-form-group
-                  v-if="item.anyDepotDifferentFromDefault"
-                  :label="$t('table.details.productproperty.server')"
-                >
+                <b-form-group v-if="item.anyDepotDifferentFromDefault" :label="$t('table.details.productproperty.server')">
                   <p v-for="(v, k) in item.depots" :key="k">
-                    {{ k + t_fixed("keep-english.colon") }} <b>{{ v }}</b>
+                    {{ k + t_fixed('keep-english.colon') }} <b>{{ v }}</b>
                   </p>
                 </b-form-group>
 
@@ -104,7 +70,7 @@
                   </b>
                   <div v-else>
                     <p v-for="(v, k) in item.descriptionDetails" :key="k">
-                      {{ k + t_fixed("keep-english.colon") }} <b>{{ v }}</b>
+                      {{ k + t_fixed('keep-english.colon') }} <b>{{ v }}</b>
                     </p>
                   </div>
                 </b-form-group>
@@ -126,125 +92,108 @@
 </template>
 
 <script lang="ts">
-import { Component, namespace, Prop, Vue } from "nuxt-property-decorator";
-import { IProp, IProperty } from "../../.utils/types/ttable";
-import { IObjectString2Any } from "../../.utils/types/tgeneral";
-import { ChangeObj } from "../../.utils/types/tchanges";
-import { arrayEqual } from "../../.utils/utils/scompares";
-import { Icons } from "../../mixins/icons";
-import { Client } from "../../mixins/get";
-import { SaveProductProperties } from "../../mixins/save";
-import { Strings } from "../../mixins/strings";
-const selections = namespace("selections");
-const settings = namespace("settings");
-const changes = namespace("changes");
+  import { Component, namespace, Prop, Vue } from 'nuxt-property-decorator'
+  import { IProp, IProperty } from '../../.utils/types/ttable'
+  import { IObjectString2Any } from '../../.utils/types/tgeneral'
+  import { ChangeObj } from '../../.utils/types/tchanges'
+  import { arrayEqual } from '../../.utils/utils/scompares'
+  import { Icons } from '../../mixins/icons'
+  import { Client } from '../../mixins/get'
+  import { SaveProductProperties } from '../../mixins/save'
+  import { Strings } from '../../mixins/strings'
+  const selections = namespace('selections')
+  const settings = namespace('settings')
+  const changes = namespace('changes')
 
-@Component({ mixins: [Icons, Strings, SaveProductProperties, Client] })
-export default class GProductProperties extends Vue {
-  @Prop({}) id!: string;
-  @Prop({ default: "" }) errorText!: string;
-  @Prop({}) properties!: IProp;
-  saveProdProperties: any;
-  getClientToDepot: any;
-  icon: any;
-  $axios: any;
-  $nuxt: any;
-  $mq: any;
-  $t: any;
-  t_fixed: any;
-  result: Object = {};
-  isLoading: boolean = false;
-  fetchedDataClients2Depots: object = {};
+  @Component({ mixins: [Icons, Strings, SaveProductProperties, Client] })
+  export default class GProductProperties extends Vue {
+    @Prop({}) id!: string
+    @Prop({ default: '' }) errorText!: string
+    @Prop({}) properties!: IProp
+    saveProdProperties: any
+    getClientToDepot: any
+    icon: any
+    $axios: any
+    $nuxt: any
+    $mq: any
+    $t: any
+    t_fixed: any
+    result: Object = {}
+    isLoading: boolean = false
+    fetchedDataClients2Depots: object = {}
 
-  @selections.Getter public selectionDepots!: Array<string>;
-  @selections.Getter public selectionClients!: Array<string>;
-  @changes.Getter public changesProducts!: Array<ChangeObj>;
-  @changes.Mutation public pushToChangesProducts!: (o: object) => void;
-  @changes.Mutation public delWithIndexChangesProducts!: (i: number) => void;
-  @settings.Getter public quicksave!: boolean;
+    @selections.Getter public selectionDepots!: Array<string>
+    @selections.Getter public selectionClients!: Array<string>
+    @changes.Getter public changesProducts!: Array<ChangeObj>
+    @changes.Mutation public pushToChangesProducts!: (o: object) => void
+    @changes.Mutation public delWithIndexChangesProducts!: (i: number) => void
+    @settings.Getter public quicksave!: boolean
 
-  async fetch() {
-    if (this.selectionClients.length > 0) {
-      await this.getClientToDepot(this.selectionClients);
-    }
-  }
-
-  async handleChange(
-    propertyId: string,
-    values: Array<string | boolean>,
-    orgValues: Array<
-      string | boolean
-    > /* , type:'UnicodeProductProperty'|'BoolProductProperty' */
-  ) {
-    if (!this.quicksave) {
+    async fetch() {
       if (this.selectionClients.length > 0) {
-        this.handleTrackingChanges(
-          this.selectionClients,
-          "clientId",
-          propertyId,
-          values,
-          orgValues
-        );
-      } else {
-        this.handleTrackingChanges(
-          this.selectionDepots,
-          "depotId",
-          propertyId,
-          values,
-          orgValues
-        );
+        await this.getClientToDepot(this.selectionClients)
       }
-      return;
     }
-    this.isLoading = true;
-    if (!arrayEqual(values, orgValues)) {
-      const propObj: any = {};
-      propObj[propertyId] = values;
-      const data: IObjectString2Any = { properties: propObj };
-      if (this.selectionClients.length > 0) {
-        data.clientIds = this.selectionClients;
-      } else {
-        data.depotIds = this.selectionDepots;
-      }
-      await this.saveProdProperties(this.id, data, null, true);
-    }
-    this.isLoading = false;
-  }
 
-  handleTrackingChanges(
-    hosts: Array<string>,
-    key: string,
-    propertyId: string,
-    values: Array<string | boolean>,
-    orgValues: Array<string | boolean>
-  ) {
-    for (const h in hosts) {
-      const changeObject: Object = {
-        user: localStorage.getItem("username"),
-        [key]: hosts[h],
-        productId: this.id,
-        property: propertyId,
-        propertyValue: values,
-      };
-      const objIndex = this.changesProducts.findIndex(
-        (item) =>
-          item[key] === hosts[h] &&
-          item.productId === this.id &&
-          item.property === propertyId
-      );
-      if (objIndex > -1) {
-        this.delWithIndexChangesProducts(objIndex);
+    async handleChange(
+      propertyId: string,
+      values: Array<string | boolean>,
+      orgValues: Array<string | boolean> /* , type:'UnicodeProductProperty'|'BoolProductProperty' */
+    ) {
+      if (!this.quicksave) {
+        if (this.selectionClients.length > 0) {
+          this.handleTrackingChanges(this.selectionClients, 'clientId', propertyId, values, orgValues)
+        } else {
+          this.handleTrackingChanges(this.selectionDepots, 'depotId', propertyId, values, orgValues)
+        }
+        return
       }
+      this.isLoading = true
       if (!arrayEqual(values, orgValues)) {
-        this.pushToChangesProducts(changeObject);
+        const propObj: any = {}
+        propObj[propertyId] = values
+        const data: IObjectString2Any = { properties: propObj }
+        if (this.selectionClients.length > 0) {
+          data.clientIds = this.selectionClients
+        } else {
+          data.depotIds = this.selectionDepots
+        }
+        await this.saveProdProperties(this.id, data, null, true)
+      }
+      this.isLoading = false
+    }
+
+    handleTrackingChanges(
+      hosts: Array<string>,
+      key: string,
+      propertyId: string,
+      values: Array<string | boolean>,
+      orgValues: Array<string | boolean>
+    ) {
+      for (const h in hosts) {
+        const changeObject: Object = {
+          user: localStorage.getItem('username'),
+          [key]: hosts[h],
+          productId: this.id,
+          property: propertyId,
+          propertyValue: values,
+        }
+        const objIndex = this.changesProducts.findIndex(
+          (item) => item[key] === hosts[h] && item.productId === this.id && item.property === propertyId
+        )
+        if (objIndex > -1) {
+          this.delWithIndexChangesProducts(objIndex)
+        }
+        if (!arrayEqual(values, orgValues)) {
+          this.pushToChangesProducts(changeObject)
+        }
+      }
+    }
+
+    updateNewPropertyValuesRow(rowItem: IProperty) {
+      if (rowItem.newValue && rowItem.newValues) {
+        rowItem.newValues.push(rowItem.newValue)
       }
     }
   }
-
-  updateNewPropertyValuesRow(rowItem: IProperty) {
-    if (rowItem.newValue && rowItem.newValues) {
-      rowItem.newValues.push(rowItem.newValue);
-    }
-  }
-}
 </script>
--- opsiweb/uib-components/components/grid/cell/GCProductPropertyId.vue
+++ opsiweb/uib-components/components/grid/cell/GCProductPropertyId.vue
@@ -1,52 +1,42 @@
 <template>
   <div data-testid="GCProductPropertyId">
-    <i
-      v-if="row.anyDepotDifferentFromDefault"
-      :id="`TProductProperties_PropertyId_hover_${row.propertyId}`"
-    >
+    <i v-if="row.anyDepotDifferentFromDefault" :id="`TProductProperties_PropertyId_hover_${row.propertyId}`">
       <b v-if="row.anyClientDifferentFromDepot">{{ row.propertyId }}</b>
-      {{ row.anyClientDifferentFromDepot ? "" : row.propertyId }}
+      {{ row.anyClientDifferentFromDepot ? '' : row.propertyId }}
     </i>
-    <div
-      v-else
-      :id="`TProductProperties_PropertyId_hover_${row.propertyId}`"
-      class="TProductProperties_Property_p"
-    >
+    <div v-else :id="`TProductProperties_PropertyId_hover_${row.propertyId}`" class="TProductProperties_Property_p">
       <b v-if="row.anyClientDifferentFromDepot">{{ row.propertyId }}</b>
-      {{ row.anyClientDifferentFromDepot ? "" : row.propertyId }}
+      {{ row.anyClientDifferentFromDepot ? '' : row.propertyId }}
     </div>
     <IconIDetails
       v-if="
-        Object.values(productVersions).filter((n) => n).length ==
-          selectionDepots.length &&
+        Object.values(productVersions).filter((n) => n).length == selectionDepots.length &&
         Object.keys(row.depots).length != selectionDepots.length
       "
       :id="`btn_tt_${row.propertyId}`"
-      :title="
-        $t('label.ppId-onlyOnDepots', { depots: Object.keys(row.depots) })
-      "
+      :title="$t('label.ppId-onlyOnDepots', { depots: Object.keys(row.depots) })"
       content="ppid-not-exists-on-depot"
     />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, namespace, Prop, Vue } from "nuxt-property-decorator";
-import { IObjectString2StringOrUndefined } from "../../../.utils/types/tgeneral";
-const selections = namespace("selections");
+  import { Component, namespace, Prop, Vue } from 'nuxt-property-decorator'
+  import { IObjectString2StringOrUndefined } from '../../../.utils/types/tgeneral'
+  const selections = namespace('selections')
 
-@Component
-export default class GCProductPropertyId extends Vue {
-  @Prop({}) row!: any;
-  @Prop({}) productVersions!: IObjectString2StringOrUndefined;
+  @Component
+  export default class GCProductPropertyId extends Vue {
+    @Prop({}) row!: any
+    @Prop({}) productVersions!: IObjectString2StringOrUndefined
 
-  @selections.Getter public selectionDepots!: Array<string>;
-  @selections.Getter public selectionClients!: Array<string>;
-}
+    @selections.Getter public selectionDepots!: Array<string>
+    @selections.Getter public selectionClients!: Array<string>
+  }
 </script>
 
 <style>
-.TProductProperties_Property_p {
-  margin-block-end: 0px;
-}
+  .TProductProperties_Property_p {
+    margin-block-end: 0px;
+  }
 </style>
