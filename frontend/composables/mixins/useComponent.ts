64ee4bb6a84d8522e165addeab3ec7bffc4a3df3
john <j.john@uib.de>
2024-08-30 16:15:48 +0000

    [chg] Display 'Clear All Notifications' button as a footer when there are more than three notifications.

--- frontend/composables/mixins/useComponent.ts
+++ frontend/composables/mixins/useComponent.ts
@@ -1,63 +1,101 @@
 import { ElNotification } from 'element-plus'
 import { _getI18nInComposable } from './helper-i18n'
-import { h } from 'vue';
+import { h } from 'vue'
 
 interface NotificationOptions {
-  title?: string;
-  message?: any;
-  showClose?: boolean;
-  duration?: number;
-  onClose?: () => void;
-  button?: { label: string; onClick: () => void; };
+  title?: string
+  message?: any
+  showClose?: boolean
+  duration?: number
+  onClose?: () => void
+  button?: { label: string; onClick: () => void }
 }
 
 export function useNotification() {
-  const notification = ref();
+  const notifications = ref<any[]>([])
+  const clearAllNotification = ref<any>(null)
+
+  const formatMessage = (message: any) => {
+    if (typeof message === 'object' && message !== null) {
+      return h('pre', {
+        innerHTML: Object.entries(message)
+          .map(([key, value]) => {
+            if (typeof value === 'object' && value !== null) {
+              value = Object.entries(value)
+                .map(([k, v]) => (v !== null ? `${k}: ${v}` : ''))
+                .filter(Boolean)
+                .join('\n')
+            }
+            return `<b>${key}</b>:\n${value}\n`
+          })
+          .join('\n'),
+        style: { 'white-space': 'pre-wrap' },
+      })
+    }
+    return message
+  }
 
   const createNotification = (type: 'success' | 'error' | 'warning' | 'info') => {
     return ({ title, message = '', showClose = true, duration, onClose, button }: NotificationOptions) => {
-        let customMessage = message && typeof message === 'object' && message !== null ?
-          h('pre', {
-            innerHTML:
-              Object.entries(message)
-                .map(([key, value]) => {
-                  if (typeof value === 'object' && value !== null) {
-                    value = Object.entries(value)
-                      .map(([k, v]) => v !== null ? `${k}: ${v}` : '')
-                      .filter(Boolean)
-                      .join('\n');
-                  }
-                  return `<b>${key}</b>:\n${value}\n`;
-                })
-                .join('\n'),
-            style: { 'white-space': 'pre-wrap' } })
-          : message
+      const customMessage = button
+        ? h('div', {}, [
+            formatMessage(message),
+            h(
+              'button',
+              {
+                onClick: button.onClick,
+                style: {
+                  display: 'block',
+                  width: '100%',
+                  border: '1px solid #000',
+                  cursor: 'pointer',
+                },
+              },
+              button.label
+            ),
+          ])
+        : formatMessage(message)
 
+      const autoHideDuration = type === 'success' ? duration ?? 8000 : 0
 
-      if (button) {
-        customMessage = h('div', {}, [
-          customMessage,
-          h('button', {
-            onClick: button.onClick,
-            style: {
-              display: 'block',
-              width: '100%',
-              border: '1px solid #000',
-              cursor: 'pointer'
-            }
-          }, button.label)
-        ])
-      }
-
-      const autoHideDuration = type === 'success' ? (duration ?? 8000) : 0 // 0 means no Auto Hide
-
-      notification.value = ElNotification[type]({
+      const notificationInstance = ElNotification[type]({
         title,
         message: customMessage,
         showClose,
         duration: autoHideDuration,
-        onClose,
+        onClose: () => {
+          notifications.value = notifications.value.filter((n) => n !== notificationInstance)
+          if (onClose) onClose()
+          if (notifications.value.length <= 3 && clearAllNotification.value) {
+            clearAllNotification.value.close()
+            clearAllNotification.value = null
+          }
+        },
       })
+
+      notifications.value.push(notificationInstance)
+
+      if (notifications.value.length > 3 && !clearAllNotification.value) {
+        clearAllNotification.value = ElNotification({
+          message: h(
+            'button',
+            {
+              onClick: () => {
+                notifications.value.forEach((n) => n.close())
+                notifications.value = []
+                if (clearAllNotification.value) {
+                  clearAllNotification.value.close()
+                  clearAllNotification.value = null
+                }
+              },
+            },
+            'Clear All Notifications'
+          ),
+          duration: 0,
+          showClose: true,
+          position: 'bottom-right',
+        })
+      }
     }
   }
 
@@ -69,24 +107,22 @@ export function useNotification() {
   }
 }
 
-
 export const useHoverDropdown = () => {
-  function onOver (ref: any) {
+  function onOver(ref: any) {
     if (ref) {
       ref.visible = true
     }
   }
-  function onLeave (ref: any) {
+  function onLeave(ref: any) {
     if (ref) {
       ref.visible = false
     }
   }
-  return {onOver, onLeave}
+  return { onOver, onLeave }
 }
 
-
 import debounce from 'lodash/debounce'
-export const useScrollListener = (refComponent: Ref<HTMLElement|undefined>, handleScroll = (...args: any[]) => {}) => {
+export const useScrollListener = (refComponent: Ref<HTMLElement | undefined>, handleScroll = (...args: any[]) => {}) => {
   // const handleDebouncedScroll = debounce(handleScroll, 100)
   let resizeObserver: any = null
   onMounted(() => {
@@ -105,8 +141,7 @@ export const useScrollListener = (refComponent: Ref<HTMLElement|undefined>, hand
     const h = refComponent.value?.clientHeight + 'px'
   }
   onUnmounted(() => {
-    if (resizeObserver !== null)
-    resizeObserver.unobserve(refComponent)
+    if (resizeObserver !== null) resizeObserver.unobserve(refComponent)
     // if (refComponent.value == undefined) {
     //   console.error('unmount. component for scroll listener undefined')
     //   return
@@ -118,11 +153,10 @@ export const useScrollListener = (refComponent: Ref<HTMLElement|undefined>, hand
   // return {handleDebouncedScroll}
 }
 
-
 export const useSynchronization = () => {
   // TODO check if useCookies is needed instead of useCookie
   // setCookie: any
-  function syncSort (fromSort: any, toSort: any, emitToSort: any, id: any) {
+  function syncSort(fromSort: any, toSort: any, emitToSort: any, id: any) {
     const sortingCookie = useCookie('sorting_' + id)
 
     if (fromSort.filterQuery && toSort.filterQuery !== fromSort.filterQuery) {
@@ -138,8 +172,9 @@ export const useSynchronization = () => {
       sortingCookie.value = JSON.stringify({ sortBy: toSort.sortBy, sortDesc: toSort.sortDesc })
       // this.setCookie('sorting_' + id, JSON.stringify({ sortBy: toSort.sortBy, sortDesc: toSort.sortDesc }), { expires: 365 })
     }
-    if (emitToSort) { emitToSort('update:sort', toSort) }
+    if (emitToSort) {
+      emitToSort('update:sort', toSort)
+    }
     // if (emitToSort) { this.$emit('update:sort', toSort) }
   }
 }
-
