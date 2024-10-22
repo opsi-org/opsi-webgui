572cb4500fadae0c47a53d75de0a69b22151ef51
sucher <a.sucher@uib.de>
2024-10-22 10:16:21 +0000

    [fix] styling of version cell content

--- frontend/components/icon/IDetails.vue
+++ frontend/components/icon/IDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div data-testid="IDetails" :class="'IDetails ' + content">
+  <div data-testid="IDetails" :class="'IDetails ' + content" class="inline">
     <el-tag :type="props.variant" size="small">
     <!-- <b-badge
       v-bind="$props"
@@ -9,24 +9,25 @@
     > -->
       <span v-if="props.content=='depot-unequal'" class="inline">
         <el-text>{{ props.text ? props.text: t_fixed('unequal') }}</el-text>
-        <IconIIcon :icon="icons.depots" size="small"/>
+        <IconIIcon :icon="icons.depots" size="small"  class="inline"/>
       </span>
       <span v-else-if="props.content=='depot-wo-prod'" class="h6">
-        <el-text>{{ $t('notOrigin') }} </el-text>
-        <IconIIcon :icon="icons.depots" />
+        <el-text>{{ t_fixed('notOrigin') }} </el-text>
+        <IconIIcon :icon="icons.depots" class="inline"/>
       </span>
       <span v-else-if="props.content=='client-outdated'" class="h6">
         <el-text>{{ props.text ? props.text: t_fixed('unequal') }}</el-text>
         <!-- <span> {{ props.text ? props.text: t_fixed('unequal') }}</span> -->
-        <IconIIcon :icon="icons.client" />
+        <IconIIcon :icon="icons.client"  class="inline"/>
       </span>
       <span v-else-if="props.content=='ppv-client-different'" class="h6">
         <el-text>{{ props.text ? props.text: t_fixed('unequal') }}</el-text>
         <!-- <span> {{ props.text ? props.text: t_fixed('unequal') }}</span>  -->
-        <IconIIcon :icon="icons.client" />
+        <IconIIcon :icon="icons.client" class="inline" />
       </span>
       <span v-else-if="props.content=='ppid-not-exists-on-depot'" class="h6">
-        <el-text>{{ t_fixed('unequal') }} </el-text><IconIIcon :icon="icons.depots" />
+        <el-text>{{ t_fixed('unequal') }} </el-text>
+        <IconIIcon :icon="icons.depots"  class="inline"/>
       </span>
       <el-text v-else class="h6">{{ props.content==='unequal'? t_fixed('unequal'):props.content }} </el-text>
     <!-- </b-badge> -->
