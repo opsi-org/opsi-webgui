8b9ca7042c0db4216a2baa6125616aaf8e9b4a01
sucher <a.sucher@uib.de>
2024-11-06 15:10:50 +0000

    [fix] styling

--- frontend/components/form/FDiagnostics.vue
+++ frontend/components/form/FDiagnostics.vue
@@ -1,16 +1,22 @@
 <template>
-  <el-form label-width="250px" class="diagnostics-form" v-if="Object.keys(data).length > 0">
+  <!-- TODO: mobile first (use row layout not columns) -->
+  <el-form
+  :label-width="($mq !== 'mobile') ? '300px': '250px'"
+    class="diagnostics-form" v-if="Object.keys(data).length > 0"
+  >
     <div v-for="values, category in data" :key="category">
       <template v-if="values && Object.keys(values).length !== 0">
         <el-text tag="b"> {{ category }} </el-text><br>
         <el-form-item v-for="v, k in values" :label="k.toString()" :key="k">
             <template v-if="typeof v == 'object'">
               <div class="scrollValue">
-                <pre>{{ JSON.stringify(v, null, 2) }}</pre>
+                <pre class="min-w-[250px]">{{ JSON.stringify(v, null, 2) }}</pre>
+                <br>
               </div>
             </template>
             <template v-else>
               {{ v }}
+              <br>
             </template>
         </el-form-item>
       </template>
@@ -19,6 +25,7 @@
 </template>
 
 <script setup lang="ts">
+const $mq = useMQ().$mq
 const _props = defineProps({
   data: { type: Object, required: true }
 })
