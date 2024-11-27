23b14335bbbe556e46fc2109bbc2e710c79c5e60
sucher <a.sucher@uib.de>
2024-11-27 13:41:40 +0000

    [new] add width and icons to table

--- frontend/components/table/TTable.vue
+++ frontend/components/table/TTable.vue
@@ -8,8 +8,16 @@
             :prop="column.key"
             :label="column.title"
             :type="column.type"
+            :width="column.width || ''"
             :sortable="column.sortable"
           >
+            <template #header>
+              <div class="table-header">
+                <el-text v-if="column.icon">
+                  <IconIIcon :icon="icons[column.icon]" /> </el-text>
+                <el-text v-else>{{ column.title }}</el-text>
+              </div>
+              </template>
             <template #default="scope" v-if="column.key === 'actions'">
               <div v-contextmenu="(event: MouseEvent) => $emit('showContextMenu',{event, row:scope.row})">
                 <el-tooltip :content="$t('title.config')" placement="top">
--- frontend/components/view/VClients.vue
+++ frontend/components/view/VClients.vue
@@ -158,14 +158,15 @@ const tableColumn = ref([
   {title: 'notes', key: 'notes', sortable: true, visible: false},
   {title: 'lastSeen', key: 'lastSeen', sortable: true, visible: false},
   {title: 'uefi', key: 'uefi', sortable: true, visible: false},
-  {title: 'version_outdated_localboot', key: 'version_outdated', sortable: true, visible: true, alwaysVisible: true},
-  {title: 'version_outdated_netboot', key: 'version_outdated_netboot', sortable: true, visible: true, alwaysVisible: true},
-  {title: 'installationStatus_unknown', key: 'installationStatus_unknown', sortable: true, visible: true, alwaysVisible: true},
-  {title: 'installationStatus_installed', key: 'installationStatus_installed', sortable: true, visible: true, alwaysVisible: true},
-  {title: 'actionResult_failed', key: 'actionResult_failed', sortable: true, visible: true, alwaysVisible: true},
-  {title: 'actionResult_successful', key: 'actionResult_successful', sortable: true, visible: true, alwaysVisible: true},
-  {title: 'reachable', key: 'reachable', sortable: false, visible: true, alwaysVisible: true},
-  {title: 'actions', key: 'actions', sortable: false, visible: true, alwaysVisible: true},
+
+  {title: 'version_outdated_localboot', key: 'version_outdated', sortable: true, visible: true, alwaysVisible: true, width:"50px", icon:"productsOutdated"},
+  {title: 'version_outdated_netboot', key: 'version_outdated_netboot', sortable: true, visible: true, alwaysVisible: true, width:"50px", icon:"productsOutdated"},
+  {title: 'installationStatus_unknown', key: 'installationStatus_unknown', sortable: true, visible: true, alwaysVisible: true, width:"50px", icon:"productInstallationStatusUnknown"},
+  {title: 'installationStatus_installed', key: 'installationStatus_installed', sortable: true, visible: true, alwaysVisible: true, width:"50px", icon:"product"},
+  {title: 'actionResult_failed', key: 'actionResult_failed', sortable: true, visible: true, alwaysVisible: true, width:"50px", icon:"productsFailedActionResult"},
+  {title: 'actionResult_successful', key: 'actionResult_successful', sortable: true, visible: true, alwaysVisible: true, width:"50px", icon:"productActionResultSuccessful"},
+  {title: 'reachable', key: 'reachable', sortable: false, visible: true, alwaysVisible: true, width:"50px"},
+  {title: 'actions', key: 'actions', sortable: false, visible: true, alwaysVisible: true, width:"150px"},
 ])
 
 
