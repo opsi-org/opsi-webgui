03e8efd57e30e736c137a2c6da6e0b8135ac5dcf
sucher <a.sucher@uib.de>
2024-12-03 18:00:02 +0000

    [fix] lint error for self-closing tags

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,34 +2,37 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  { // https://github.com/nuxt/eslint/discussions/413
+  {
+    // https://github.com/nuxt/eslint/discussions/413
     ignores: [
-    "node_modules/",
-    "dist/*",
-    "./package-dry.json",
-    "./package.json",
-    "package.json",
-    "./locale/*.json",
-    ".vscode/*.json",
-    "**/*/tsconfig.json",
-    "vuex-persist.js",
-    "tests/",
-    "pages/*.test.unit.js"
-  ],
+      'node_modules/',
+      'dist/*',
+      './package-dry.json',
+      './package.json',
+      'package.json',
+      './locale/*.json',
+      '.vscode/*.json',
+      '**/*/tsconfig.json',
+      'vuex-persist.js',
+      'tests/',
+      'pages/*.test.unit.js',
+    ],
   },
   {
-  // Your custom configs here
-  // https://eslint.nuxt.com/packages/module
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "vue/attributes-order": "off",
+    // Your custom configs here
+    // https://eslint.nuxt.com/packages/module
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'vue/attributes-order': 'off',
+      'vue/html-self-closing': 'off',
 
-    // console error and warn are allowed
-    "no-console": ["error", {
-        "allow": [
-            "warn",
-            "error"
-        ]
-    }],
+      // console error and warn are allowed
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+    },
   },
-})
+)
