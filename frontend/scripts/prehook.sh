32927f0fabc42e0df0b846bd93968dd8515f6d9e
sucher <a.sucher@uib.de>
2024-12-03 18:10:52 +0000

    [chg] prepush hook

--- frontend/scripts/prehook.sh
+++ frontend/scripts/prehook.sh
@@ -1,14 +1,15 @@
 cd /workspace/frontend/
 echo "====================================================== Check test files"
 # file=".tmp_prepush_result_changed_files.log"
-cd /workspace/frontend
 # bash ./scripts/playwright_coverage_by_files.sh
 # rm $file
+
 npm run lint
+
 npm run tsc
 
 echo "======================================================"
+cd -
 echo ""
-cd ..
 echo "exitcode $exit_code"
 exit $exit_code
