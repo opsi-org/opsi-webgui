4d12ce6fc4ac489a4d9038889bee430a3243f3f3
john <j.john@uib.de>
2024-12-10 14:50:05 +0000

    [chg] Add translations and tooltips

--- frontend/components/dropdown/DDClientActions.vue
+++ frontend/components/dropdown/DDClientActions.vue
@@ -1,17 +1,19 @@
 <template>
   <el-dropdown trigger="click">
-    <el-button class="ml-1 mt-1" :link="props.link" :disabled="isLoading">
-      <IconIIcon
-        :icon="getIcon(props.icon)"
-        :title="$t('button.tablerow.moreoptions')"
-      />
-      <IconILoading
-        v-if="isLoading"
-        class="ml-1"
-        small
-        :title="$t('message.loading')"
-      />
-    </el-button>
+    <el-tooltip :content="$t('label.clientaction')" placement="top">
+      <el-button class="ml-1 mt-1" :link="props.link" :disabled="isLoading">
+        <IconIIcon
+          :icon="getIcon(props.icon)"
+          :title="$t('button.tablerow.moreoptions')"
+        />
+        <IconILoading
+          v-if="isLoading"
+          class="ml-1"
+          small
+          :title="$t('message.loading')"
+        />
+      </el-button>
+    </el-tooltip>
     <template #dropdown>
       <el-dropdown-menu>
         <div
