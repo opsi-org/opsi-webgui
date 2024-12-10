6b9097fa9ad2e1dab25e9c85ceee726d9342dd41
sucher <a.sucher@uib.de>
2024-12-10 15:03:31 +0000

    [fix] product version tooltip

--- frontend/components/tooltip/TTooltip.vue
+++ frontend/components/tooltip/TTooltip.vue
@@ -1,15 +1,42 @@
 <template>
-  <div data-testid="TTooltip" @mouseenter="op?.toggle" @mouseleave="op?.toggle">
+  <div
+    data-testid="TTooltip"
+    @mouseenter="
+      (ev: any) => (props.method == 'hover' ? popoverRef?.show(ev) : null)
+    "
+    @click="
+      (ev: any) => (props.method == 'click' ? popoverRef?.toggle(ev) : null)
+    "
+    @mouseleave="
+      (ev: any) =>
+        props.method == 'hover' &&
+        /* overwise the popover will be closed if mouse is over the popover (leaves this div) */
+        ev.relatedTarget.className !== 'p-popover-content'
+          ? popoverRef?.hide()
+          : null
+    "
+  >
     <slot name="default" />
 
-    <PPopover v-if="!disabled" ref="op"> <slot name="tooltip" /> </PPopover>
+    <PPopover
+      v-if="!disabled"
+      ref="popoverRef"
+      id="popover"
+      class="mt-[-1px]"
+      @mouseleave="
+        (ev: any) => (props.method == 'hover' ? popoverRef?.hide() : null)
+      "
+    >
+      <slot name="tooltip" />
+    </PPopover>
   </div>
 </template>
 
 <script lang="ts" setup>
-  const op = useTemplateRef<any>('op')
-
-  const _props = defineProps({
+  const popoverRef = useTemplateRef<any>('popoverRef')
+  type TMethod = 'hover' | 'click'
+  const props = defineProps({
     disabled: { type: Boolean, default: false },
+    method: { type: String as PropType<TMethod>, default: 'hover' },
   })
 </script>
