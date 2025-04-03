649590b507c0c8141e81565ca34170ee048ccdd6
john <j.john@uib.de>
2025-04-03 14:32:16 +0000

    [chg] Add browser clean state before each tests

--- frontend/tests/shared/utils.ts
+++ frontend/tests/shared/utils.ts
@@ -6,68 +6,102 @@ All rights reserved.
 License: AGPL-3.0
 */
 
-import type { BrowserContext, Page, Route } from '@playwright/test'
+import type { Browser, BrowserContext, Page, Route } from '@playwright/test'
 import { defaultResponseHeaders } from '../shared/constants'
-import { fetchMockData, mockData, serverId, userId, serverObjectList, clientObjectList } from './mocks'
+import {
+  fetchMockData,
+  mockData,
+  serverId,
+  userId,
+  serverObjectList,
+  clientObjectList,
+} from './mocks'
 
-export const getFreshBrowserState = async (page: Page, context: BrowserContext) => {
-  await context.clearCookies()
-  await context.clearPermissions()
-  await page.goto('about:blank')
+export const getFreshBrowserState = async (
+  browser: Browser,
+): Promise<{ context: BrowserContext; page: Page }> => {
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  return { context, page }
 }
 
-export const setupMockRoutes = async (page: Page, context: BrowserContext) => {
-  await getFreshBrowserState(page, context)
+export const addMockRoute = async (
+  page: Page,
+  url: string,
+  response: any,
+  headers: Record<string, string> = defaultResponseHeaders,
+) => {
+  await page.route(url, (route: Route) => {
+    route.fulfill({
+      status: 200,
+      headers,
+      contentType: 'application/json',
+      body: JSON.stringify(response),
+    })
+  })
+}
+
+export const setupMockRoutes = async (
+  browser: Browser, // Accept the browser object
+  isLoggedIn: boolean = false,
+  customRoutes: Array<{ url: string; response: any }> = [],
+): Promise<{ context: BrowserContext; page: Page }> => {
+  const { context, page } = await getFreshBrowserState(browser)
   await fetchMockData()
   await page.unroute('**/api/**')
 
-  const apiResponses = [
+  const defaultRoutes = [
     { url: '**/webgui/api/**', response: {} },
     { url: '**/api/user/opsiserver', response: { result: serverId } },
-    { url: '**/api/auth/login', response: { result: 'Login success' } },
-    {
-      url: '**/api/user/configuration',
-      response: { user: userId, configuration: mockData.config_files },
-    },
-    {
-      url: '**/api/opsidata/server/disabled-features',
-      response: [],
-    },
-    {
-      url: '**/addons/webgui/api/opsidata/depots?**',
-      response: serverObjectList,
-    },
-    {
-      url: '**/addons/webgui/api/opsidata/clients?**',
-      response: clientObjectList,
-    },
+    ...(isLoggedIn
+      ? [
+          { url: '**/api/auth/login', response: { result: 'Login success' } },
+          {
+            url: '**/api/user/configuration',
+            response: { user: userId, configuration: mockData.config_files },
+          },
+          { url: '**/api/opsidata/server/disabled-features', response: [] },
+          {
+            url: '**/addons/webgui/api/opsidata/depots?**',
+            response: serverObjectList,
+          },
+          {
+            url: '**/addons/webgui/api/opsidata/clients?**',
+            response: clientObjectList,
+          },
+        ]
+      : []),
   ]
 
-  for (const { url, response } of apiResponses) {
-    await page.route(url, (route: Route) => {
-      route.fulfill({
-        status: 200,
-        headers: {
-          ...defaultResponseHeaders,
-          'x-opsi-worker-id': `${serverId}:1`,
-          'x-opsi-user-id': `user:${userId}`,
-        },
-        contentType: 'application/json',
-        body: JSON.stringify(response),
-      })
-    })
+  const routesToMock = customRoutes.length > 0 ? customRoutes : defaultRoutes
+  for (const { url, response } of routesToMock) {
+    await addMockRoute(page, url, response)
   }
+  return { context, page }
 }
 
-export const toggleTheme = async (page: Page, targetTheme: 'light' | 'dark') => {
-  const isLightTheme = (await page.getByTestId('theme-switch').getAttribute('aria-checked')) === 'false'
-  if ((targetTheme === 'dark' && isLightTheme) || (targetTheme === 'light' && !isLightTheme)) {
+export const toggleTheme = async (
+  page: Page,
+  targetTheme: 'light' | 'dark',
+) => {
+  const isLightTheme =
+    (await page.getByTestId('theme-switch').getAttribute('aria-checked')) ===
+    'false'
+  if (
+    (targetTheme === 'dark' && isLightTheme) ||
+    (targetTheme === 'light' && !isLightTheme)
+  ) {
     await page.getByTestId('theme-toggle').click()
   }
 }
 
-export const selectLanguage = async (page: Page, targetLanguage: 'en' | 'de') => {
-  const currentLanguage = await page.getByTestId('language-dropdown').getAttribute('aria-label')
+export const selectLanguage = async (
+  page: Page,
+  targetLanguage: 'en' | 'de',
+) => {
+  const currentLanguage = await page
+    .getByTestId('language-dropdown')
+    .getAttribute('aria-label')
   if (currentLanguage !== targetLanguage) {
     await page.getByTestId('language-dropdown').click()
     await page.getByText(targetLanguage).click()
