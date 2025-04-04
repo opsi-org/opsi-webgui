dac6e31544eb2e7d93910194dfe8c84b68e735b9
john <j.john@uib.de>
2025-04-04 14:17:12 +0000

    [chg] Update devcontainer post start script

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-
 # echo 'Setup /etc/hosts' 1>&2
 # echo "127.0.0.1       $HOSTNAME $(hostname) mysql redis grafana localhost" > /tmp/hosts
 # grep -v "127.0.0.1" /etc/hosts | grep -v $HOSTNAME >> /tmp/hosts
 # cp /tmp/hosts /etc/hosts
 # rm /tmp/hosts
 
-# # echo "========================================== Install nodejs"
-# npm install -g n
-# n lts
-# # echo "========================================== Install playwright"
-# # cd /workspace/opsiweb/scripts
-# # ./install.sh
+set -e
 
-echo "========================================== Install dev-tools"
-wget http://binaryindex.uib.gmbh/development/opsi-dev-tools/linux/x64/opsi-dev-tools_linux_x64_1.27.0.tar.gz
-tar -xf opsi-dev-tools_linux_x64_1.27.0.tar.gz
-./opsi-dev-tool --self-install
-opsi-dev-tool --self-upgrade
-rm -f opsi-dev-tools_linux_x64_1.27.0.tar.gz opsi-dev-tools_linux_x64_1.27.0.tar.gz.1
-rm -f opsi-dev-tool
+DEV_TOOLS_URL="http://binaryindex.uib.gmbh/development/opsi-dev-tools/linux/x64/opsi-dev-tools_linux_x64_1.27.0.tar.gz"
+DEV_TOOLS_ARCHIVE="opsi-dev-tools_linux_x64_1.27.0.tar.gz"
+DEV_TOOLS_BINARY="opsi-dev-tool"
+WEBGUI_BACKEND_PATH="/workspace/backend/addon/webgui"
+WEBGUI_INSTALL_PATH="/data/opsiconfd/addons/webgui"
 
-echo "========================================== Install webgui dependencies"
+echo -e "\n==========================================\nInstalling dev-tools\n===================================================="
+wget $DEV_TOOLS_URL
+tar -xf $DEV_TOOLS_ARCHIVE
+./$DEV_TOOLS_BINARY --self-install
+$DEV_TOOLS_BINARY --self-upgrade
+$DEV_TOOLS_BINARY git-hooks --install
+rm -f $DEV_TOOLS_ARCHIVE ${DEV_TOOLS_ARCHIVE}.1
+rm -f $DEV_TOOLS_BINARY
+
+echo -e "\n==========================================\nInstalling webgui dependencies\n==========================================="
 cd /workspace/frontend/
-npm i
+npm install
 npx nuxi clean
 
+echo -e "\n==========================================\nInstalling Playwright\n===================================================="
+npx playwright install-deps
 
+echo -e "\n==========================================\nStarting opsiconfd container\n===================================================="
+echo 'alias opsiconfdrestart="docker exec -u root $(docker ps --format "{{.Names}}" | grep gui | grep server | grep opsi) supervisorctl reload"' >>/etc/bash.bashrc
+echo 'alias opsiconfdcontainer="docker exec -u root $(docker ps --format "{{.Names}}" | grep gui | grep server | grep opsi)" >>/etc/bash.bashrc'
 
-echo 'alias opsiconfdrestart="docker exec -u root $(docker ps --format "{{.Names}}" | grep gui | grep server | grep opsi) supervisorctl reload"' >> /etc/bash.bashrc
-echo 'alias opsiconfdcontainer="docker exec -u root $(docker ps --format "{{.Names}}" | grep gui | grep server | grep opsi)"'  >> /etc/bash.bashrc
+exec zsh
