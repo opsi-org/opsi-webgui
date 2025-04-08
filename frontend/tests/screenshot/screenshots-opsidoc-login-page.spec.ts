c3f2fd1354446bca06f0910af91d09b7ed18f7f2
john <j.john@uib.de>
2025-04-08 14:14:09 +0000

    [chg] Update login and related components screenshots

--- frontend/tests/screenshot/screenshots-opsidoc-login-page.spec.ts
+++ frontend/tests/screenshot/screenshots-opsidoc-login-page.spec.ts
@@ -8,11 +8,11 @@ License: AGPL-3.0
 
 import { test } from '@playwright/test'
 import {
-  setupMockRoutes,
   toggleTheme,
   selectLanguage,
   takeFullPageScreenshot,
 } from '../shared/utils'
+import { addMockRoute, serverId } from '../shared/mock/mocks'
 import { themes, languages } from '../shared/constants'
 
 test.describe('Login Page', () => {
@@ -21,18 +21,30 @@ test.describe('Login Page', () => {
       test(`${theme.charAt(0).toUpperCase() + theme.slice(1)} - ${language.toUpperCase()}`, async ({
         page,
       }) => {
-        await setupMockRoutes(page, false) // Not logged in
+        await addMockRoute(page, '**/api/user/opsiserver', { result: serverId })
+        await page.waitForTimeout(1000)
         await page.goto('/login', {
           waitUntil: 'networkidle',
           timeout: 60000,
         })
         await toggleTheme(page, theme)
-        // await selectLanguage(page, language)
-        await page.waitForTimeout(1000)
+        await selectLanguage(page, language)
         await takeFullPageScreenshot(
           page,
           `screenshots/opsidoc/${theme}/${language}/opsi-webgui-login.png`,
         )
+        const themeToggle = page.getByTestId('theme-toggle')
+        await page.waitForTimeout(500)
+        await themeToggle.screenshot({
+          path: `screenshots/opsidoc/${theme}/${language}/opsi-webgui-theme.png`,
+        })
+
+        const languageDropdown = page.getByTestId('language-dropdown')
+        await languageDropdown.click()
+        await page.waitForTimeout(500)
+        await languageDropdown.screenshot({
+          path: `screenshots/opsidoc/${theme}/${language}/opsi-webgui-language.png`,
+        })
       })
     }
   }
