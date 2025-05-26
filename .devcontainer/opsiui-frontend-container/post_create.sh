2324084939e83f3f87a758c30d6ceab34fabb9fe
sucher <a.sucher@uib.de>
2025-05-26 10:58:14 +0000

    [chg] copy lisence file if exists

--- .devcontainer/opsiui-frontend-container/post_create.sh
+++ .devcontainer/opsiui-frontend-container/post_create.sh
@@ -27,4 +27,11 @@ echo "========================================== Link webgui backend"
 rm -rf /data/opsiconfd/addons/webgui
 cp -r /workspace/backend/addon/webgui /data/opsiconfd/addons/.
 
+
+# use license if available
+if [ -f /workspace/docker/test.opsilic ]; then
+    sudo cp /workspace/docker/test.opsilic /etc/opsi/licenses/test.opsilic
+fi
+
+
 zsh
