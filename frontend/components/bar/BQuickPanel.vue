35eb259bc81d78a00bc5c64d748a48cc970f497f
Jeena John <j.john@uib.de>
2025-07-03 14:16:14 +0000

    [fix] disable client actions if no clients are selected or if user config is readonly

--- frontend/components/bar/BQuickPanel.vue
+++ frontend/components/bar/BQuickPanel.vue
@@ -35,6 +35,9 @@ License: AGPL-3.0
           :client-ids="storeSelection.selectionClients"
           icon="client"
           :link="false"
+          :disabled="
+            storeSelection.selectionClients.length === 0 || storeConfigapp().config?.read_only
+          "
         />
         <DialogDProductQuickActions />
       </div>
