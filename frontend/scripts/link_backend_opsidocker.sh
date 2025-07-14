6b87b1840c58d1264ec73f5b883ec3837bb40849
sucher <a.sucher@uib.de>
2025-07-14 08:28:53 +0000

    [new] vscode button to copy local backend files to opsi-docker (44471/.../webgui/...)

--- frontend/scripts/link_backend_opsidocker.sh
+++ frontend/scripts/link_backend_opsidocker.sh
@@ -1,6 +1,7 @@
-#!/bin/bash -ex
+#!/bin/zsh
+set -ex
+shopt -s expand_aliases
 
 sudo rm -r /data/opsiconfd/addons/webgui/python
 sudo cp -rf /workspace/backend/addon/webgui/python/ /data/opsiconfd/addons/webgui/.
 sudo chown $(whoami):$(whoami) -R /data/opsiconfd/addons/
-opsiconfd-docker-restart
