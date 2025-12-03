4b32c5fa22d99eb554f87339a25018444f66e565
sucher <a.sucher@uib.de>
2025-12-03 09:21:39 +0000

    [fix] install opsi-dev-cli

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "========================================== Install dev-tools"
-wget http://binaryindex.uib.gmbh/development/opsi-dev-tools/linux/x64/opsi-dev-tools_linux_x64_1.16.3.tar.gz
-tar -xf opsi-dev-tools_linux_x64_1.16.3.tar.gz
-./opsi-dev-tool --self-install
-rm -f opsi-dev-tools_linux_x64_1.16.3.tar.gz opsi-dev-tools_linux_x64_1.16.3.tar.gz.1
-rm -f opsi-dev-tool
+wget http://binaryindex.uib.gmbh/development/opsi-dev-tools/linux/x64/installer.sh
+chmod +x installer.sh
+./installer.sh
+opsi-dev-cli self upgrade
+rm -f installer.sh
 
 
 
@@ -15,31 +15,4 @@ npm i
 echo "========================================== Link webgui backend"
 
 rm -rf /data/opsiconfd/addons/webgui
-# ln -s /workspace/backend/addon/webgui /data/opsiconfd/addons/
-# chown pwuser:pwuser /data/opsiconfd/addons/webgui
-# ln -s $OPSI_ADDON_DIRS /data/opsiconfd/addons
 cp -r /workspace/backend/addon/webgui /data/opsiconfd/addons/.
-
-# alias opsiconfdrestart="docker exec -u root $(docker ps --format "{{.Names}}" | grep gui | grep server | grep opsi) supervisorctl reload"  alias opsiconfdcontainer="docker exec -u root $(docker ps --format "{{.Names}}" | grep gui | grep server | grep opsi)"
-# # Setup mariadb server
-# id
-# service mysql restart
-# sleep 5
-# awk -v var="password" -v new_val=$MYSQL_ROOT_PASSWORD 'BEGIN{FS=OFS="="}match($1, "^\\s*" var "\\s*") {$2=" " new_val}1' /etc/mysql/debian.cnf | sudo tee /etc/mysql/debian.cnf
-
-# sleep 5
-
-# echo "create opsi user"
-# mysql -u root -e "CREATE USER $MYSQL_USER@localhost IDENTIFIED BY '$MYSQL_PASSWORD';"
-# mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO $MYSQL_USER@localhost IDENTIFIED BY '$MYSQL_PASSWORD';"
-# mysql -u root -e "FLUSH PRIVILEGES"
-# echo "create database opsi" | mysql -u root
-# zcat /workspace/docker/confd-dev-data.sql.gz | sed 's/dev-server.uib.local/'$OPSI_HOSTNAME'/g'  | mariadb -h localhost -u opsi --password=opsi opsi
-
-# openssl req -x509 -newkey rsa:4096 -keyout /var/tmp/key.pem -out /var/tmp/cert.pem -sha256 -days 365 --nodes -subj '/CN=localhost'
-# chown node:node /var/tmp/*
-# chmod 750 /var/tmp/*
-
-# npm install -g n
-# n lts
-# # bash
--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
 
-# echo 'Setup /etc/hosts' 1>&2
-# echo "127.0.0.1       $HOSTNAME $(hostname) mysql redis grafana localhost" > /tmp/hosts
-# grep -v "127.0.0.1" /etc/hosts | grep -v $HOSTNAME >> /tmp/hosts
-# cp /tmp/hosts /etc/hosts
-# rm /tmp/hosts
-
-# # echo "========================================== Install nodejs"
-# npm install -g n
-# n lts
-# # echo "========================================== Install playwright"
-# # cd /workspace/opsiweb/scripts
-# # ./install.sh
 
 echo "========================================== Install dev-tools"
-wget http://binaryindex.uib.gmbh/development/opsi-dev-tools/linux/x64/opsi-dev-tools_linux_x64_1.16.3.tar.gz
-tar -xf opsi-dev-tools_linux_x64_1.16.3.tar.gz
-./opsi-dev-tool --self-install
-rm -f opsi-dev-tools_linux_x64_1.16.3.tar.gz opsi-dev-tools_linux_x64_1.16.3.tar.gz.1
-rm -f opsi-dev-tool
 
-# pwd
-# # pip3 install --trusted-host pypi.uib.gmbh --index-url http://pypi.uib.gmbh:8080/simple opsi-dev-tools
-# cd /workspace
+wget http://binaryindex.uib.gmbh/development/opsi-dev-tools/linux/x64/installer.sh
+chmod +x installer.sh
+./installer.sh
 opsi-dev-cli self upgrade
-# # opsi-dev-tool --self-install
-opsi-dev-tool git-hooks --install
-# echo "=========================================="
-# # ./opsi-dev-tools git-hooks --install
+rm -f installer.sh
 
-# # su - node <<SHT
+opsi-dev-cli self upgrade
+opsi-dev-cli git-hook install
+
 
 echo -e "======Configure opsiconfd-docker container commands======"
 CONTAINER_NAME_DOCKER=$(sudo docker ps --format "{{.Names}}" | grep gui-43 | grep server | grep opsi)
