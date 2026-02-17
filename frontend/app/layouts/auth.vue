e0ef7a6eca1a171c751613cc90efc12f2f9f23cc
Jeena John <j.john@uib.de>
2026-02-17 19:00:09 +0000

    [chg] update auth and default layouts

--- frontend/app/layouts/auth.vue
+++ frontend/app/layouts/auth.vue
@@ -6,5 +6,18 @@ All rights reserved.
 License: AGPL-3.0
 -->
 <template>
-  <LayoutLAuth><slot /> </LayoutLAuth>
+  <el-container class="h-screen">
+    <el-header />
+    <!-- do not remove header -->
+    <el-main>
+      <slot />
+    </el-main>
+    <BarBAuthFooter v-once />
+  </el-container>
 </template>
+
+<script setup lang="ts">
+  const settings = storeSettings()
+  settings.initColormode()
+  settings.initLanguage()
+</script>
