7313e9eb15329239c36e90e24a055143795b58a8
Jeena John <j.john@uib.de>
2026-02-18 10:24:31 +0000

    [chg] add tests and related configs

---
+++ frontend/tests/screenshot/screenshots-marketing.spec.ts
@@ -0,0 +1,58 @@
+/**
+This file is part of opsi-webgui application.
+opsi-webgui is part of the desktop management solution opsi http://www.opsi.org
+Copyright (c) uib GmbH <info@uib.de> 2025
+All rights reserved.
+License: AGPL-3.0
+*/
+
+import { test } from '@playwright/test'
+import {
+  useHighResolutionViewport,
+  toggleTheme,
+  selectLanguage,
+  login,
+  takeFullPageScreenshot,
+} from '../shared/utils'
+import { setupMockRoutes } from '../shared/mock/mocks'
+import { themes, languages } from '../shared/constants'
+
+test.describe('Clients Page with Products', () => {
+  useHighResolutionViewport()
+  for (const theme of themes) {
+    for (const language of languages) {
+      test(`${theme.charAt(0).toUpperCase() + theme.slice(1)} - ${language.toUpperCase()}`, async ({
+        context,
+        page,
+      }) => {
+        await setupMockRoutes(page, true) // Logged in
+        await page.goto('/login', {
+          waitUntil: 'networkidle',
+          timeout: 60000,
+        })
+        await toggleTheme(page, theme)
+        await selectLanguage(page, language)
+        await login(context, page)
+        if (page.url() !== '/clients') {
+          await page.goto('/clients/', {
+            waitUntil: 'networkidle',
+            timeout: 60000,
+          })
+        }
+        await page.waitForSelector('[data-testid="clients-products-button"]', {
+          state: 'visible',
+        })
+        await page.click('[data-testid="clients-products-button"]')
+        await page.waitForURL('**/clients/products/**', {
+          waitUntil: 'networkidle',
+          timeout: 60000,
+        })
+        await page.waitForTimeout(10000)
+        await takeFullPageScreenshot(
+          page,
+          `screenshots/marketing/${theme}/${language}/opsi-webgui-clients-with-products.png`
+        )
+      })
+    }
+  }
+})
