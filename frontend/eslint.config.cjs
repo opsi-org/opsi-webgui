8a31be06f753b0eedab187609dd66b77cb0c37cf
Jeena John <j.john@uib.de>
2026-02-20 16:19:34 +0000

    [fix] eslint configuration

---
+++ frontend/eslint.config.cjs
@@ -0,0 +1,44 @@
+const vueParser = require('vue-eslint-parser')
+const tsParser = require('@typescript-eslint/parser')
+const vuePlugin = require('eslint-plugin-vue')
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+
+module.exports = [
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+      ecmaVersion: 2021,
+    },
+    plugins: {
+      vue: vuePlugin,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...(vuePlugin.configs['vue3-recommended']?.rules || {}),
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...(tsPlugin.configs.recommended?.rules || {}),
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]
